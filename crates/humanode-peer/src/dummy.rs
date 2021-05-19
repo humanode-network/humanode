f3d2f70c85efd0e7e457d696ed02ed9d4665ace9
MOZGIII <mike-n@narod.ru>
2021-05-19 16:21:11 +0300

    --wip-- [skip ci]

--- crates/humanode-peer/src/dummy.rs
+++ crates/humanode-peer/src/dummy.rs
@@ -1,19 +1,25 @@
 use std::marker::PhantomData;
 
-use sp_consensus::{Error as ConsensusError, SelectChain};
+use sp_consensus::{import_queue::ImportQueue, Error as ConsensusError, SelectChain};
 use sp_runtime::traits::Block as BlockT;
 
-#[derive(Clone, Default)]
-pub struct DummyConsensus<Block> {
-    block_type: PhantomData<Block>,
-}
+// #[derive(Clone, Default)]
+// pub struct DummyConsensus<Block> {
+//     block_type: PhantomData<Block>,
+// }
 
-impl<Block: BlockT> SelectChain<Block> for DummyConsensus<Block> {
-    fn leaves(&self) -> Result<Vec<<Block as BlockT>::Hash>, ConsensusError> {
-        todo!()
-    }
+// impl<Block: BlockT> SelectChain<Block> for DummyConsensus<Block> {
+//     fn leaves(&self) -> Result<Vec<<Block as BlockT>::Hash>, ConsensusError> {
+//         todo!()
+//     }
 
-    fn best_chain(&self) -> Result<<Block as BlockT>::Header, ConsensusError> {
-        todo!()
-    }
-}
+//     fn best_chain(&self) -> Result<<Block as BlockT>::Header, ConsensusError> {
+//         todo!()
+//     }
+// }
+
+// pub struct DummyImportQueue<Block> {
+//     block_type: PhantomData<Block>,
+// }
+
+// impl<Block: BlockT> ImportQueue<Block> for DummyImportQueue<Block> {}
