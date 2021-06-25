81610c15101fe976620ff1012c14a0cfe38976f5
MOZGIII <mike-n@narod.ru>
2021-06-25 13:08:13 +0300

    Switched robonode-crypto implementation to ed25519-dalek

--- crates/robonode-crypto/src/lib.rs
+++ crates/robonode-crypto/src/lib.rs
@@ -7,57 +7,57 @@
 )]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edwards 25519 curve cryptography.
-pub mod ed25519 {
+pub use ed25519_dalek::{self, Signer, Verifier};
 
-    /// An Robonode keypair using Ed25519 as its crypto.
-    pub type RobonodePair = sp_application_crypto::ed25519::Pair;
+/// Robonode keypair.
+pub type Keypair = ed25519_dalek::Keypair;
 
-    /// An Robonode signature using Ed25519 as its crypto.
-    pub type RobonodeSignature = sp_application_crypto::ed25519::Signature;
+/// Robonode signature.
+pub type Signature = ed25519_dalek::Signature;
 
-    /// An Robonode identifier using Ed25519 as its crypto.
-    pub type RobonodePublicKey = sp_application_crypto::ed25519::Public;
-}
+/// Robonode public key.
+pub type PublicKey = ed25519_dalek::PublicKey;
+
+/// Robonode secret key.
+pub type SecretKey = ed25519_dalek::SecretKey;
 
 #[cfg(test)]
 mod tests {
-    use crate::ed25519::{RobonodePair, RobonodePublicKey, RobonodeSignature};
+    use super::*;
     use hex_literal::hex;
-    use sp_application_crypto::Pair;
+    use rand::rngs::OsRng;
+    use std::convert::TryInto;
+
+    // Test vectors.
+    // See: https://ed25519.cr.yp.to/python/sign.py
+    //      https://ed25519.cr.yp.to/python/sign.input
+    const SK: [u8; 64] = hex!("9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a");
+    const PK: [u8; 32] = hex!("d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a");
+    const M: [u8; 0] = hex!("");
+    const SM: [u8; 64] = hex!("e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b");
 
     #[test]
-    fn generated_pair_should_work() {
-        let pair = RobonodePair::generate();
-        let public = pair.0.public();
-        let message = b"Something important";
-        let signature = pair.0.sign(&message[..]);
-        assert!(RobonodePair::verify(&signature, &message[..], &public));
-        assert!(!RobonodePair::verify(
-            &signature,
-            b"Something else",
-            &public
-        ));
+    fn test_vectors() {
+        let pair = Keypair::from_bytes(&SK[..]).unwrap();
+        let public = PublicKey::from_bytes(&PK[..]).unwrap();
+        assert_eq!(pair.public, public);
+
+        let signature: Signature = SM.try_into().unwrap();
+
+        println!("{:#02X?}", pair.sign(&M[..]).to_bytes());
+        assert!(pair.sign(&M[..]) == signature);
+        assert!(public.verify(&M[..], &signature).is_ok());
     }
 
     #[test]
-    fn test_vector_by_string_should_work() {
-        let pair = RobonodePair::from_string(
-            "0x9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60",
-            None,
-        )
-        .unwrap();
-        let public = pair.public();
-        assert_eq!(
-            public,
-            RobonodePublicKey::from_raw(hex!(
-                "d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a"
-            ))
-        );
-        let message = b"";
-        let signature = hex!("e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b");
-        let signature = RobonodeSignature::from_raw(signature);
-        assert!(pair.sign(&message[..]) == signature);
-        assert!(RobonodePair::verify(&signature, &message[..], &public));
+    fn generated_pair() {
+        let mut csprng = OsRng {};
+        let pair = Keypair::generate(&mut csprng);
+
+        let message = b"Something important";
+        let signature = pair.sign(&message[..]);
+
+        assert!(pair.verify(&message[..], &signature).is_ok());
+        assert!(pair.verify(b"Something else", &signature).is_err());
     }
 }
