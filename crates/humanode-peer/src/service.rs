25e86ac6d0b859b37df05fc172301cfdb2859e38
MOZGIII <mike-n@narod.ru>
2021-07-04 09:34:26 +0300

    Remove the debug tx submission

--- crates/humanode-peer/src/service.rs
+++ crates/humanode-peer/src/service.rs
@@ -183,29 +183,6 @@ pub async fn new_full(config: Configuration) -> Result<TaskManager, ServiceError
         let client = Arc::clone(&client);
         let transaction_pool = Arc::clone(&transaction_pool);
         Box::pin(async move {
-            let authenticate = pallet_bioauth::Authenticate {
-                ticket: vec![12, 0, 255, 0, 8, 255, 255],
-                ticket_signature: vec![
-                    62, 31, 179, 117, 32, 155, 212, 172, 1, 37, 235, 137, 53, 165, 135, 141, 48,
-                    106, 53, 68, 205, 61, 61, 154, 128, 120, 198, 224, 101, 148, 146, 117, 33, 245,
-                    132, 4, 103, 96, 228, 75, 61, 248, 19, 254, 244, 114, 190, 250, 86, 249, 186,
-                    31, 142, 149, 120, 112, 202, 140, 202, 187, 55, 182, 145, 6,
-                ],
-            };
-            let call = pallet_bioauth::Call::authenticate(authenticate);
-            let ext = humanode_runtime::UncheckedExtrinsic::new_unsigned(call.into());
-
-            let at = client.chain_info().best_hash;
-            transaction_pool
-                .pool()
-                .submit_and_watch(
-                    &sp_runtime::generic::BlockId::Hash(at),
-                    sp_runtime::transaction_validity::TransactionSource::Local,
-                    ext.into(),
-                )
-                .await
-                .unwrap();
-
             info!("bioauth flow starting up");
             let should_enroll = std::env::var("ENROLL").unwrap_or_default() == "true";
             if should_enroll {
