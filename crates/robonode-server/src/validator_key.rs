58c38ff9607ec714357d695eddfda77c56d0b4e8
MOZGIII <mike-n@narod.ru>
2021-09-20 13:07:20 +0300

    Abstract robonode validator key around substrate key type (#147)

--- crates/robonode-server/src/validator_key.rs
+++ crates/robonode-server/src/validator_key.rs
@@ -1,17 +1,19 @@
 //! The validator key integration logic.
 
-use std::convert::TryFrom;
+use std::convert::{TryFrom, TryInto};
 
-use sp_application_crypto::Public;
-use sp_application_crypto::RuntimePublic;
+use sp_core::crypto::{CryptoType, Pair, Public};
 
 use crate::logic;
 
-/// A validator public key implemented via substrate's aura consensus key.
-pub struct AuraPublic(sp_application_crypto::sr25519::Public);
+/// A validator public key implemented via substrate crypto.
+pub struct SubstratePublic<T: CryptoType>(<T::Pair as Pair>::Public);
 
 #[async_trait::async_trait]
-impl logic::Verifier<Vec<u8>> for AuraPublic {
+impl<T: CryptoType> logic::Verifier<Vec<u8>> for SubstratePublic<T>
+where
+    <T::Pair as Pair>::Signature: for<'a> TryFrom<&'a [u8]>,
+{
     type Error = ();
 
     async fn verify<'a, D>(&self, data: D, signature: Vec<u8>) -> Result<bool, Self::Error>
@@ -19,26 +21,29 @@ impl logic::Verifier<Vec<u8>> for AuraPublic {
         D: AsRef<[u8]> + Send + 'a,
     {
         let data = data.as_ref();
-        let signature = sp_application_crypto::sr25519::Signature::try_from(signature.as_slice())?;
-        Ok(self.0.verify(&data, &signature))
+        let signature = signature.as_slice().try_into().map_err(|_| ())?;
+        Ok(T::Pair::verify(&signature, &data, &self.0))
     }
 }
 
-impl TryFrom<&[u8]> for AuraPublic {
+impl<T: CryptoType> TryFrom<&[u8]> for SubstratePublic<T>
+where
+    <T::Pair as Pair>::Public: for<'a> TryFrom<&'a [u8]>,
+{
     type Error = ();
 
     fn try_from(val: &[u8]) -> Result<Self, Self::Error> {
-        Ok(Self(sp_application_crypto::sr25519::Public::try_from(val)?))
+        Ok(Self(val.try_into().map_err(|_| ())?))
     }
 }
 
-impl From<AuraPublic> for Vec<u8> {
-    fn from(val: AuraPublic) -> Self {
-        val.as_ref().to_vec()
+impl<T: CryptoType> From<SubstratePublic<T>> for Vec<u8> {
+    fn from(val: SubstratePublic<T>) -> Self {
+        val.0.to_raw_vec()
     }
 }
 
-impl AsRef<[u8]> for AuraPublic {
+impl<T: CryptoType> AsRef<[u8]> for SubstratePublic<T> {
     fn as_ref(&self) -> &[u8] {
         self.0.as_slice()
     }
