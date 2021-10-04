9b8823aae2afadae6f73457360b8245a53b2c94f
tonycdot <69084044+tonycdot@users.noreply.github.com>
2021-10-04 21:29:13 +0300

    Add smoke-test for the node code at CI (#151)
    * Add script for smoke test
    
    * Move script exec from test job to new one
    
    * Refactoring e2e tests
    
    * Add block check
    
    * Set up polkadot tools
    
    * Change names
    
    * Change smoke tests in accordance with conversations
    
    * Change code.yml
    
    * Update utils/e2e-tests/run-tests
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>
    
    * Change debug to release in code.yml
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>

---
+++ utils/e2e-tests/tests/block-check.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+
+get_address() {
+    "$COMMAND" key inspect "$@" | grep "SS58 Address:" | awk '{print $3}'
+}
+
+# Set up command.
+COMMAND="$1"
+
+# Make temporary test directory.
+TEMPDIR="$(mktemp -d)"
+trap 'rm -rf "$TEMPDIR"; pkill -P "$$"' EXIT
+
+# Run the node.
+"$COMMAND" --dev --base-path "$TEMPDIR" &
+
+# Get the address.
+ADDR="$(get_address "//Alice")"
+
+# Send TX and wait for block creation.
+# The test will also fail if no block is created within 20 sec.
+POLKA_JSON="$(timeout 20 polkadot-js-api --ws "ws://127.0.0.1:9944" --seed "//Alice" tx.balances.transfer "$ADDR" 10000)"
+
+# Log polkadot-js-api response.
+printf "polkadot-js-api response:\n%s\n" "$POLKA_JSON" >&2
+
+# Look for "InBlock" field in response. Fail the test if absent.
+grep -q "InBlock" <<<"$POLKA_JSON"
+
+printf "Test succeded\n" >&2
