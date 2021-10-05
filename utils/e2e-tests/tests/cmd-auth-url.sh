a1eb4654274981d2d541255c83ac63244f6062a5
MOZGIII <mike-n@narod.ru>
2021-10-05 12:00:18 +0300

    Add E2E test

---
+++ utils/e2e-tests/tests/cmd-auth-url.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+# Make temporary test directory.
+TEMPDIR="$(mktemp -d)"
+trap 'rm -rf "$TEMPDIR"' EXIT
+
+OUTPUT="$("$@" bioauth auth-url --webapp-url https://example.com --rpc-url https://localhost:9933)"
+EXPECTED="https://example.com/open?url=https%3A%2F%2Flocalhost%3A9933"
+
+if [[ "$OUTPUT" != "$EXPECTED" ]]; then
+  printf "Output did not match:\n  expected: %s\n  actual:   %s\n" "$EXPECTED" "$OUTPUT"
+  exit 1
+fi
+
+printf "Test succeded\n" >&2
