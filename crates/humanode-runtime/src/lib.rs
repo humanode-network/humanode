9499d6fc74b2c163423bbb9e5d31a678a2542e48
dmitrylavrenov <lawrenowdima@gmail.com>
2021-12-03 14:20:33 +0300

    Change MaxNonces value

--- crates/humanode-runtime/src/lib.rs
+++ crates/humanode-runtime/src/lib.rs
@@ -420,7 +420,7 @@ const TIMESTAMP_HOUR: UnixMilliseconds = 60 * TIMESTAMP_MINUTE;
 parameter_types! {
     pub const AuthenticationsExpireAfter: UnixMilliseconds = 72 * TIMESTAMP_HOUR;
     pub const MaxAuthentications: u32 = 512;
-    pub const MaxNonces: u32 = 512;
+    pub const MaxNonces: u32 = 102400;
 }
 
 impl pallet_bioauth::Config for Runtime {
