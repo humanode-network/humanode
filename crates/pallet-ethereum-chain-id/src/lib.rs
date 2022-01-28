f14fa6050330b61c37f12291baa03932d97f296f
dmitrylavrenov <lawrenowdima@gmail.com>
2022-01-28 15:58:38 +0300

    Add ethereum-chain-id based on Moonbeam pallet

---
+++ crates/pallet-ethereum-chain-id/src/lib.rs
@@ -0,0 +1,58 @@
+//! A substrate minimal Pallet that stores the numeric Ethereum-style chain id in the runtime.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::pallet;
+
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+// We have to temporarily allow some clippy lints. Later on we'll send patches to substrate to
+// fix them at their end.
+#[allow(
+    missing_docs,
+    clippy::missing_docs_in_private_items,
+    clippy::unused_unit
+)]
+#[pallet]
+pub mod pallet {
+
+    use frame_support::pallet_prelude::*;
+
+    /// The Ethereum Chain Id Pallet
+    #[pallet::pallet]
+    pub struct Pallet<T>(PhantomData<T>);
+
+    /// Configuration trait of this pallet.
+    #[pallet::config]
+    pub trait Config: frame_system::Config {}
+
+    impl<T: Config> Get<u64> for Pallet<T> {
+        fn get() -> u64 {
+            Self::chain_id()
+        }
+    }
+
+    /// Ethereum-style chain id.
+    #[pallet::storage]
+    #[pallet::getter(fn chain_id)]
+    pub type ChainId<T> = StorageValue<_, u64, ValueQuery>;
+
+    #[pallet::genesis_config]
+    #[derive(Default)]
+    pub struct GenesisConfig {
+        pub chain_id: u64,
+    }
+
+    #[pallet::genesis_build]
+    impl<T: Config> GenesisBuild<T> for GenesisConfig {
+        fn build(&self) {
+            ChainId::<T>::put(self.chain_id);
+        }
+    }
+}
