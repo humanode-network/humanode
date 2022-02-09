7392e43ea4e99973bcdc4b274416d766608f879c
dmitrylavrenov <lawrenowdima@gmail.com>
2022-02-09 11:00:05 +0300

    Add bioauth-babe feature and corresponding tests

--- crates/bioauth-consensus/src/babe.rs
+++ crates/bioauth-consensus/src/babe.rs
@@ -100,12 +100,23 @@ mod tests {
     use sp_runtime::{Digest, DigestItem};
     use std::sync::Arc;
 
-    type MockAuraAuthorityId = sp_consensus_aura::sr25519::AuthorityId;
-
     mock! {
         RuntimeApi {
-            fn authorities(&self, _at: &sp_api::BlockId<Block>) -> Result<NativeOrEncoded<Vec<MockAuraAuthorityId>>, ApiError>;
-            fn slot_duration(&self) -> sp_consensus_aura::SlotDuration;
+            fn configuration(&self) -> sp_consensus_babe::BabeGenesisConfiguration;
+            fn current_epoch_start(&self) -> sp_consensus_babe::Slot;
+            fn current_epoch(&self, _at: &sp_api::BlockId<Block>) -> Result<NativeOrEncoded<sp_consensus_babe::Epoch>, ApiError>;
+            fn next_epoch(&self) -> sp_consensus_babe::Epoch;
+            fn generate_key_ownership_proof(
+                &self,
+                _slot: sp_consensus_babe::Slot,
+                _authority_id: sp_consensus_babe::AuthorityId,
+            ) -> Option<sp_consensus_babe::OpaqueKeyOwnershipProof>;
+            fn submit_report_equivocation_unsigned_extrinsic(
+                &self,
+                _equivocation_proof: sp_consensus_babe::EquivocationProof<<Block as BlockT>::Header>,
+                _key_owner_proof: sp_consensus_babe::OpaqueKeyOwnershipProof,
+            ) -> Option<()>;
+
         }
     }
 
@@ -113,17 +124,41 @@ mod tests {
     struct MockWrapperRuntimeApi(Arc<MockRuntimeApi>);
 
     sp_api::mock_impl_runtime_apis! {
-        impl AuraApi<Block, MockAuraAuthorityId> for MockWrapperRuntimeApi {
-            #[advanced]
-            fn authorities(&self, at: &sp_api::BlockId<Block>) -> Result<
-                NativeOrEncoded<Vec<MockAuraAuthorityId>>,
-                ApiError
-            > {
-                self.0.authorities(at)
+        impl BabeApi<Block> for MockWrapperRuntimeApi {
+            fn configuration() -> sp_consensus_babe::BabeGenesisConfiguration {
+                self.0.configuration()
             }
 
-            fn slot_duration() -> sp_consensus_aura::SlotDuration {
-                self.0.slot_duration()
+            fn current_epoch_start() -> sp_consensus_babe::Slot {
+                self.0.current_epoch_start()
+            }
+
+            #[advanced]
+            fn current_epoch(&self, at: &sp_api::BlockId<Block>) -> Result<
+                NativeOrEncoded<sp_consensus_babe::Epoch>,
+                ApiError
+            > {
+                self.0.current_epoch(at)
+            }
+
+            fn next_epoch() -> sp_consensus_babe::Epoch {
+                self.0.next_epoch()
+            }
+
+            fn generate_key_ownership_proof(
+                &self,
+                slot: sp_consensus_babe::Slot,
+                authority_id: sp_consensus_babe::AuthorityId,
+            ) -> Option<sp_consensus_babe::OpaqueKeyOwnershipProof> {
+                self.0.generate_key_ownership_proof(slot, authority_id)
+            }
+
+            fn submit_report_equivocation_unsigned_extrinsic(
+                &self,
+                equivocation_proof: sp_consensus_babe::EquivocationProof<<Block as BlockT>::Header>,
+                key_owner_proof: sp_consensus_babe::OpaqueKeyOwnershipProof,
+            ) -> Option<()> {
+                self.0.submit_report_equivocation_unsigned_extrinsic(equivocation_proof, key_owner_proof)
             }
         }
     }
@@ -139,13 +174,20 @@ mod tests {
         }
     }
 
-    fn prepare_block_header_with_aura_pre_digest(empty_digest: bool) -> Header {
+    fn prepare_block_header_with_babe_pre_digest(
+        empty_digest: bool,
+        authority_index: u32,
+    ) -> Header {
         let mut digest_items = vec![];
         if !empty_digest {
-            let slot = sp_consensus_aura::Slot::from(1);
-            let item = <DigestItem as CompatibleDigestItem<
-                sp_consensus_aura::sr25519::AuthoritySignature,
-            >>::aura_pre_digest(slot);
+            let slot = sp_consensus_babe::Slot::from(1);
+            let primary_pre_digest = sp_consensus_babe::digests::SecondaryPlainPreDigest {
+                authority_index,
+                slot,
+            };
+            let pre_digest =
+                sp_consensus_babe::digests::PreDigest::SecondaryPlain(primary_pre_digest);
+            let item = <DigestItem as CompatibleDigestItem>::babe_pre_digest(pre_digest);
             digest_items.push(item);
         }
 
@@ -158,16 +200,33 @@ mod tests {
         }
     }
 
-    /// This test verifies aura block author extractor success when a respective runtime_api call (authorities)
-    /// succeeds and the block header contains a correct aura digest.
+    fn prepare_epoch() -> sp_consensus_babe::Epoch {
+        sp_consensus_babe::Epoch {
+            epoch_index: Default::default(),
+            start_slot: Default::default(),
+            duration: Default::default(),
+            authorities: vec![(
+                sp_consensus_babe::AuthorityId::default(),
+                Default::default(),
+            )],
+            randomness: Default::default(),
+            config: sp_consensus_babe::BabeEpochConfiguration {
+                c: Default::default(),
+                allowed_slots: sp_consensus_babe::AllowedSlots::PrimaryAndSecondaryPlainSlots,
+            },
+        }
+    }
+
+    /// This test verifies babe block author extractor success when a respective runtime_api call (current_epoch)
+    /// succeeds and the block header contains a correct babe digest.
     #[test]
     fn success() {
         let mut mock_client = MockClient::new();
 
         let mut mock_runtime_api = MockRuntimeApi::new();
         mock_runtime_api
-            .expect_authorities()
-            .returning(|_| Ok(NativeOrEncoded::from(vec![MockAuraAuthorityId::default()])));
+            .expect_current_epoch()
+            .returning(|_| Ok(NativeOrEncoded::from(prepare_epoch())));
 
         let runtime_api = MockWrapperRuntimeApi(Arc::new(mock_runtime_api));
 
@@ -177,13 +236,13 @@ mod tests {
 
         let client = Arc::new(mock_client);
 
-        let block_author_extractor: BlockAuthorExtractor<Block, _, MockAuraAuthorityId> =
+        let block_author_extractor: BlockAuthorExtractor<Block, _> =
             BlockAuthorExtractor::new(Arc::clone(&client));
 
         let res = crate::BlockAuthorExtractor::extract_block_author(
             &block_author_extractor,
             &sp_api::BlockId::Number(0),
-            &prepare_block_header_with_aura_pre_digest(false),
+            &prepare_block_header_with_babe_pre_digest(false, 0),
         );
 
         // Drop the test object and all the mocks in it, effectively running the mock assertions.
@@ -191,60 +250,17 @@ mod tests {
         // Unwrap the client from the Arc and drop it, ensuring it's mock assertions run too.
         drop(Arc::try_unwrap(client).unwrap());
 
-        assert_eq!(res.unwrap(), MockAuraAuthorityId::default());
+        assert_eq!(res.unwrap(), sp_consensus_babe::AuthorityId::default());
     }
 
-    /// This test verifies aura block author extractor failure when a respective runtime_api call (authorities)
-    /// succeeds, but the block header contains an incorrect digest.
-    #[test]
-    fn error_unable_to_obtain_slot() {
-        let mut mock_client = MockClient::new();
-
-        let mut mock_runtime_api = MockRuntimeApi::new();
-        mock_runtime_api
-            .expect_authorities()
-            .returning(|_| Ok(NativeOrEncoded::from(vec![MockAuraAuthorityId::default()])));
-
-        let runtime_api = MockWrapperRuntimeApi(Arc::new(mock_runtime_api));
-
-        mock_client
-            .expect_runtime_api()
-            .returning(move || runtime_api.clone().into());
-
-        let client = Arc::new(mock_client);
-
-        let block_author_extractor: BlockAuthorExtractor<Block, _, MockAuraAuthorityId> =
-            BlockAuthorExtractor::new(Arc::clone(&client));
-
-        let res = crate::BlockAuthorExtractor::extract_block_author(
-            &block_author_extractor,
-            &sp_api::BlockId::Number(0),
-            &prepare_block_header_with_aura_pre_digest(true),
-        );
-
-        // Drop the test object and all the mocks in it, effectively running the mock assertions.
-        drop(block_author_extractor);
-        // Unwrap the client from the Arc and drop it, ensuring it's mock assertions run too.
-        drop(Arc::try_unwrap(client).unwrap());
-
-        match res.unwrap_err() {
-            AuraBlockAuthorExtractorError::UnableToObtainSlot => {}
-            ref e => panic!(
-                "assertion failed: `{:?}` does not match `{}`",
-                e,
-                stringify!(AuraBlockAuthorExtractorError::UnableToObtainSlot)
-            ),
-        }
-    }
-
-    /// This test verifies aura block author extractor failure when
-    /// a respective runtime_api call (authorities) fails.
+    /// This test verifies babe block author extractor failure when
+    /// a respective runtime_api call (current_epoch) fails.
     #[test]
     fn runtime_error() {
         let mut mock_client = MockClient::new();
 
         let mut mock_runtime_api = MockRuntimeApi::new();
-        mock_runtime_api.expect_authorities().returning(|_| {
+        mock_runtime_api.expect_current_epoch().returning(|_| {
             Err((Box::from("Test error") as Box<dyn std::error::Error + Send + Sync>).into())
         });
 
@@ -256,13 +272,13 @@ mod tests {
 
         let client = Arc::new(mock_client);
 
-        let block_author_extractor: BlockAuthorExtractor<Block, _, MockAuraAuthorityId> =
+        let block_author_extractor: BlockAuthorExtractor<Block, _> =
             BlockAuthorExtractor::new(Arc::clone(&client));
 
         let res = crate::BlockAuthorExtractor::extract_block_author(
             &block_author_extractor,
             &sp_api::BlockId::Number(0),
-            &prepare_block_header_with_aura_pre_digest(false),
+            &prepare_block_header_with_babe_pre_digest(false, 0),
         );
 
         // Drop the test object and all the mocks in it, effectively running the mock assertions.
@@ -271,14 +287,100 @@ mod tests {
         drop(Arc::try_unwrap(client).unwrap());
 
         match res.unwrap_err() {
-            AuraBlockAuthorExtractorError::UnableToExtractAuthorities(e)
+            BabeBlockAuthorExtractorError::UnableToExtractCurrentEpoch(e)
                 if e.to_string() == "Test error" => {}
             ref e => panic!(
                 "assertion failed: `{:?}` does not match `{}`",
                 e,
-                stringify!(AuraBlockAuthorExtractorError::UnableToExtractAuthorities(
+                stringify!(BabeBlockAuthorExtractorError::UnableToExtractCurrentEpoch(
                     "Test error"
                 ))
+            ),
+        }
+    }
+
+    /// This test verifies babe block author extractor failure when a respective runtime_api call (current_epoch)
+    /// succeeds, but the block header contains an incorrect digest.
+    #[test]
+    fn error_unable_to_obtain_slot() {
+        let mut mock_client = MockClient::new();
+
+        let mut mock_runtime_api = MockRuntimeApi::new();
+        mock_runtime_api
+            .expect_current_epoch()
+            .returning(|_| Ok(NativeOrEncoded::from(prepare_epoch())));
+
+        let runtime_api = MockWrapperRuntimeApi(Arc::new(mock_runtime_api));
+
+        mock_client
+            .expect_runtime_api()
+            .returning(move || runtime_api.clone().into());
+
+        let client = Arc::new(mock_client);
+
+        let block_author_extractor: BlockAuthorExtractor<Block, _> =
+            BlockAuthorExtractor::new(Arc::clone(&client));
+
+        let res = crate::BlockAuthorExtractor::extract_block_author(
+            &block_author_extractor,
+            &sp_api::BlockId::Number(0),
+            &prepare_block_header_with_babe_pre_digest(true, 0),
+        );
+
+        // Drop the test object and all the mocks in it, effectively running the mock assertions.
+        drop(block_author_extractor);
+        // Unwrap the client from the Arc and drop it, ensuring it's mock assertions run too.
+        drop(Arc::try_unwrap(client).unwrap());
+
+        match res.unwrap_err() {
+            BabeBlockAuthorExtractorError::UnableToObtainSlot => {}
+            ref e => panic!(
+                "assertion failed: `{:?}` does not match `{}`",
+                e,
+                stringify!(BabeBlockAuthorExtractorError::UnableToObtainSlot)
+            ),
+        }
+    }
+
+    /// This test verifies babe block author extractor failure when a respective runtime_api call (current_epoch)
+    /// succeeds, slot has been extracted, but the author can't be extracted.
+    #[test]
+    fn error_unable_to_obtain_author() {
+        let mut mock_client = MockClient::new();
+
+        let mut mock_runtime_api = MockRuntimeApi::new();
+        mock_runtime_api
+            .expect_current_epoch()
+            .returning(|_| Ok(NativeOrEncoded::from(prepare_epoch())));
+
+        let runtime_api = MockWrapperRuntimeApi(Arc::new(mock_runtime_api));
+
+        mock_client
+            .expect_runtime_api()
+            .returning(move || runtime_api.clone().into());
+
+        let client = Arc::new(mock_client);
+
+        let block_author_extractor: BlockAuthorExtractor<Block, _> =
+            BlockAuthorExtractor::new(Arc::clone(&client));
+
+        let res = crate::BlockAuthorExtractor::extract_block_author(
+            &block_author_extractor,
+            &sp_api::BlockId::Number(0),
+            &prepare_block_header_with_babe_pre_digest(false, 1),
+        );
+
+        // Drop the test object and all the mocks in it, effectively running the mock assertions.
+        drop(block_author_extractor);
+        // Unwrap the client from the Arc and drop it, ensuring it's mock assertions run too.
+        drop(Arc::try_unwrap(client).unwrap());
+
+        match res.unwrap_err() {
+            BabeBlockAuthorExtractorError::UnableToObtainAuthor => {}
+            ref e => panic!(
+                "assertion failed: `{:?}` does not match `{}`",
+                e,
+                stringify!(BabeBlockAuthorExtractorError::UnableToObtainAuthor)
             ),
         }
     }
