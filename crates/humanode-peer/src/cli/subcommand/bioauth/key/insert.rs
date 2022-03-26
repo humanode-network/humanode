87b4c506a969ad4a75161e500dd05e0c9d6129b1
Dmitry Lavrenov <39522748+dmitrylavrenov@users.noreply.github.com>
2022-03-26 14:47:14 +0400

    Add bioauth key insert subcommand (#275) (#279)
    * Add bioauth key insert subcommand
    
    * Use babeId instead of bioauthId for now
    
    * Extract bioauth key existence at the keystore into helper function
    
    * Bioauth key insert into helper function
    
    * Import babe authorityId as BabeId
    
    * Use master Cargo.lock
    
    * Fixes based on review
    
    (cherry picked from commit a39b0954e97b71d7835ffd79e79b685018915f73)

---
+++ crates/humanode-peer/src/cli/subcommand/bioauth/key/insert.rs
@@ -0,0 +1,93 @@
+//! Bioauth key management subcommands.
+
+use sc_cli::{utils, CliConfiguration, KeystoreParams, SharedParams};
+use sc_service::KeystoreContainer;
+use sp_application_crypto::{AppKey, AppPublic, Pair};
+use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+use sp_keystore::CryptoStore;
+use std::sync::Arc;
+use structopt::StructOpt;
+
+use crate::cli::CliConfigurationExt;
+
+/// The `bioauth key insert` command.
+#[derive(Debug, StructOpt)]
+pub struct InsertKeyCmd {
+    /// The secret key uri (mnemonic).
+    #[structopt(long)]
+    pub suri: String,
+
+    #[allow(missing_docs, clippy::missing_docs_in_private_items)]
+    #[structopt(flatten)]
+    pub shared_params: SharedParams,
+
+    #[allow(missing_docs, clippy::missing_docs_in_private_items)]
+    #[structopt(flatten)]
+    pub keystore_params: KeystoreParams,
+}
+
+/// An error that occured during key insert.
+#[derive(thiserror::Error, Debug)]
+pub enum InsertKeyError {
+    /// Something went wrong while extracting the list of bioauth keys.
+    #[error("Unable to extract keys list from keystore: {0}")]
+    UnableToExtractKeys(sp_keystore::Error),
+    /// The bioauth key is already inserted error.
+    #[error("The bioauth key is already inserted")]
+    AlreadyInserted,
+}
+
+/// A helper function to verify that there is no bioauth key at the keystore yet.
+pub async fn ensure_bioauth_key_absent<PK: AppPublic>(
+    keystore: Arc<dyn CryptoStore>,
+) -> Result<(), InsertKeyError> {
+    let mut current_keys = crate::validator_key::AppCryptoPublic::<PK>::list(keystore.as_ref())
+        .await
+        .map_err(InsertKeyError::UnableToExtractKeys)?;
+
+    if current_keys.next().is_some() {
+        return Err(InsertKeyError::AlreadyInserted);
+    }
+    Ok(())
+}
+
+/// A helper function to insert bioauth key into the keystore.
+pub async fn insert_bioauth_key<PK: AppPublic>(
+    suri: &str,
+    keystore: Arc<dyn CryptoStore>,
+) -> sc_cli::Result<()> {
+    // We don't use a password for keystore at the current moment. That's why None is passed.
+    let pair = utils::pair_from_suri::<<PK as AppKey>::Pair>(suri, None)?;
+    let public = pair.public().as_ref().to_vec();
+    keystore
+        .insert_unknown(PK::ID, suri, &public[..])
+        .await
+        .map_err(|_| sc_cli::Error::KeyStoreOperation)?;
+    Ok(())
+}
+
+impl InsertKeyCmd {
+    /// Run the insert command.
+    pub async fn run(&self, keystore_container: KeystoreContainer) -> sc_cli::Result<()> {
+        let keystore = keystore_container.keystore();
+
+        ensure_bioauth_key_absent::<AuraId>(Arc::clone(&keystore))
+            .await
+            .map_err(|err| sc_cli::Error::Service(sc_service::Error::Other(err.to_string())))?;
+
+        insert_bioauth_key::<AuraId>(self.suri.as_ref(), keystore).await?;
+        Ok(())
+    }
+}
+
+impl CliConfiguration for InsertKeyCmd {
+    fn shared_params(&self) -> &SharedParams {
+        &self.shared_params
+    }
+
+    fn keystore_params(&self) -> Option<&KeystoreParams> {
+        Some(&self.keystore_params)
+    }
+}
+
+impl CliConfigurationExt for InsertKeyCmd {}
