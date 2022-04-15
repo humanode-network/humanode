bfeea8709fb999355a27d073e7699926aa8b0c5e
MOZGIII <mike-n@narod.ru>
2022-04-15 13:07:25 +0400

    Use heredoc at EXPECTED_INSPECT_OUTPUT

--- utils/e2e-tests/tests/cmd-bioauth-key.sh
+++ utils/e2e-tests/tests/cmd-bioauth-key.sh
@@ -11,12 +11,16 @@ GENERATE_OUTPUT="$("$@" bioauth key generate)"
 grep -q "Secret phrase:" <<<"$GENERATE_OUTPUT"
 
 INSPECT_OUTPUT="$("$@" bioauth key inspect --suri "custom resemble extend detect expand ready battle never deputy argue right tent")"
-EXPECTED_INSPECT_OUTPUT="Secret phrase:       custom resemble extend detect expand ready battle never deputy argue right tent
+EXPECTED_INSPECT_OUTPUT="$(
+  cat <<EOF
+Secret phrase:       custom resemble extend detect expand ready battle never deputy argue right tent
   Secret seed:       0x5cfec9f10108959aa5bb6b5966a0ba8df2c2f5c1181acf91dcdf9ec227f17226
   Public key (hex):  0xeeaf49c07a5c2af27b081042338ad41b7991f4aa078e2f7a6ddfd8266b8e985c
   Account ID:        0xeeaf49c07a5c2af27b081042338ad41b7991f4aa078e2f7a6ddfd8266b8e985c
   Public key (SS58): 5HTfKp6LcTcDh5ep2gszAHcKKC1YUExzvpphnJ7NTtbFBwBn
-  SS58 Address:      5HTfKp6LcTcDh5ep2gszAHcKKC1YUExzvpphnJ7NTtbFBwBn"
+  SS58 Address:      5HTfKp6LcTcDh5ep2gszAHcKKC1YUExzvpphnJ7NTtbFBwBn
+EOF
+)"
 
 if [[ "$INSPECT_OUTPUT" != "$EXPECTED_INSPECT_OUTPUT" ]]; then
   printf "Output did not match:\n\n  expected: %s\n\n  actual:   %s\n" "$INSPECT_OUTPUT" "$EXPECTED_INSPECT_OUTPUT"
