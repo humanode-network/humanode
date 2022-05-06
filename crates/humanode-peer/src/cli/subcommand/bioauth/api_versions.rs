25046ebb57d792fc8b12de402048fdfb1646248e
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-05-06 15:08:38 +0300

    Implement api versions

---
+++ crates/humanode-peer/src/cli/subcommand/bioauth/api_versions.rs
@@ -0,0 +1,36 @@
+//! ApiVersions subcommand logic.
+
+use sc_cli::{CliConfiguration, SharedParams};
+use serde_json::json;
+use structopt::StructOpt;
+
+use crate::{cli::CliConfigurationExt, version};
+
+/// The `bioauth api-verisons` command.
+#[derive(Debug, StructOpt)]
+pub struct ApiVersionsCmd {
+    #[allow(missing_docs, clippy::missing_docs_in_private_items)]
+    #[structopt(flatten)]
+    pub shared_params: SharedParams,
+}
+
+impl ApiVersionsCmd {
+    /// Run the api-verisons command.
+    pub async fn run(&self) -> sc_cli::Result<()> {
+        let current_api_versions = version::API_VERSIONS;
+        let json = json!(current_api_versions);
+        println!(
+            "{}",
+            serde_json::to_string_pretty(&json).expect("Json pretty print failed")
+        );
+        Ok(())
+    }
+}
+
+impl CliConfiguration for ApiVersionsCmd {
+    fn shared_params(&self) -> &SharedParams {
+        &self.shared_params
+    }
+}
+
+impl CliConfigurationExt for ApiVersionsCmd {}
---
+++ crates/humanode-peer/src/version.rs
@@ -0,0 +1,14 @@
+//! The version definition of the peer's related components.
+
+use serde::Serialize;
+
+/// Define API versions.
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ApiVersions {
+    /// Bioauth flow version.
+    pub bioauth_flow: u8,
+}
+
+/// The Current API versions.
+pub const API_VERSIONS: ApiVersions = ApiVersions { bioauth_flow: 1 };
