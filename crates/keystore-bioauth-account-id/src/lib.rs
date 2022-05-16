ac1344440cd344478d3e4846f452d635e433081e
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-05-16 15:38:11 +0300

    Implement AppCrypto for KeystoreBioauthAccountId

--- crates/keystore-bioauth-account-id/src/lib.rs
+++ crates/keystore-bioauth-account-id/src/lib.rs
@@ -3,7 +3,9 @@
 // Either generate code at stadard mode, or `no_std`, based on the `std` feature presence.
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use frame_system::offchain::AppCrypto;
 use sp_application_crypto::KeyTypeId;
+use sp_runtime::{MultiSignature, MultiSigner};
 
 /// Keystore Bioauth Account ID key type definition.
 pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"kbai");
@@ -16,3 +18,9 @@ mod app {
 
 /// App key export.
 pub type KeystoreBioauthAccountId = app::Public;
+
+impl AppCrypto<MultiSigner, MultiSignature> for KeystoreBioauthAccountId {
+    type RuntimeAppPublic = KeystoreBioauthAccountId;
+    type GenericSignature = sp_core::sr25519::Signature;
+    type GenericPublic = sp_core::sr25519::Public;
+}
