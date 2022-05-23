e55ef0e799130d3d09470167b8709eb1a1da0bf4
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-05-23 15:58:09 +0300

    Require MaxEncodedLen

--- crates/pallet-bioauth/src/lib.rs
+++ crates/pallet-bioauth/src/lib.rs
@@ -270,7 +270,6 @@ pub mod pallet {
     #[pallet::pallet]
     #[pallet::storage_version(STORAGE_VERSION)]
     #[pallet::generate_store(pub(super) trait Store)]
-    #[pallet::generate_storage_info]
     pub struct Pallet<T>(_);
 
     /// The public key of the robonode.
