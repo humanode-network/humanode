77b404be1edc0ce42ec68329d2d5a3c1379bdd58
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-05-24 02:14:51 +0300

    Update frontier service

--- crates/humanode-peer/src/service/frontier.rs
+++ crates/humanode-peer/src/service/frontier.rs
@@ -6,7 +6,7 @@ use sc_service::BasePath;
 use super::Block;
 
 /// Create frontier dir.
-pub fn database_dir(config: &sc_service::Configuration) -> std::path::PathBuf {
+pub fn database_dir(config: &sc_service::Configuration, path: &str) -> std::path::PathBuf {
     let config_dir = config
         .base_path
         .as_ref()
@@ -15,15 +15,26 @@ pub fn database_dir(config: &sc_service::Configuration) -> std::path::PathBuf {
             BasePath::from_project("", "", &crate::cli::Root::executable_name())
                 .config_dir(config.chain_spec.id())
         });
-    config_dir.join("frontier").join("db")
+    config_dir.join("frontier").join(path)
 }
 
 /// Construct frontier backend.
 pub fn open_backend(config: &sc_service::Configuration) -> Result<fc_db::Backend<Block>, String> {
     fc_db::Backend::<Block>::new(&fc_db::DatabaseSettings {
-        source: fc_db::DatabaseSettingsSrc::RocksDb {
-            path: database_dir(config),
-            cache_size: 0,
+        source: match config.database {
+            fc_db::DatabaseSource::RocksDb { .. } => fc_db::DatabaseSource::RocksDb {
+                path: database_dir(config, "db"),
+                cache_size: 0,
+            },
+            fc_db::DatabaseSource::ParityDb { .. } => fc_db::DatabaseSource::ParityDb {
+                path: database_dir(config, "paritydb"),
+            },
+            fc_db::DatabaseSource::Auto { .. } => fc_db::DatabaseSource::Auto {
+                rocksdb_path: database_dir(config, "db"),
+                paritydb_path: database_dir(config, "paritydb"),
+                cache_size: 0,
+            },
+            _ => return Err("Supported db sources: `rocksdb` | `paritydb` | `auto`".to_string()),
         },
     })
 }
