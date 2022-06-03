f7c0c326a2ff0801d3f08f87499c071a97d56d8a
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-06-03 13:32:55 +0300

    Make session related constants private

--- crates/humanode-runtime/src/lib.rs
+++ crates/humanode-runtime/src/lib.rs
@@ -311,10 +311,10 @@ impl pallet_bioauth::Verifier<Vec<u8>> for RobonodePublicKeyWrapper {
 
 impl pallet_randomness_collective_flip::Config for Runtime {}
 
-pub const SESSIONS_PER_ERA: u64 = 6;
-pub const BONDING_DURATION: u64 = 24 * 28;
-pub const EPOCH_DURATION: u64 = EPOCH_DURATION_IN_SLOTS;
-pub const REPORT_LONGEVITY: u64 = BONDING_DURATION * SESSIONS_PER_ERA * EPOCH_DURATION;
+const SESSIONS_PER_ERA: u64 = 6;
+const BONDING_DURATION: u64 = 24 * 28;
+const EPOCH_DURATION: u64 = EPOCH_DURATION_IN_SLOTS;
+const REPORT_LONGEVITY: u64 = BONDING_DURATION * SESSIONS_PER_ERA * EPOCH_DURATION;
 
 impl pallet_babe::Config for Runtime {
     type EpochDuration = ConstU64<EPOCH_DURATION>;
