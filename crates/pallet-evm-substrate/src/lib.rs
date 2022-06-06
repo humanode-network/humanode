1ee3f6076a4ecf2574799216a00345fe19e20507
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-06-06 18:02:51 +0300

    Initial impl of evm-substrate mapping

---
+++ crates/pallet-evm-substrate/src/lib.rs
@@ -0,0 +1,86 @@
+//! A substrate pallet.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+// Fix clippy for sp_api::decl_runtime_apis!
+#![allow(clippy::too_many_arguments, clippy::unnecessary_mut_passed)]
+
+use frame_support::pallet_prelude::*;
+use frame_system::pallet_prelude::*;
+pub use pallet::*;
+
+pub type EvmAddress = sp_core::H160;
+
+// We have to temporarily allow some clippy lints. Later on we'll send patches to substrate to
+// fix them at their end.
+#[allow(
+    missing_docs,
+    clippy::missing_docs_in_private_items,
+    clippy::unused_unit
+)]
+#[frame_support::pallet]
+pub mod pallet {
+
+    use super::*;
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    }
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(crate) fn deposit_event)]
+    pub enum Event<T: Config> {
+        ClaimAccount {
+            account_id: T::AccountId,
+            evm_address: EvmAddress,
+        },
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        /// AccountId has already mapped.
+        AccountIdAlreadyMapped,
+        /// Eth address has already mapped.
+        EthAddressAlreadyMapped,
+    }
+
+    #[pallet::storage]
+    #[pallet::getter(fn accounts)]
+    pub type Accounts<T: Config> =
+        StorageMap<_, Twox64Concat, EvmAddress, T::AccountId, OptionQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn evm_addresses)]
+    pub type EvmAddresses<T: Config> =
+        StorageMap<_, Twox64Concat, T::AccountId, EvmAddress, OptionQuery>;
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight(10_000)]
+        pub fn claim_account(origin: OriginFor<T>, eth_address: EvmAddress) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+
+            ensure!(
+                !EvmAddresses::<T>::contains_key(&who),
+                Error::<T>::AccountIdAlreadyMapped
+            );
+            ensure!(
+                !Accounts::<T>::contains_key(eth_address),
+                Error::<T>::EthAddressAlreadyMapped
+            );
+
+            Accounts::<T>::insert(eth_address, &who);
+            EvmAddresses::<T>::insert(&who, eth_address);
+
+            Self::deposit_event(Event::ClaimAccount {
+                account_id: who,
+                evm_address: eth_address,
+            });
+
+            Ok(())
+        }
+    }
+}
