d5cd17225cf075b682ffe003db280d7289eb9dcd
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-06-08 11:55:39 +0300

    Implement Eip712

--- crates/pallet-evm-accounts-mapping/src/lib.rs
+++ crates/pallet-evm-accounts-mapping/src/lib.rs
@@ -1,24 +1,30 @@
-//! A substrate pallet.
+//! A substrate pallet that provides logic to map Substrate and EVM accounts.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 // Fix clippy for sp_api::decl_runtime_apis!
 #![allow(clippy::too_many_arguments, clippy::unnecessary_mut_passed)]
 
-use frame_support::pallet_prelude::*;
+use frame_support::{pallet_prelude::*, sp_runtime::traits::Zero};
 use frame_system::pallet_prelude::*;
 pub use pallet::*;
+use sp_core::H256;
+use sp_io::{crypto::secp256k1_ecdsa_recover, hashing::keccak_256};
 use sp_std::prelude::*;
 
 /// Evm address type.
 pub type EvmAddress = sp_core::H160;
 
-/// Provides the capability to verify an ethereum signature.
-pub trait EthSignatureVerifier {
-    /// The error that can occur during ethereum signature verification.
-    type Error;
+/// A signature (a 512-bit value, plus 8 bits for recovery ID).
+pub type Eip712Signature = [u8; 65];
 
+/// Provides the capability to verify an Eip712 based ethereum signature.
+pub trait Eip712Verifier {
     /// Verify the signature and extract a corresponding [`EvmAddress`] if it's ok.
-    fn verify(signature: Vec<u8>) -> Result<EvmAddress, Self::Error>;
+    fn verify(
+        domain_separator: [u8; 32],
+        message: Vec<u8>,
+        signature: Eip712Signature,
+    ) -> Option<EvmAddress>;
 }
 
 // We have to temporarily allow some clippy lints. Later on we'll send patches to substrate to
@@ -36,7 +42,12 @@ pub mod pallet {
     #[pallet::config]
     pub trait Config: frame_system::Config {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-        type EthSignatureVerifier: EthSignatureVerifier;
+
+        /// Chain ID of EVM.
+        #[pallet::constant]
+        type ChainId: Get<u64>;
+
+        type Eip712Verifier: Eip712Verifier;
     }
 
     #[pallet::event]
@@ -79,7 +90,7 @@ pub mod pallet {
         pub fn claim_account(
             origin: OriginFor<T>,
             eth_address: EvmAddress,
-            signature: Vec<u8>,
+            signature: Eip712Signature,
         ) -> DispatchResult {
             let who = ensure_signed(origin)?;
 
@@ -92,8 +103,12 @@ pub mod pallet {
                 Error::<T>::EthAddressAlreadyMapped
             );
 
-            let address = <T as Config>::EthSignatureVerifier::verify(signature)
-                .map_err(|_| Error::<T>::BadSignature)?;
+            let address = <T as Config>::Eip712Verifier::verify(
+                Self::account_domain_separator(),
+                who.encode(),
+                signature,
+            )
+            .ok_or(Error::<T>::BadSignature)?;
 
             ensure!(eth_address == address, Error::<T>::InvalidSignature);
 
@@ -108,4 +123,60 @@ pub mod pallet {
             Ok(())
         }
     }
+}
+
+impl<T: Config> Pallet<T> {
+    /// A domain separator used at Eip712 flow.
+    fn account_domain_separator() -> [u8; 32] {
+        let domain_hash =
+            keccak_256(b"EIP712Domain(string name,string version,uint256 chainId,bytes32 salt)");
+        let mut domain_seperator_msg = domain_hash.to_vec();
+        domain_seperator_msg.extend_from_slice(&keccak_256(b"Humanode EVM claim")); // name
+        domain_seperator_msg.extend_from_slice(&keccak_256(b"1")); // version
+        domain_seperator_msg.extend_from_slice(&keccak_256(b"5234")); // chain id
+        domain_seperator_msg.extend_from_slice(
+            frame_system::Pallet::<T>::block_hash(T::BlockNumber::zero()).as_ref(),
+        ); // genesis block hash
+        keccak_256(domain_seperator_msg.as_slice())
+    }
+}
+
+///
+pub struct Eip712VerifierFactory;
+
+impl Eip712Verifier for Eip712VerifierFactory {
+    fn verify(
+        domain_separator: [u8; 32],
+        message: Vec<u8>,
+        signature: Eip712Signature,
+    ) -> Option<EvmAddress> {
+        let msg = Self::eip712_signable_message(domain_separator, message);
+        let msg_hash = keccak_256(msg.as_slice());
+
+        recover_signer(&signature, &msg_hash)
+    }
+}
+
+impl Eip712VerifierFactory {
+    /// Eip-712 message to be signed.
+    fn eip712_signable_message(domain_separator: [u8; 32], message: Vec<u8>) -> Vec<u8> {
+        let payload_hash = Self::payload_hash(message);
+
+        let mut msg = b"\x19\x01".to_vec();
+        msg.extend_from_slice(&domain_separator);
+        msg.extend_from_slice(&payload_hash);
+        msg
+    }
+
+    /// Get payload hash from message.
+    fn payload_hash(message: Vec<u8>) -> [u8; 32] {
+        keccak_256(message.as_slice())
+    }
+}
+
+/// A helper function to return a corresponding [`EvmAddress`] from signature and message hash.
+fn recover_signer(sig: &Eip712Signature, msg_hash: &[u8; 32]) -> Option<EvmAddress> {
+    secp256k1_ecdsa_recover(sig, msg_hash)
+        .map(|pubkey| EvmAddress::from(H256::from_slice(&keccak_256(&pubkey))))
+        .ok()
 }
