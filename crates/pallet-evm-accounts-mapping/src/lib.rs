5dbf5814aa96cf79a95021e2a1dced32b4eafa11
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-06-08 13:02:45 +0300

    Move eip712 logic to separate mod

--- crates/pallet-evm-accounts-mapping/src/lib.rs
+++ crates/pallet-evm-accounts-mapping/src/lib.rs
@@ -7,25 +7,12 @@
 use frame_support::{pallet_prelude::*, sp_runtime::traits::Zero};
 use frame_system::pallet_prelude::*;
 pub use pallet::*;
-use sp_core::H256;
-use sp_io::{crypto::secp256k1_ecdsa_recover, hashing::keccak_256};
-use sp_std::prelude::*;
+use sp_io::hashing::keccak_256;
+
+pub mod eip_712;
 
 /// Evm address type.
 pub type EvmAddress = sp_core::H160;
-
-/// A signature (a 512-bit value, plus 8 bits for recovery ID).
-pub type Eip712Signature = [u8; 65];
-
-/// Provides the capability to verify an Eip712 based ethereum signature.
-pub trait Eip712Verifier {
-    /// Verify the signature and extract a corresponding [`EvmAddress`] if it's ok.
-    fn verify(
-        domain_separator: [u8; 32],
-        message: Vec<u8>,
-        signature: Eip712Signature,
-    ) -> Option<EvmAddress>;
-}
 
 // We have to temporarily allow some clippy lints. Later on we'll send patches to substrate to
 // fix them at their end.
@@ -37,12 +24,14 @@ pub trait Eip712Verifier {
 #[frame_support::pallet]
 pub mod pallet {
 
+    use eip_712::Verifier;
+
     use super::*;
 
     #[pallet::config]
     pub trait Config: frame_system::Config + pallet_evm::Config {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-        type Eip712Verifier: Eip712Verifier;
+        type Eip712Verifier: eip_712::Verifier;
     }
 
     #[pallet::event]
@@ -85,7 +74,7 @@ pub mod pallet {
         pub fn claim_account(
             origin: OriginFor<T>,
             eth_address: EvmAddress,
-            signature: Eip712Signature,
+            signature: eip_712::Signature,
         ) -> DispatchResult {
             let who = ensure_signed(origin)?;
 
@@ -137,44 +126,4 @@ impl<T: Config> Pallet<T> {
         domain_seperator_msg.extend_from_slice(genesis_block_hash.as_ref());
         keccak_256(domain_seperator_msg.as_slice())
     }
-}
-
-/// Verify Eip712 typed signature based on provided domain_separator and entire message.
-pub struct Eip712VerifierFactory;
-
-impl Eip712Verifier for Eip712VerifierFactory {
-    fn verify(
-        domain_separator: [u8; 32],
-        message: Vec<u8>,
-        signature: Eip712Signature,
-    ) -> Option<EvmAddress> {
-        let msg = Self::eip712_signable_message(domain_separator, message);
-        let msg_hash = keccak_256(msg.as_slice());
-
-        recover_signer(&signature, &msg_hash)
-    }
-}
-
-impl Eip712VerifierFactory {
-    /// Eip-712 message to be signed.
-    fn eip712_signable_message(domain_separator: [u8; 32], message: Vec<u8>) -> Vec<u8> {
-        let payload_hash = Self::payload_hash(message);
-
-        let mut msg = b"\x19\x01".to_vec();
-        msg.extend_from_slice(&domain_separator);
-        msg.extend_from_slice(&payload_hash);
-        msg
-    }
-
-    /// Get payload hash from message.
-    fn payload_hash(message: Vec<u8>) -> [u8; 32] {
-        keccak_256(message.as_slice())
-    }
-}
-
-/// A helper function to return a corresponding [`EvmAddress`] from signature and message hash.
-fn recover_signer(sig: &Eip712Signature, msg_hash: &[u8; 32]) -> Option<EvmAddress> {
-    secp256k1_ecdsa_recover(sig, msg_hash)
-        .map(|pubkey| EvmAddress::from(H256::from_slice(&keccak_256(&pubkey))))
-        .ok()
 }
