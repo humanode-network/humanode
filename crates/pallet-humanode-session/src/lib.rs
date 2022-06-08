260cb3a5faa55c927fb701fb64693e48951864a1
MOZGIII <mike-n@narod.ru>
2022-06-08 21:10:18 +0400

    Integrate the bootnodes with the humanode session manager

--- crates/pallet-humanode-session/src/lib.rs
+++ crates/pallet-humanode-session/src/lib.rs
@@ -22,11 +22,15 @@ pub mod pallet {
 
     /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
-    pub trait Config: frame_system::Config + pallet_bioauth::Config {
+    pub trait Config:
+        frame_system::Config + pallet_bioauth::Config + pallet_bootnodes::Config
+    {
         type ValidatorPublicKeyOf: Convert<
             <Self as pallet_bioauth::Config>::ValidatorPublicKey,
             Option<Self::AccountId>,
         >;
+
+        type BootnodeIdOf: Convert<<Self as pallet_bootnodes::Config>::BootnodeId, Self::AccountId>;
     }
 
     #[pallet::pallet]
@@ -39,30 +43,47 @@ type FullBioauthAuthentication<T> = pallet_bioauth::Authentication<
     <T as pallet_bioauth::Config>::Moment,
 >;
 
-impl<T: Config>
-    pallet_session::historical::SessionManager<
-        T::AccountId,
-        pallet_bioauth::Authentication<
-            <T as pallet_bioauth::Config>::ValidatorPublicKey,
-            <T as pallet_bioauth::Config>::Moment,
-        >,
-    > for Pallet<T>
+type BootnodeIdOf<T> = <T as pallet_bootnodes::Config>::BootnodeId;
+
+/// The identification type, to indicate where does a particular validator comes from, in a given
+/// session.
+pub enum Identification<T: Config> {
+    /// The validator is a bootnode.
+    Bootnode(BootnodeIdOf<T>),
+    /// The validator is bioauthenticated.
+    Bioauth(FullBioauthAuthentication<T>),
+}
+
+impl<T: Config> pallet_session::historical::SessionManager<T::AccountId, Identification<T>>
+    for Pallet<T>
 {
-    fn new_session(_new_index: u32) -> Option<Vec<(T::AccountId, FullBioauthAuthentication<T>)>> {
-        let next_authorities_data = <pallet_bioauth::Pallet<T>>::active_authentications()
+    fn new_session(_new_index: u32) -> Option<Vec<(T::AccountId, Identification<T>)>> {
+        let bootnodes = <pallet_bootnodes::Pallet<T>>::bootnodes()
+            .into_iter()
+            .map(|id| {
+                (
+                    T::BootnodeIdOf::convert(id.clone()),
+                    Identification::Bootnode(id),
+                )
+            });
+
+        let bioauth_active_authentications = <pallet_bioauth::Pallet<T>>::active_authentications()
             .into_inner()
-            .iter()
+            .into_iter()
             .filter_map(|authentication| {
                 T::ValidatorPublicKeyOf::convert(authentication.public_key.clone())
-                    .map(|account_id| (account_id, authentication.clone()))
-            })
+                    .map(|account_id| (account_id, Identification::Bioauth(authentication)))
+            });
+
+        let next_authorities_data = bootnodes
+            .chain(bioauth_active_authentications)
             .collect::<Vec<_>>();
 
         Some(next_authorities_data)
     }
 
-    // This part of code is reachable but we leave it empty
-    // as we don't have any bioauth related logic code here.
+    // This part of code is reachable, but we leave it empty
+    // as we don't have any meaningful things to do here.
     fn start_session(_start_index: u32) {}
     fn end_session(_end_index: u32) {}
 }
