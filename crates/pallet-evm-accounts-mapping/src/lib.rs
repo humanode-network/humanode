9e9f0f54d076dc528aa13c41bf93f199c5d9510a
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-06-09 22:25:15 +0300

    Remove redundant eth_address argument from claim call

--- crates/pallet-evm-accounts-mapping/src/lib.rs
+++ crates/pallet-evm-accounts-mapping/src/lib.rs
@@ -1,8 +1,6 @@
 //! A substrate pallet that provides logic to map Substrate and EVM accounts.
 
 #![cfg_attr(not(feature = "std"), no_std)]
-// Fix clippy for sp_api::decl_runtime_apis!
-#![allow(clippy::too_many_arguments, clippy::unnecessary_mut_passed)]
 
 use frame_support::{pallet_prelude::*, sp_runtime::traits::Zero};
 use frame_system::pallet_prelude::*;
@@ -15,11 +13,7 @@ pub type EvmAddress = sp_core::H160;
 
 // We have to temporarily allow some clippy lints. Later on we'll send patches to substrate to
 // fix them at their end.
-#[allow(
-    missing_docs,
-    clippy::missing_docs_in_private_items,
-    clippy::unused_unit
-)]
+#[allow(clippy::missing_docs_in_private_items)]
 #[frame_support::pallet]
 pub mod pallet {
 
@@ -29,15 +23,20 @@ pub mod pallet {
 
     #[pallet::config]
     pub trait Config: frame_system::Config + pallet_evm::Config {
+        /// Event type.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+        /// Verifier type to extract author of EIP-712 claim signature.
         type Eip712Verifier: eip_712::Verifier;
     }
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(crate) fn deposit_event)]
     pub enum Event<T: Config> {
+        /// Claim event.
         ClaimAccount {
+            /// AccountId that does claiming.
             account_id: T::AccountId,
+            /// Eth address that is claimed.
             evm_address: EvmAddress,
         },
     }
@@ -50,15 +49,15 @@ pub mod pallet {
         EthAddressAlreadyMapped,
         /// Bad ethereum signature.
         BadSignature,
-        /// Invalid ethereum sugnature.
-        InvalidSignature,
     }
 
+    /// [`EvmAddress`] -> [`AccountId`] storage map.
     #[pallet::storage]
     #[pallet::getter(fn accounts)]
     pub type Accounts<T: Config> =
         StorageMap<_, Twox64Concat, EvmAddress, T::AccountId, OptionQuery>;
 
+    /// [`AccountId`] -> [`EvmAddress`] storage map.
     #[pallet::storage]
     #[pallet::getter(fn evm_addresses)]
     pub type EvmAddresses<T: Config> =
@@ -69,10 +68,10 @@ pub mod pallet {
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
+        /// Extrinsic that provides a possibility to claim eth address once.
         #[pallet::weight(10_000)]
         pub fn claim_account(
             origin: OriginFor<T>,
-            eth_address: EvmAddress,
             signature: eip_712::Signature,
         ) -> DispatchResult {
             let who = ensure_signed(origin)?;
@@ -80,10 +79,6 @@ pub mod pallet {
             ensure!(
                 !EvmAddresses::<T>::contains_key(&who),
                 Error::<T>::AccountIdAlreadyMapped
-            );
-            ensure!(
-                !Accounts::<T>::contains_key(eth_address),
-                Error::<T>::EthAddressAlreadyMapped
             );
 
             let account_claim = eip_712::AccountClaimTypedData {
@@ -98,13 +93,12 @@ pub mod pallet {
                 account: who.encode(),
             };
 
-            let eth_extracted_address =
-                <T as Config>::Eip712Verifier::verify(account_claim, signature)
-                    .ok_or(Error::<T>::BadSignature)?;
+            let eth_address = <T as Config>::Eip712Verifier::verify(account_claim, signature)
+                .ok_or(Error::<T>::BadSignature)?;
 
             ensure!(
-                eth_address == eth_extracted_address,
-                Error::<T>::InvalidSignature
+                !Accounts::<T>::contains_key(eth_address),
+                Error::<T>::EthAddressAlreadyMapped
             );
 
             Accounts::<T>::insert(eth_address, &who);
