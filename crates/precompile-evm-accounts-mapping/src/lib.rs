d10279cc2375a711137a73cf71c58c0a249e1f84
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-06-13 11:41:33 +0300

    Implement precompile to get mapped native account for provided evm address

---
+++ crates/precompile-evm-accounts-mapping/src/lib.rs
@@ -0,0 +1,45 @@
+//! A precompile to check and return a proper native account for provided evm address.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::Encode;
+use fp_evm::{
+    ExitError, ExitSucceed, Precompile, PrecompileFailure, PrecompileHandle, PrecompileOutput,
+    PrecompileResult,
+};
+use sp_std::marker::PhantomData;
+
+/// The cost of the operation in gas.
+// TODO(#352): implement proper dynamic gas cost estimation.
+const GAS_COST: u64 = 200;
+
+/// Exposes a proper evm accounts mapping to native accounts.
+pub struct EvmAccountsMapping<Runtime>(PhantomData<Runtime>);
+
+impl<T> Precompile for EvmAccountsMapping<T>
+where
+    T: pallet_evm_accounts_mapping::Config,
+{
+    fn execute(handle: &mut impl PrecompileHandle) -> PrecompileResult {
+        handle.record_cost(GAS_COST)?;
+
+        let evm_address_bytes: [u8; 20] =
+            handle
+                .input()
+                .try_into()
+                .map_err(|_| PrecompileFailure::Error {
+                    exit_status: ExitError::Other("input must be a valid evm address".into()),
+                })?;
+
+        let evm_address = sp_core::H160::from(evm_address_bytes);
+        let native_account = pallet_evm_accounts_mapping::Accounts::<T>::get(evm_address)
+            .ok_or_else(|| PrecompileFailure::Error {
+                exit_status: ExitError::Other("evm address isn't mapped".into()),
+            })?;
+
+        Ok(PrecompileOutput {
+            exit_status: ExitSucceed::Returned,
+            output: native_account.encode(),
+        })
+    }
+}
