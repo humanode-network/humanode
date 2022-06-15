57eb74fb949f4b668ac4e8a4590cfc06f16b83e2
MOZGIII <mike-n@narod.ru>
2022-06-15 15:19:06 +0400

    Implement NoFee for pallet_transaction_payment and pallet_evm

---
+++ crates/humanode-runtime/src/fees.rs
@@ -0,0 +1,50 @@
+use super::*;
+
+pub struct NoFee;
+
+impl pallet_transaction_payment::OnChargeTransaction<Runtime> for NoFee {
+    type Balance = Balance;
+    type LiquidityInfo = ();
+
+    fn withdraw_fee(
+        _who: &AccountId,
+        _call: &Call,
+        _info: &DispatchInfoOf<Call>,
+        _fee: Self::Balance,
+        _tip: Self::Balance,
+    ) -> Result<Self::LiquidityInfo, TransactionValidityError> {
+        Ok(())
+    }
+
+    fn correct_and_deposit_fee(
+        _who: &AccountId,
+        _dispatch_info: &DispatchInfoOf<Call>,
+        _post_info: &PostDispatchInfoOf<Call>,
+        _corrected_fee: Self::Balance,
+        _tip: Self::Balance,
+        _already_withdrawn: Self::LiquidityInfo,
+    ) -> Result<(), TransactionValidityError> {
+        Ok(())
+    }
+}
+
+impl pallet_evm::OnChargeEVMTransaction<Runtime> for NoFee {
+    type LiquidityInfo = ();
+
+    fn withdraw_fee(
+        _who: &H160,
+        _fee: U256,
+    ) -> Result<Self::LiquidityInfo, pallet_evm::Error<Runtime>> {
+        Ok(())
+    }
+
+    fn correct_and_deposit_fee(
+        _who: &H160,
+        _corrected_fee: U256,
+        _base_fee: U256,
+        _already_withdrawn: Self::LiquidityInfo,
+    ) -> Self::LiquidityInfo {
+    }
+
+    fn pay_priority_fee(_tip: Self::LiquidityInfo) {}
+}
