cd1fa5d97118e34bd688303cd79eaac160a78433
MOZGIII <mike-n@narod.ru>
2022-06-21 02:03:59 +0400

    Tweak vergen

--- crates/humanode-peer/build.rs
+++ crates/humanode-peer/build.rs
@@ -3,5 +3,9 @@
 use vergen::{vergen, Config};
 
 fn main() {
-    vergen(Config::default()).unwrap();
+    let mut cfg = Config::default();
+    *cfg.git_mut().branch_mut() = false;
+    *cfg.git_mut().commit_timestamp_mut() = false;
+    *cfg.git_mut().semver_mut() = false;
+    vergen(cfg).unwrap();
 }
