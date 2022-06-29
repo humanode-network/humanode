e66c61c53843e9e6beadf9024d552671870fc1be
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-06-29 11:53:27 +0300

    Add missing docs

--- crates/chain-properties/src/lib.rs
+++ crates/chain-properties/src/lib.rs
@@ -1,15 +1,21 @@
+//! Interface for getting chain properties.
+
 use std::{marker::PhantomData, sync::Arc};
 
 use chain_properties_api::ChainPropertiesApi;
 use sp_api::{BlockT, ProvideRuntimeApi};
 use sp_blockchain::HeaderBackend;
 
+/// Provides access to chain properties.
 pub struct ChainProperties<Client, Block> {
+    /// The substrate client, provides access to the runtime APIs.
     client: Arc<Client>,
+    /// The phantom types.
     phantom_types: PhantomData<Block>,
 }
 
 impl<Client, Block> ChainProperties<Client, Block> {
+    /// Create a new [`ChainProperties`].
     pub fn new(client: Arc<Client>) -> Self {
         Self {
             client,
@@ -26,6 +32,7 @@ where
     Client::Api: ChainPropertiesApi<Block>,
     Block: BlockT,
 {
+    /// Get chain Ss58 prefix.
     pub fn ss58_prefix(&self) -> u16 {
         // Extract an id of the genesis block.
         let at = sp_api::BlockId::Hash(self.client.info().genesis_hash);
