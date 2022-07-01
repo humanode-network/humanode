28ce87165d01415c1fddecd703eb3def3c5e68e9
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-07-01 09:49:40 +0300

    Add comment for SS58Prefix

--- crates/humanode-runtime/src/lib.rs
+++ crates/humanode-runtime/src/lib.rs
@@ -260,6 +260,8 @@ impl frame_system::Config for Runtime {
     /// Weight information for the extrinsics of this pallet.
     type SystemWeightInfo = ();
     /// This is used as an identifier of the chain. 42 is the generic substrate prefix.
+    /// The Humande prefix is defined at pallet-chain-properties. It allows us to set up
+    /// it easy in genesis before launching the chain without changing the code itself.
     type SS58Prefix = ChainProperties;
     /// The set code logic, just the default since we're not a parachain.
     type OnSetCode = ();
