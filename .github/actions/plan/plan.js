f5164425a8567d31bfd7ae7153fcd3780a62abf2
Henry <henrybyhee@gmail.com>
2022-07-07 03:02:25 +0000

    Optimized benchmark

--- .github/actions/plan/plan.js
+++ .github/actions/plan/plan.js
@@ -75,8 +75,8 @@ const codeModes = {
   },
   runBenchmark: {
     name: "test-run pallet benchmarks",
-    cargoCommand: "run",
-    cargoArgs: "--features runtime-benchmarks benchmark pallet --chain benchmark --execution wasm --wasm-execution compiled --pallet 'pallet-bioauth' --extrinsic '*' --steps 1 --repeat 1",
+    cargoCommand: "run --release",
+    cargoArgs: "--features runtime-benchmarks benchmark pallet --chain benchmark --execution wasm --wasm-execution compiled --pallet '*' --extrinsic '*' --steps 1 --repeat 1",
     cargoCacheKey: "run-benchmark",
   },
 };
