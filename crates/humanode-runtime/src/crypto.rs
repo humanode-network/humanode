0eff95749e03477e73f9d8e15de3d720431c9873
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-07-11 13:46:03 +0300

    Move crypto helper functions to separate mod

---
+++ crates/humanode-runtime/src/crypto.rs
@@ -0,0 +1,33 @@
+//! Various crypto helper functions.
+
+use sp_runtime::app_crypto::{sr25519, Pair, Public};
+
+use crate::{AccountId, BabeId, GrandpaId, IdentifyAccount, ImOnlineId, Signature, Verify};
+
+/// The public key for the accounts.
+type AccountPublic = <Signature as Verify>::Signer;
+
+/// Generate a crypto pair from seed.
+pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
+    TPublic::Pair::from_string(&format!("//{}", seed), None)
+        .expect("static values are valid; qed")
+        .public()
+}
+
+/// Generate an account ID from seed.
+pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId
+where
+    AccountPublic: From<<TPublic::Pair as Pair>::Public>,
+{
+    AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
+}
+
+/// Generate consensus authority keys.
+pub fn authority_keys_from_seed(seed: &str) -> (AccountId, BabeId, GrandpaId, ImOnlineId) {
+    (
+        get_account_id_from_seed::<sr25519::Public>(seed),
+        get_from_seed::<BabeId>(seed),
+        get_from_seed::<GrandpaId>(seed),
+        get_from_seed::<ImOnlineId>(seed),
+    )
+}
