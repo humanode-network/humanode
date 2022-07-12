106ce6cd0d65aa4bf23308b96b8ef7c08c22e023
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-07-12 20:55:22 +0300

    Implement test for evm call

--- crates/humanode-runtime/src/tests/fixed_supply.rs
+++ crates/humanode-runtime/src/tests/fixed_supply.rs
@@ -1,12 +1,18 @@
 //! Tests to verify fixed supply logic.
 
+use std::str::FromStr;
+
 use frame_support::{
     dispatch::DispatchInfo,
+    traits::Currency,
     weights::{DispatchClass, Pays},
 };
+use pallet_evm::AddressMapping;
+use sp_application_crypto::ByteArray;
 use sp_runtime::traits::SignedExtension;
 
 use super::*;
+use crate::find_author::truncate_account_id_into_ethereum_address;
 
 const INIT_BALANCE: u128 = 10u128.pow(18 + 6);
 
@@ -107,17 +113,20 @@ fn total_issuance_dust_removal() {
 fn total_issuance_transaction_payment_validate() {
     // Build the state from the config.
     new_test_ext_with().execute_with(move || {
+        // Prepare test data.
         let call = pallet_balances::Call::transfer {
             dest: account_id("Alice").into(),
             value: 100,
         }
         .into();
-
         let normal = DispatchInfo {
             weight: 10,
             class: DispatchClass::Normal,
             pays_fee: Pays::Yes,
         };
+
+        // Check total issuance before making transaction validate.
+        let total_issuance_before = Balances::total_issuance();
 
         assert_ok!(
             pallet_transaction_payment::ChargeTransactionPayment::<Runtime>::from(0).validate(
@@ -127,5 +136,47 @@ fn total_issuance_transaction_payment_validate() {
                 10
             )
         );
+
+        // Check total issuance after making transaction validate.
+        assert_eq!(Balances::total_issuance(), total_issuance_before);
+    })
+}
+
+#[test]
+fn total_issuance_evm_call() {
+    // Build the state from the config.
+    new_test_ext_with().execute_with(move || {
+        let existential_deposit =
+            <<Runtime as pallet_balances::Config>::ExistentialDeposit as Get<u128>>::get();
+
+        // Check total issuance before making evm call.
+        let total_issuance_before = Balances::total_issuance();
+
+        let bob_evm = H160::from_slice(&account_id("Bob").as_slice()[0..20]);
+        let hashed_bob_evm =
+            <Runtime as pallet_evm::Config>::AddressMapping::into_account_id(bob_evm);
+
+        // Send tokens to hashed_bob_evm to make call from bob_evm.
+        assert_ok!(Balances::transfer(
+            Some(account_id("Bob")).into(),
+            hashed_bob_evm.into(),
+            INIT_BALANCE - existential_deposit - 1,
+        ));
+
+        assert_ok!(EVM::call(
+            Some(account_id("Bob")).into(),
+            bob_evm,
+            H160::from_str("1000000000000000000000000000000000000001").unwrap(),
+            Vec::new(),
+            U256::from(1_000),
+            1000000,
+            U256::from(2_000_000_000),
+            Some(U256::from(1)),
+            Some(U256::from(0)),
+            Vec::new(),
+        ));
+
+        // Check total issuance after making evm call.
+        assert_eq!(Balances::total_issuance(), total_issuance_before);
     })
 }
