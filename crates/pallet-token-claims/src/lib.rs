7d8ba61f9ba9f94f6644d226e30536168389696c
MOZGIII <mike-n@narod.ru>
2022-07-26 18:50:15 +0400

    Add base pallet structure

--- crates/pallet-token-claims/src/lib.rs
+++ crates/pallet-token-claims/src/lib.rs
@@ -1,3 +1,28 @@
 //! Token claims.
 
 #![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::traits::StorageVersion;
+
+/// The current storage version.
+const STORAGE_VERSION: StorageVersion = StorageVersion::new(0);
+
+// We have to temporarily allow some clippy lints. Later on we'll send patches to substrate to
+// fix them at their end.
+#[allow(clippy::missing_docs_in_private_items)]
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    use super::*;
+
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    #[pallet::storage_version(STORAGE_VERSION)]
+    pub struct Pallet<T>(_);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {}
+}
+pub use pallet::*;
