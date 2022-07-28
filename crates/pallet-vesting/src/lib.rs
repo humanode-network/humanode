308c841c166c06bcc63d71662c4d3a8cee4eb7cc
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-07-28 17:16:01 +0300

    Initial pallet

--- crates/pallet-vesting/src/lib.rs
+++ crates/pallet-vesting/src/lib.rs
@@ -2,7 +2,9 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-mod vesting_info;
+mod moment;
+mod vesting_driver;
+mod vesting_driver_timestamp;
 mod vesting_schedule;
 
 use codec::MaxEncodedLen;
@@ -13,14 +15,11 @@ use frame_support::{
 };
 pub use pallet::*;
 use sp_runtime::traits::{AtLeast32BitUnsigned, MaybeSerializeDeserialize, Zero};
-use vesting_info::VestingInfo;
 use vesting_schedule::VestingSchedule;
 
 /// Balance type alias.
 type BalanceOf<T> =
     <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
-/// Full VestingInfo type.
-type FullVestingInfo<T> = VestingInfo<BalanceOf<T>, <T as Config>::Moment>;
 
 /// The current storage version.
 const STORAGE_VERSION: StorageVersion = StorageVersion::new(0);
@@ -62,9 +61,6 @@ pub mod pallet {
         /// The vesting schedule value itself.
         type VestinScheduleValue: Get<Self::VestingSchedule>;
 
-        /// The getter for the current moment.
-        type CurrentMoment: CurrentMoment<Self::Moment>;
-
         /// Maximum number of vesting schedules an account may have at a given moment.
         type MaxVestingSchedules: Get<u32>;
 
@@ -75,52 +71,4 @@ pub mod pallet {
     #[pallet::pallet]
     #[pallet::storage_version(STORAGE_VERSION)]
     pub struct Pallet<T>(_);
-
-    /// Information regarding the vesting of a given account.
-    #[pallet::storage]
-    #[pallet::getter(fn vesting)]
-    pub type Vesting<T: Config> = StorageMap<
-        _,
-        Blake2_128Concat,
-        T::AccountId,
-        BoundedVec<FullVestingInfo<T>, T::MaxVestingSchedules>,
-    >;
-
-    #[pallet::genesis_config]
-    pub struct GenesisConfig<T: Config> {
-        /// The list of vesting to use.
-        pub vesting: Vec<(T::AccountId, BalanceOf<T>, T::Moment)>,
-    }
-
-    #[cfg(feature = "std")]
-    impl<T: Config> Default for GenesisConfig<T> {
-        fn default() -> Self {
-            GenesisConfig {
-                vesting: Default::default(),
-            }
-        }
-    }
-
-    #[pallet::genesis_build]
-    impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
-        fn build(&self) {
-            for &(ref who, locked, start) in self.vesting.iter() {
-                let balance = T::Currency::free_balance(who);
-                assert!(
-                    !balance.is_zero(),
-                    "Currencies must be init'd before vesting"
-                );
-
-                VestingSchedule::validate(&T::VestinScheduleValue::get(), locked, start);
-
-                let vesting_info = VestingInfo { locked, start };
-
-                Vesting::<T>::try_append(who, vesting_info)
-                    .expect("Too many vesting schedules at genesis.");
-
-                let reasons = WithdrawReasons::TRANSFER | WithdrawReasons::RESERVE;
-                T::Currency::set_lock(T::VestingLockId::get(), who, locked, reasons);
-            }
-        }
-    }
 }
