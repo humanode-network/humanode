dd6caefa19061d02032812c24bf21b9bda400183
MOZGIII <mike-n@narod.ru>
2022-09-01 19:05:41 +0400

    Split the CreateInherentDataProviders for block import and production

--- crates/humanode-peer/src/service/inherents.rs
+++ crates/humanode-peer/src/service/inherents.rs
@@ -5,7 +5,7 @@ use sc_service::Arc;
 use sp_core::U256;
 use sp_runtime::traits::Block;
 
-/// Create inherent data providers.
+/// Create inherent data providers for block creation.
 #[derive(Debug)]
 pub struct Creator<Client> {
     /// Consensus slot duration.
@@ -16,8 +16,16 @@ pub struct Creator<Client> {
     pub client: Arc<Client>,
 }
 
+/// The inherents creator for block production.
+#[derive(Debug, Clone)]
+pub struct ForProduction<T>(pub T);
+
+/// The inherents creator for block import.
+pub struct ForImport<T>(pub T);
+
 #[async_trait::async_trait]
-impl<Client> sp_inherents::CreateInherentDataProviders<super::Block, ()> for Creator<Client>
+impl<Client> sp_inherents::CreateInherentDataProviders<super::Block, ()>
+    for ForProduction<Creator<Client>>
 where
     Client: Send + Sync,
     Client: ProvideUncles<super::Block>,
@@ -35,18 +43,56 @@ where
         _extra_args: (),
     ) -> Result<Self::InherentDataProviders, Box<dyn std::error::Error + Send + Sync>> {
         let uncles =
-            sc_consensus_uncles::create_uncles_inherent_data_provider(&*self.client, parent)?;
+            sc_consensus_uncles::create_uncles_inherent_data_provider(&*self.0.client, parent)?;
 
         let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
 
         let slot =
             sp_consensus_babe::inherents::InherentDataProvider::from_timestamp_and_slot_duration(
                 *timestamp,
-                self.raw_slot_duration,
+                self.0.raw_slot_duration,
             );
 
         let dynamic_fee =
-            pallet_dynamic_fee::InherentDataProvider(U256::from(self.eth_target_gas_price));
+            pallet_dynamic_fee::InherentDataProvider(U256::from(self.0.eth_target_gas_price));
+
+        Ok((timestamp, slot, uncles, dynamic_fee))
+    }
+}
+
+#[async_trait::async_trait]
+impl<Client> sp_inherents::CreateInherentDataProviders<super::Block, ()>
+    for ForImport<Creator<Client>>
+where
+    Client: Send + Sync,
+    Client: ProvideUncles<super::Block>,
+{
+    type InherentDataProviders = (
+        sp_timestamp::InherentDataProvider,
+        sp_consensus_babe::inherents::InherentDataProvider,
+        sp_authorship::InherentDataProvider<<super::Block as Block>::Header>,
+        pallet_dynamic_fee::InherentDataProvider,
+    );
+
+    async fn create_inherent_data_providers(
+        &self,
+        _parent: <super::Block as Block>::Hash,
+        _extra_args: (),
+    ) -> Result<Self::InherentDataProviders, Box<dyn std::error::Error + Send + Sync>> {
+        let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
+
+        let slot =
+            sp_consensus_babe::inherents::InherentDataProvider::from_timestamp_and_slot_duration(
+                *timestamp,
+                self.0.raw_slot_duration,
+            );
+
+        let uncles =
+            sp_authorship::InherentDataProvider::<<super::Block as Block>::Header>::check_inherents(
+            );
+
+        let dynamic_fee =
+            pallet_dynamic_fee::InherentDataProvider(U256::from(self.0.eth_target_gas_price));
 
         Ok((timestamp, slot, uncles, dynamic_fee))
     }
