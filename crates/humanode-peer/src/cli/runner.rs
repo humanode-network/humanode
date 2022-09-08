e163ed3b7b16c2efa0c03a2bf434e48208d762e9
MOZGIII <mike-n@narod.ru>
2022-09-08 19:35:23 +0400

    Set the default SS58 prefix to our chosen one in the chain code

--- crates/humanode-peer/src/cli/runner.rs
+++ crates/humanode-peer/src/cli/runner.rs
@@ -121,7 +121,7 @@ fn get_ss58_format(properties: &Properties) -> Result<u16> {
     let value = match properties.get("ss58Format") {
         Some(value) => value,
         // Use default 42 SS58Prefix if it's not set.
-        None => return Ok(42),
+        None => return Ok(crate::chain_spec::SS58_PREFIX),
     };
     let value = value
         .as_u64()
