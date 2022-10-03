fc113c6f111e82cc8d7d45325fa0d8bd0e53ea8d
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-10-03 12:34:41 +0300

    Proper message preparing for token claim

--- crates/eip191-token-claim/src/lib.rs
+++ crates/eip191-token-claim/src/lib.rs
@@ -16,10 +16,11 @@ impl<'a> Message<'a> {
     /// Prepare EIP-191 token claim message.
     pub fn prepare_message(&self) -> Vec<u8> {
         let mut buf = vec![];
-        buf.extend_from_slice("I hereby sign that I claim HMND to ".as_bytes());
-        buf.extend_from_slice(self.substrate_address);
-        buf.extend_from_slice(" on network with genesis ".as_bytes());
-        buf.extend_from_slice(self.genesis_hash);
+        buf.extend_from_slice("I hereby sign that I claim HMND to 0x".as_bytes());
+        buf.extend_from_slice(hex::encode(self.substrate_address).as_bytes());
+        buf.extend_from_slice(" on network with genesis 0x".as_bytes());
+        buf.extend_from_slice(hex::encode(self.genesis_hash).as_bytes());
+        buf.extend_from_slice(".".as_bytes());
         buf
     }
 }
