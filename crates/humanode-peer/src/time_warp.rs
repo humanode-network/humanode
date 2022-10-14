71f84fd32891aa5b5aa681388d21fb7131e46e7d
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-10-14 12:23:27 +0300

    Add docs to time warp

--- crates/humanode-peer/src/time_warp.rs
+++ crates/humanode-peer/src/time_warp.rs
@@ -1,9 +1,15 @@
+//! Time warp mode logic.
+
 use sp_timestamp::Timestamp;
 
+/// Time warp mode to simulate time acceleration.
 #[derive(Debug, Clone)]
 pub struct TimeWarp {
+    /// The time in the future when the warp is going to be started.
     pub revive_timestamp: Timestamp,
+    /// The time of the last block that was finalized before the chain bricked.
     pub fork_timestamp: Timestamp,
+    /// Warp factor that is going to be adopted.
     pub warp_factor: u64,
 }
 
@@ -25,6 +31,7 @@ impl TimeWarp {
     }
 }
 
+/// Get current timestamp.
 pub fn current_timestamp() -> Timestamp {
     sp_timestamp::InherentDataProvider::from_system_time().timestamp()
 }
