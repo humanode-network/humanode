752629c7bc19a5ca8453b244c43b7f35ebd8dd9e
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-12-02 19:18:15 +0300

    Fix cargo clippy

--- crates/pallet-vesting/src/weights.rs
+++ crates/pallet-vesting/src/weights.rs
@@ -4,10 +4,10 @@ use frame_support::dispatch::Weight;
 
 /// The weight information trait, to be implemented in from the benches.
 pub trait WeightInfo {
-    /// Weight for the unlock call.
+    /// Weight for the `unlock` call.
     fn unlock() -> Weight;
 
-    /// Weight for the update_schedule call.
+    /// Weight for the `update_schedule` call.
     fn update_schedule() -> Weight;
 }
 
