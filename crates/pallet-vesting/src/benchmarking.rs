d1b284cc3d880ac6cc8eead8241afa1cee3807ab
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-02-16 16:08:10 +0300

    Add update_schedule call for benchmarking at pallet-vesting

--- crates/pallet-vesting/src/benchmarking.rs
+++ crates/pallet-vesting/src/benchmarking.rs
@@ -74,6 +74,38 @@ benchmarks! {
         <CurrencyOf<T>>::settle(&account_id, imbalance, WithdrawReasons::RESERVE, ExistenceRequirement::AllowDeath).ok().unwrap();
     }
 
+    update_schedule {
+        let account_id = <T as Interface>::account_id();
+        let schedule = <T as Interface>::schedule();
+        let init_balance = <CurrencyOf<T>>::total_balance(&account_id);
+
+        let imbalance = <CurrencyOf<T>>::deposit_creating(&account_id, 1000u32.into());
+        assert_eq!(<CurrencyOf<T>>::free_balance(&account_id), init_balance + 1000u32.into());
+        assert!(<CurrencyOf<T>>::ensure_can_withdraw(&account_id, init_balance + 1000u32.into(), WithdrawReasons::empty(), 0u32.into()).is_ok());
+
+        let scheduling_driver = <T as super::Config>::SchedulingDriver::prepare_init();
+
+        <Pallet<T>>::lock_under_vesting(&account_id, schedule.clone())?;
+        assert_eq!(<CurrencyOf<T>>::free_balance(&account_id), init_balance + 1000u32.into());
+        assert!(<CurrencyOf<T>>::ensure_can_withdraw(&account_id, init_balance + 1000u32.into(), WithdrawReasons::empty(), 0u32.into()).is_err());
+
+        // Update vesting with full unlock.
+        let scheduling_driver = <T as super::Config>::SchedulingDriver::prepare_advance(scheduling_driver);
+
+        let origin = RawOrigin::Root;
+
+    }: _(origin, account_id.clone(), schedule)
+    verify {
+        assert_eq!(Schedules::<T>::get(&account_id), None);
+        assert_eq!(<CurrencyOf<T>>::free_balance(&account_id), init_balance + 1000u32.into());
+        assert!(<CurrencyOf<T>>::ensure_can_withdraw(&account_id, init_balance + 1000u32.into(), WithdrawReasons::empty(), 0u32.into()).is_ok());
+
+        assert_ok!(<T as super::Config>::SchedulingDriver::verify(scheduling_driver));
+
+        // Clean up imbalance after ourselves.
+        <CurrencyOf<T>>::settle(&account_id, imbalance, WithdrawReasons::RESERVE, ExistenceRequirement::AllowDeath).ok().unwrap();
+    }
+
     impl_benchmark_test_suite!(
         Pallet,
         crate::mock::new_test_ext(),
