bfb4afe25aaa878702414656b7cab681ab4cb968
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-02-28 13:01:39 +0200

    Store generated weights

---
+++ crates/humanode-runtime/src/weights/mod.rs
@@ -0,0 +1,13 @@
+pub mod frame_benchmarking;
+pub mod frame_system;
+pub mod pallet_babe;
+pub mod pallet_balances;
+pub mod pallet_bioauth;
+pub mod pallet_evm_accounts_mapping;
+pub mod pallet_grandpa;
+pub mod pallet_im_online;
+pub mod pallet_multisig;
+pub mod pallet_timestamp;
+pub mod pallet_token_claims;
+pub mod pallet_utility;
+pub mod pallet_vesting;
