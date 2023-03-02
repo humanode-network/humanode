e778a40db45ece30bb83417996e3cc555447ad0e
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-03-02 12:27:03 +0300

    Use internal weights for pallet-babe and pallet-grandpa

--- crates/humanode-runtime/src/weights/mod.rs
+++ crates/humanode-runtime/src/weights/mod.rs
@@ -1,9 +1,7 @@
 pub mod frame_system;
-pub mod pallet_babe;
 pub mod pallet_balances;
 pub mod pallet_bioauth;
 pub mod pallet_evm_accounts_mapping;
-pub mod pallet_grandpa;
 pub mod pallet_im_online;
 pub mod pallet_multisig;
 pub mod pallet_timestamp;
