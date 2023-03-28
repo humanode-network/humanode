f4f05a9b57fe7fcedc8f53cbc1cb6670033c79ff
MOZGIII <mike-n@narod.ru>
2023-03-29 00:22:00 +0400

    Cleanup

--- crates/humanode-runtime/src/tests/fees.rs
+++ crates/humanode-runtime/src/tests/fees.rs
@@ -98,10 +98,8 @@ fn assert_fee(call: RuntimeCall, len: u32, expected_fee: Balance, epsilon: Balan
 /// The testing cryptography to match the real one we use for the accounts.
 /// We use it to simulate the signatures in the test to estimate the tx size.
 pub mod crypto {
-    use sp_core::sr25519::Signature as Sr25519Signature;
     use sp_runtime::{
         app_crypto::{app_crypto, sr25519},
-        traits::Verify,
         KeyTypeId, MultiSignature, MultiSigner,
     };
 
@@ -111,15 +109,6 @@ pub mod crypto {
     pub struct TestAuthId;
 
     impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
-        type RuntimeAppPublic = Public;
-        type GenericSignature = sp_core::sr25519::Signature;
-        type GenericPublic = sp_core::sr25519::Public;
-    }
-
-    // implemented for mock runtime in test
-    impl frame_system::offchain::AppCrypto<<Sr25519Signature as Verify>::Signer, Sr25519Signature>
-        for TestAuthId
-    {
         type RuntimeAppPublic = Public;
         type GenericSignature = sp_core::sr25519::Signature;
         type GenericPublic = sp_core::sr25519::Public;
