4289f007e9cd639c3bdeb840642f16b6932f4af8
MOZGIII <mike-n@narod.ru>
2023-04-10 13:32:18 -0300

    Update to vergen 8 (#640)

--- crates/humanode-peer/build.rs
+++ crates/humanode-peer/build.rs
@@ -1,11 +1,11 @@
 //! Node build script.
 
-use vergen::{vergen, Config};
-
 fn main() {
-    let mut cfg = Config::default();
-    *cfg.git_mut().branch_mut() = false;
-    *cfg.git_mut().commit_timestamp_mut() = false;
-    *cfg.git_mut().semver_mut() = false;
-    vergen(cfg).unwrap();
+    vergen::EmitBuilder::builder()
+        .fail_on_error()
+        .git_sha(false)
+        .cargo_features()
+        .cargo_debug()
+        .emit()
+        .unwrap();
 }
