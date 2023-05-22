d4a837504860961af407e17e9ed1d34afb2f98a5
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-05-22 18:25:27 +0200

    Fix EvmTransactionCharger with account id from account provider

--- crates/humanode-runtime/src/fixed_supply.rs
+++ crates/humanode-runtime/src/fixed_supply.rs
@@ -8,45 +8,49 @@ use sp_runtime::traits::UniqueSaturatedInto;
 
 use super::*;
 
-// /// The corrected implementation of the [`pallet_evm::EVMCurrencyAdapter`].
-// pub struct EvmTransactionCharger<C, OU>(PhantomData<(C, OU)>);
+/// The corrected implementation of the [`pallet_evm::EVMCurrencyAdapter`].
+pub struct EvmTransactionCharger<C, OU>(PhantomData<(C, OU)>);
 
-// impl<T, C, OU> pallet_evm::OnChargeEVMTransaction<T> for EvmTransactionCharger<C, OU>
-// where
-//     T: pallet_evm::Config,
-//     C: Currency<
-//         <<T as pallet_evm::Config>::AccountProvider as pallet_evm::AccountProvider>::AccountId,
-//     >,
-//     OU: OnUnbalanced<
-//         <C as Currency<
-//             <<T as pallet_evm::Config>::AccountProvider as pallet_evm::AccountProvider>::AccountId,
-//         >>::NegativeImbalance,
-//     >,
-//     U256: UniqueSaturatedInto<<C as Currency<<T as frame_system::Config>::AccountId>>::Balance>,
-// {
-//     type LiquidityInfo = Option<
-//         <C as Currency<
-//             <<T as pallet_evm::Config>::AccountProvider as pallet_evm::AccountProvider>::AccountId,
-//         >>::NegativeImbalance,
-//     >;
+impl<T, C, OU> pallet_evm::OnChargeEVMTransaction<T> for EvmTransactionCharger<C, OU>
+where
+    T: pallet_evm::Config,
+    C: Currency<
+        <<T as pallet_evm::Config>::AccountProvider as pallet_evm::AccountProvider>::AccountId,
+    >,
+    OU: OnUnbalanced<
+        <C as Currency<
+            <<T as pallet_evm::Config>::AccountProvider as pallet_evm::AccountProvider>::AccountId,
+        >>::NegativeImbalance,
+    >,
+    U256: UniqueSaturatedInto<
+        <C as Currency<
+            <<T as pallet_evm::Config>::AccountProvider as pallet_evm::AccountProvider>::AccountId,
+        >>::Balance,
+    >,
+{
+    type LiquidityInfo = Option<
+        <C as Currency<
+            <<T as pallet_evm::Config>::AccountProvider as pallet_evm::AccountProvider>::AccountId,
+        >>::NegativeImbalance,
+    >;
 
-//     fn withdraw_fee(who: &H160, fee: U256) -> Result<Self::LiquidityInfo, pallet_evm::Error<T>> {
-//         <pallet_evm::EVMCurrencyAdapter<C, OU> as pallet_evm::OnChargeEVMTransaction<T>>::withdraw_fee(who, fee)
-//     }
+    fn withdraw_fee(who: &H160, fee: U256) -> Result<Self::LiquidityInfo, pallet_evm::Error<T>> {
+        <pallet_evm::EVMCurrencyAdapter<C, OU> as pallet_evm::OnChargeEVMTransaction<T>>::withdraw_fee(who, fee)
+    }
 
-//     fn correct_and_deposit_fee(
-//         who: &H160,
-//         corrected_fee: U256,
-//         base_fee: U256,
-//         already_withdrawn: Self::LiquidityInfo,
-//     ) -> Self::LiquidityInfo {
-//         <pallet_evm::EVMCurrencyAdapter<C, OU> as pallet_evm::OnChargeEVMTransaction<T>>::correct_and_deposit_fee(who, corrected_fee, base_fee, already_withdrawn)
-//     }
+    fn correct_and_deposit_fee(
+        who: &H160,
+        corrected_fee: U256,
+        base_fee: U256,
+        already_withdrawn: Self::LiquidityInfo,
+    ) -> Self::LiquidityInfo {
+        <pallet_evm::EVMCurrencyAdapter<C, OU> as pallet_evm::OnChargeEVMTransaction<T>>::correct_and_deposit_fee(who, corrected_fee, base_fee, already_withdrawn)
+    }
 
-//     fn pay_priority_fee(tip: Self::LiquidityInfo) {
-//         if let Some(tip) = tip {
-//             // Handle the tips in the same manner as the regular fee.
-//             OU::on_unbalanced(tip);
-//         }
-//     }
-// }
+    fn pay_priority_fee(tip: Self::LiquidityInfo) {
+        if let Some(tip) = tip {
+            // Handle the tips in the same manner as the regular fee.
+            OU::on_unbalanced(tip);
+        }
+    }
+}
