74732e9222999312b603d96ed4fca0d6694d5fbe
Dmitry Lavrenov <39522748+dmitrylavrenov@users.noreply.github.com>
2023-06-30 00:33:53 +0300

    Introduce currency swap related primitives (#680)
    * Introduce currency swap related primitives
    
    * Edit docs for From type.
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>
    
    * Edit docs for To type
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>
    
    ---------
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>

---
+++ crates/primitives-currency-swap/src/lib.rs
@@ -0,0 +1,23 @@
+//! Currency swap related primitives.
+
+// Either generate code at stadard mode, or `no_std`, based on the `std` feature presence.
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::{sp_runtime::DispatchError, traits::Currency};
+
+/// Currency swap interface.
+pub trait CurrencySwap<AccountIdFrom, AccountIdTo> {
+    /// The currency to convert from.
+    type From: Currency<AccountIdFrom>;
+
+    /// The currency to convert to.
+    type To: Currency<AccountIdTo>;
+
+    /// A possible error happens during the actual swap logic.
+    type Error: Into<DispatchError>;
+
+    /// The actual swap logic.
+    fn swap(
+        imbalance: <Self::From as Currency<AccountIdFrom>>::NegativeImbalance,
+    ) -> Result<<Self::To as Currency<AccountIdTo>>::NegativeImbalance, Self::Error>;
+}
