8b2d3f12b471d42f5d452b2135320ec7a2958763
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-06-29 20:33:48 +0300

    Add docs for pallet

--- crates/pallet-currency-swap/src/lib.rs
+++ crates/pallet-currency-swap/src/lib.rs
@@ -2,6 +2,7 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use frame_support::traits::Currency;
 pub use pallet::*;
 pub use weights::*;
 
@@ -14,12 +15,22 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+/// The currency type related to `From` of `CurrencySwap` interface.
+type CurrencyFromOf<T> = <<T as Config>::CurrencySwap as traits::CurrencySwap<
+    <T as frame_system::Config>::AccountId,
+    <T as Config>::AccountIdTo,
+>>::From;
+
+/// The balance type related to `CurrencyFromOf`.
+type BalanceFromOf<T> =
+    <CurrencyFromOf<T> as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
 // We have to temporarily allow some clippy lints. Later on we'll send patches to substrate to
 // fix them at their end.
 #[allow(clippy::missing_docs_in_private_items)]
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::{pallet_prelude::*, traits::Currency};
+    use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
     use sp_runtime::traits::MaybeDisplay;
     use sp_std::fmt::Debug;
@@ -34,6 +45,7 @@ pub mod pallet {
     /// Configuration trait of this pallet.
     #[pallet::config]
     pub trait Config: frame_system::Config {
+        /// The user account identifier type balances send to.
         type AccountIdTo: Parameter
             + Member
             + MaybeSerializeDeserialize
@@ -42,19 +54,22 @@ pub mod pallet {
             + Ord
             + MaxEncodedLen;
 
+        /// Interface into currency swap implementation.
         type CurrencySwap: CurrencySwap<Self::AccountId, Self::AccountIdTo>;
 
+        /// Weight information for extrinsics in this pallet.
         type WeightInfo: WeightInfo;
     }
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
+        /// Swap balances.
         #[pallet::call_index(0)]
         #[pallet::weight(T::WeightInfo::swap())]
         pub fn swap(
             origin: OriginFor<T>,
             account_id_to: T::AccountIdTo,
-            balance: <<T::CurrencySwap as CurrencySwap<T::AccountId, T::AccountIdTo>>::From as Currency<T::AccountId>>::Balance,
+            balance: BalanceFromOf<T>,
         ) -> DispatchResult {
             let account_id_from = ensure_signed(origin)?;
 
