cb72f9704e4dfbdc16a6a9cd19e002ae93365be0
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-06-29 20:09:23 +0300

    Add swap_fails test

--- crates/pallet-currency-swap/src/tests.rs
+++ crates/pallet-currency-swap/src/tests.rs
@@ -1,13 +1,12 @@
 //! The tests for the pallet.
 
-use frame_support::{
-    assert_noop, assert_ok, assert_storage_noop, sp_runtime::DispatchError, traits::Currency,
-};
+use frame_support::{assert_noop, assert_ok, traits::Currency};
 use sp_core::H160;
 use sp_std::str::FromStr;
 
-use crate::{mock::*, *};
+use crate::mock::*;
 
+/// This test verifies that swap call works in the happy path.
 #[test]
 fn swap_works() {
     new_test_ext().execute_with_ext(|_| {
@@ -43,6 +42,26 @@ fn swap_works() {
         assert_eq!(
             EvmBalances::total_issuance(),
             evm_balances_before + swap_balance
+        );
+    });
+}
+
+/// This test verifies that swap call fails in case some error happens during the actual swap logic.
+#[test]
+fn swap_fails() {
+    new_test_ext().execute_with_ext(|_| {
+        let alice = 42;
+        let alice_evm = H160::from_str("1000000000000000000000000000000000000001").unwrap();
+        let alice_balance = 1000;
+        let swap_balance = 10000;
+
+        // Prepare the test state.
+        Balances::make_free_balance_be(&alice, alice_balance);
+
+        // Invoke the function under test.
+        assert_noop!(
+            CurrencySwap::swap(RuntimeOrigin::signed(alice), alice_evm, swap_balance),
+            pallet_balances::Error::<Test>::InsufficientBalance
         );
     });
 }
