07d3fd1a4936ca2de13c34f1026ae5800f9120a5
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-06-30 16:43:23 +0300

    Implement swap logic

--- crates/bridge-pot-currency-swap/src/lib.rs
+++ crates/bridge-pot-currency-swap/src/lib.rs
@@ -3,22 +3,41 @@
 // Either generate code at stadard mode, or `no_std`, based on the `std` feature presence.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{traits::Currency, sp_runtime::DispatchError};
+use frame_support::{
+    sp_runtime::DispatchError,
+    traits::{Currency, ExistenceRequirement, Get, Imbalance, WithdrawReasons},
+};
 use primitives_currency_swap::CurrencySwap;
 use sp_std::marker::PhantomData;
 
-pub struct OneToOne<AccountIdFrom, AccountIdTo, CurrencyFrom, CurrencyTo>(
-    PhantomData<(AccountIdFrom, AccountIdTo, CurrencyFrom, CurrencyTo)>,
+pub trait Config<AccountIdFrom, AccountIdTo> {
+    type PotFrom: Get<AccountIdFrom>;
+
+    type PotTo: Get<AccountIdTo>;
+}
+
+pub struct OneToOne<AccountIdFrom, AccountIdTo, ConfigT, CurrencyFrom, CurrencyTo>(
+    PhantomData<(
+        AccountIdFrom,
+        AccountIdTo,
+        ConfigT,
+        CurrencyFrom,
+        CurrencyTo,
+    )>,
 )
 where
+    ConfigT: Config<AccountIdFrom, AccountIdTo>,
     CurrencyFrom: Currency<AccountIdFrom>,
     CurrencyTo: Currency<AccountIdTo>;
 
-impl<AccountIdFrom, AccountIdTo, CurrencyFrom, CurrencyTo> CurrencySwap<AccountIdFrom, AccountIdTo>
-    for OneToOne<AccountIdFrom, AccountIdTo, CurrencyFrom, CurrencyTo>
+impl<AccountIdFrom, AccountIdTo, ConfigT, CurrencyFrom, CurrencyTo>
+    CurrencySwap<AccountIdFrom, AccountIdTo>
+    for OneToOne<AccountIdFrom, AccountIdTo, ConfigT, CurrencyFrom, CurrencyTo>
 where
+    ConfigT: Config<AccountIdFrom, AccountIdTo>,
     CurrencyFrom: Currency<AccountIdFrom>,
     CurrencyTo: Currency<AccountIdTo>,
+    CurrencyTo::Balance: From<CurrencyFrom::Balance>,
 {
     type From = CurrencyFrom;
     type To = CurrencyTo;
@@ -30,6 +49,17 @@ where
         <Self::To as frame_support::traits::Currency<AccountIdTo>>::NegativeImbalance,
         Self::Error,
     > {
-        todo!()
+        let amount = imbalance.peek();
+
+        CurrencyFrom::resolve_creating(&ConfigT::PotFrom::get(), imbalance);
+
+        let imbalance = CurrencyTo::withdraw(
+            &ConfigT::PotTo::get(),
+            amount.into(),
+            WithdrawReasons::TRANSFER,
+            ExistenceRequirement::AllowDeath,
+        )?;
+
+        Ok(imbalance)
     }
 }
