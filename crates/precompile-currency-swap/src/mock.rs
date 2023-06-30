9c5cfb64322639b702f5ba23bb037a4954af951e
MOZGIII <mike-n@narod.ru>
2023-06-29 21:36:27 -0300

    wip

--- crates/precompile-currency-swap/src/mock.rs
+++ crates/precompile-currency-swap/src/mock.rs
@@ -1,23 +1,28 @@
+//! The mock for the precompile.
+
 // Allow simple integer arithmetic in tests.
 #![allow(clippy::integer_arithmetic)]
 
 use fp_evm::{Context, ExitError, ExitReason, PrecompileHandle, Transfer};
-use frame_support::traits::{ConstU16, ConstU64};
-use frame_system as system;
-use mockall::predicate::*;
-use mockall::*;
-use sp_core::H256;
-use sp_runtime::{
-    testing::Header,
-    traits::{BlakeTwo256, IdentityLookup},
-    BuildStorage,
+use frame_support::{
+    sp_io,
+    sp_runtime::{
+        testing::Header,
+        traits::{BlakeTwo256, IdentityLookup},
+        BuildStorage, DispatchError,
+    },
+    traits::{ConstU64, Currency},
 };
+use frame_system as system;
+use mockall::{mock, predicate::*, *};
+use sp_core::{H160, H256, U256};
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
-/// An index to a block.
-pub type BlockNumber = u64;
+pub(crate) type AccountId = u64;
+pub(crate) type EvmAccountId = H160;
+pub(crate) type Balance = u64;
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
@@ -26,7 +31,12 @@ frame_support::construct_runtime!(
         NodeBlock = Block,
         UncheckedExtrinsic = UncheckedExtrinsic,
     {
-        System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+        System: frame_system,
+        Timestamp: pallet_timestamp,
+        Balances: pallet_balances,
+        EvmSystem: pallet_evm_system,
+        EvmBalances: pallet_evm_balances,
+        EVM: pallet_evm,
     }
 );
 
@@ -57,7 +67,103 @@ impl system::Config for Test {
     type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
+frame_support::parameter_types! {
+    pub const MinimumPeriod: u64 = 1000;
+}
+impl pallet_timestamp::Config for Test {
+    type Moment = u64;
+    type OnTimestampSet = ();
+    type MinimumPeriod = MinimumPeriod;
+    type WeightInfo = ();
+}
+
+impl pallet_balances::Config for Test {
+    type Balance = u64;
+    type RuntimeEvent = RuntimeEvent;
+    type DustRemoval = ();
+    type ExistentialDeposit = ConstU64<1>;
+    type AccountStore = System;
+    type MaxLocks = ();
+    type MaxReserves = ();
+    type ReserveIdentifier = [u8; 8];
+    type WeightInfo = ();
+}
+
+impl pallet_evm_system::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type AccountId = EvmAccountId;
+    type Index = u64;
+    type AccountData = pallet_evm_balances::AccountData<Balance>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+}
+
+impl pallet_evm_balances::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type AccountId = EvmAccountId;
+    type Balance = Balance;
+    type ExistentialDeposit = ConstU64<1>;
+    type AccountStore = EvmSystem;
+    type DustRemoval = ();
+}
+
+pub struct FixedGasPrice;
+impl fp_evm::FeeCalculator for FixedGasPrice {
+    fn min_gas_price() -> (U256, Weight) {
+        // Return some meaningful gas price and weight
+        (1_000_000_000u128.into(), Weight::from_ref_time(7u64))
+    }
+}
+
+frame_support::parameter_types! {
+    pub BlockGasLimit: U256 = U256::max_value();
+    pub WeightPerGas: Weight = Weight::from_ref_time(20_000);
+    pub MockPrecompiles: MockPrecompileSet = MockPrecompileSet;
+}
+
+impl pallet_evm::Config for Test {
+    type AccountProvider = pallet_evm::NativeSystemAccountProvider<Self>;
+    type FeeCalculator = FixedGasPrice;
+    type GasWeightMapping = pallet_evm::FixedGasWeightMapping<Self>;
+    type WeightPerGas = WeightPerGas;
+
+    type BlockHashMapping = pallet_evm::SubstrateBlockHashMapping<Self>;
+    type CallOrigin = pallet_evm::EnsureAddressNever<
+        <Self::AccountProvider as pallet_evm::AccountProvider>::AccountId,
+    >;
+    type WithdrawOrigin = pallet_evm::EnsureAddressNever<
+        <Self::AccountProvider as pallet_evm::AccountProvider>::AccountId,
+    >;
+    type AddressMapping = pallet_evm::IdentityAddressMapping;
+    type Currency = Balances;
+
+    type RuntimeEvent = RuntimeEvent;
+    type PrecompilesType = MockPrecompileSet;
+    type PrecompilesValue = MockPrecompiles;
+    type ChainId = ();
+    type BlockGasLimit = BlockGasLimit;
+    type Runner = pallet_evm::runner::stack::Runner<Self>;
+    type OnChargeTransaction = ();
+    type OnCreate = ();
+    type FindAuthor = ();
+}
+
 mock! {
+    #[derive(Debug)]
+    pub CurrencySwap {}
+    impl primitives_currency_swap::CurrencySwap<AccountId, EvmAccountId> for CurrencySwap {
+        type From = Balances;
+        type To = EvmBalances;
+        type Error = DispatchError;
+
+        fn swap(
+            imbalance: <Balances as Currency<AccountId>>::NegativeImbalance,
+        ) -> Result<<EvmBalances as Currency<EvmAccountId>>::NegativeImbalance, DispatchError>;
+    }
+}
+
+mock! {
+    #[derive(Debug)]
     pub PrecompileHandle {}
 
     impl PrecompileHandle for PrecompileHandle {
@@ -89,14 +195,43 @@ mock! {
     }
 }
 
-/// Build test externalities from the default genesis.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    // Build genesis.
-    let config = GenesisConfig {
-        ..Default::default()
-    };
-    let storage = config.build_storage().unwrap();
+    let genesis_config = GenesisConfig::default();
+    new_test_ext_with(genesis_config)
+}
 
-    // Make test externalities from the storage.
+// This function basically just builds a genesis storage key/value store according to
+// our desired mockup.
+pub fn new_test_ext_with(genesis_config: GenesisConfig) -> sp_io::TestExternalities {
+    let storage = genesis_config.build_storage().unwrap();
     storage.into()
+}
+
+pub fn runtime_lock() -> std::sync::MutexGuard<'static, ()> {
+    static MOCK_RUNTIME_MUTEX: std::sync::Mutex<()> = std::sync::Mutex::new(());
+
+    // Ignore the poisoning for the tests that panic.
+    // We only care about concurrency here, not about the poisoning.
+    match MOCK_RUNTIME_MUTEX.lock() {
+        Ok(guard) => guard,
+        Err(poisoned) => poisoned.into_inner(),
+    }
+}
+
+pub trait TestExternalitiesExt {
+    fn execute_with_ext<R, E>(&mut self, execute: E) -> R
+    where
+        E: for<'e> FnOnce(&'e ()) -> R;
+}
+
+impl TestExternalitiesExt for frame_support::sp_io::TestExternalities {
+    fn execute_with_ext<R, E>(&mut self, execute: E) -> R
+    where
+        E: for<'e> FnOnce(&'e ()) -> R,
+    {
+        let guard = runtime_lock();
+        let result = self.execute_with(|| execute(&guard));
+        drop(guard);
+        result
+    }
 }
--- crates/precompile-currency-swap/src/tests.rs
+++ crates/precompile-currency-swap/src/tests.rs
@@ -3,3 +3,92 @@ use pallet_evm::ExitSucceed;
 use precompile_utils::{Bytes, EvmDataWriter};
 
 use crate::{mock::*, *};
+
+/// This test verifies that swap call works in the happy path.
+#[test]
+fn swap_works() {
+    new_test_ext().execute_with_ext(|_| {
+        let alice = 42;
+        let alice_evm = H160::from_str("1000000000000000000000000000000000000001").unwrap();
+        let alice_balance = 1000;
+        let swap_balance = 100;
+
+        // Prepare the test state.
+        Balances::make_free_balance_be(&alice, alice_balance);
+
+        // Check test preconditions.
+        assert_eq!(Balances::total_balance(&alice), alice_balance);
+        assert_eq!(EvmBalances::total_balance(&alice_evm), 0);
+
+        // Set block number to enable events.
+        System::set_block_number(1);
+
+        // Set mock expectations.
+        let swap_ctx = MockCurrencySwap::swap_context();
+        swap_ctx
+            .expect()
+            .once()
+            .with(predicate::eq(
+                <Balances as Currency<AccountId>>::NegativeImbalance::new(swap_balance),
+            ))
+            .return_once(move |_| {
+                Ok(<EvmBalances as Currency<EvmAccountId>>::NegativeImbalance::new(swap_balance))
+            });
+
+        // Invoke the function under test.
+        assert_ok!(CurrencySwap::swap(
+            RuntimeOrigin::signed(alice),
+            alice_evm,
+            swap_balance
+        ));
+
+        // Assert state changes.
+        assert_eq!(
+            Balances::total_balance(&alice),
+            alice_balance - swap_balance
+        );
+        assert_eq!(EvmBalances::total_balance(&alice_evm), swap_balance);
+        System::assert_has_event(RuntimeEvent::CurrencySwap(Event::BalancesSwapped {
+            from: alice,
+            withdrawed_amount: swap_balance,
+            to: alice_evm,
+            deposited_amount: swap_balance,
+        }));
+
+        // Assert mock invocations.
+        swap_ctx.checkpoint();
+    });
+}
+
+/// This test verifies that swap call fails in case some error happens during the actual swap logic.
+#[test]
+fn swap_fails() {
+    new_test_ext().execute_with_ext(|_| {
+        let alice = 42;
+        let alice_evm = H160::from_str("1000000000000000000000000000000000000001").unwrap();
+        let alice_balance = 1000;
+        let swap_balance = 100;
+
+        // Prepare the test state.
+        Balances::make_free_balance_be(&alice, alice_balance);
+
+        // Set mock expectations.
+        let swap_ctx = MockCurrencySwap::swap_context();
+        swap_ctx
+            .expect()
+            .once()
+            .with(predicate::eq(
+                <Balances as Currency<u64>>::NegativeImbalance::new(swap_balance),
+            ))
+            .return_once(move |_| Err(DispatchError::Other("currency swap failed")));
+
+        // Invoke the function under test.
+        assert_noop!(
+            CurrencySwap::swap(RuntimeOrigin::signed(alice), alice_evm, swap_balance),
+            DispatchError::Other("currency swap failed")
+        );
+
+        // Assert mock invocations.
+        swap_ctx.checkpoint();
+    });
+}
