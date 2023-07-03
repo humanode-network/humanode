57febbf984c93a6f0008c88d665df617c9349ea6
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-07-03 12:01:02 +0300

    Implement configs for NativeToEvmOneToOne and EvmToNativeOneToOne

---
+++ crates/humanode-runtime/src/currency_swap.rs
@@ -0,0 +1,39 @@
+use bridge_pot_currency_swap::{CurrencySwap, ExistenceRequired};
+use sp_runtime::traits::Identity;
+
+use crate::{
+    parameter_types, AccountId, Balances, BalancesPot, EvmAccountId, EvmBalances, EvmBalancesPot,
+};
+
+parameter_types! {
+    pub BalancesPotAccountId: AccountId = BalancesPot::account_id();
+    pub EvmBalancesPotAccountId: EvmAccountId = EvmBalancesPot::account_id();
+}
+
+pub type NativeToEvmOneToOne = CurrencySwap<NativeToEvmOneToOneConfig, ExistenceRequired>;
+
+pub struct NativeToEvmOneToOneConfig;
+
+impl bridge_pot_currency_swap::Config for NativeToEvmOneToOneConfig {
+    type AccountIdFrom = AccountId;
+    type AccountIdTo = EvmAccountId;
+    type CurrencyFrom = Balances;
+    type CurrencyTo = EvmBalances;
+    type BalanceCoverter = Identity;
+    type PotFrom = BalancesPotAccountId;
+    type PotTo = EvmBalancesPotAccountId;
+}
+
+pub type EvmToNativeOneToOne = CurrencySwap<EvmToNativeOneToOneConfig, ExistenceRequired>;
+
+pub struct EvmToNativeOneToOneConfig;
+
+impl bridge_pot_currency_swap::Config for EvmToNativeOneToOneConfig {
+    type AccountIdFrom = EvmAccountId;
+    type AccountIdTo = AccountId;
+    type CurrencyFrom = EvmBalances;
+    type CurrencyTo = Balances;
+    type BalanceCoverter = Identity;
+    type PotFrom = EvmBalancesPotAccountId;
+    type PotTo = BalancesPotAccountId;
+}
