e1e2de41df83af1cdf1027d3bf4ac75027fda0d1
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-07-04 16:22:41 +0300

    Add estimated swapped balances logic into CurrencySwap interface

--- crates/primitives-currency-swap/src/lib.rs
+++ crates/primitives-currency-swap/src/lib.rs
@@ -3,15 +3,20 @@
 // Either generate code at stadard mode, or `no_std`, based on the `std` feature presence.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{sp_runtime::DispatchError, traits::Currency};
+use frame_support::{
+    sp_runtime::DispatchError,
+    traits::{fungible::Inspect, Currency},
+};
 
 /// Currency swap interface.
 pub trait CurrencySwap<AccountIdFrom, AccountIdTo> {
     /// The currency to convert from.
-    type From: Currency<AccountIdFrom>;
+    type From: Currency<AccountIdFrom>
+        + Inspect<AccountIdFrom, Balance = <Self::From as Currency<AccountIdFrom>>::Balance>;
 
     /// The currency to convert to.
-    type To: Currency<AccountIdTo>;
+    type To: Currency<AccountIdTo>
+        + Inspect<AccountIdTo, Balance = <Self::To as Currency<AccountIdTo>>::Balance>;
 
     /// A possible error happens during the actual swap logic.
     type Error: Into<DispatchError>;
@@ -23,13 +28,26 @@ pub trait CurrencySwap<AccountIdFrom, AccountIdTo> {
         ToNegativeImbalanceFor<Self, AccountIdFrom, AccountIdTo>,
         ErrorFor<Self, AccountIdFrom, AccountIdTo>,
     >;
+
+    /// Obtain the estimated resulted balance value.
+    fn estimate_swapped_balance(
+        balance: FromBalanceFor<Self, AccountIdFrom, AccountIdTo>,
+    ) -> ToBalanceFor<Self, AccountIdFrom, AccountIdTo>;
 }
+
+/// An easy way to access the [`Currency::Balance`] of [`CurrencySwap::From`] of `T`.
+pub type FromBalanceFor<T, AccountIdFrom, AccountIdTo> =
+    <<T as CurrencySwap<AccountIdFrom, AccountIdTo>>::From as Currency<AccountIdFrom>>::Balance;
 
 /// An easy way to access the [`Currency::NegativeImbalance`] of [`CurrencySwap::From`] of `T`.
 pub type FromNegativeImbalanceFor<T, AccountIdFrom, AccountIdTo> = <<T as CurrencySwap<
     AccountIdFrom,
     AccountIdTo,
 >>::From as Currency<AccountIdFrom>>::NegativeImbalance;
+
+/// An easy way to access the [`Currency::Balance`] of [`CurrencySwap::To`] of `T`.
+pub type ToBalanceFor<T, AccountIdFrom, AccountIdTo> =
+    <<T as CurrencySwap<AccountIdFrom, AccountIdTo>>::To as Currency<AccountIdTo>>::Balance;
 
 /// An easy way to access the [`Currency::NegativeImbalance`] of [`CurrencySwap::To`] of `T`.
 pub type ToNegativeImbalanceFor<T, AccountIdFrom, AccountIdTo> = <<T as CurrencySwap<
