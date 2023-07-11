54aa97c217dc9fc6fd15f9dcb2a94a0932202a5b
MOZGIII <mike-n@narod.ru>
2023-07-11 19:59:04 -0300

    Update the primitive

--- crates/primitives-currency-swap/src/lib.rs
+++ crates/primitives-currency-swap/src/lib.rs
@@ -1,45 +1,41 @@
-//! Currency swap related primitives.
+//! Currency (as in [`fungible::Balanced`]) swap related primitives.
 
 // Either generate code at stadard mode, or `no_std`, based on the `std` feature presence.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{sp_runtime::DispatchError, traits::Currency};
+use frame_support::{sp_runtime::DispatchError, traits::fungible};
 
 /// Currency swap interface.
 pub trait CurrencySwap<AccountIdFrom, AccountIdTo> {
     /// The currency to convert from.
-    type From: Currency<AccountIdFrom>;
+    type From: fungible::Balanced<AccountIdFrom>;
 
     /// The currency to convert to.
-    type To: Currency<AccountIdTo>;
+    type To: fungible::Balanced<AccountIdTo>;
 
     /// A possible error happens during the actual swap logic.
     type Error: Into<DispatchError>;
 
     /// The actual swap logic.
     fn swap(
-        imbalance: FromNegativeImbalanceFor<Self, AccountIdFrom, AccountIdTo>,
+        credit: FromCreditOf<Self, AccountIdFrom, AccountIdTo>,
     ) -> Result<
-        ToNegativeImbalanceFor<Self, AccountIdFrom, AccountIdTo>,
+        ToCreditOf<Self, AccountIdFrom, AccountIdTo>,
         ErrorFor<Self, AccountIdFrom, AccountIdTo>,
     >;
 }
 
-/// An easy way to access the [`Currency::NegativeImbalance`] of [`CurrencySwap::From`] of `T`.
-pub type FromNegativeImbalanceFor<T, AccountIdFrom, AccountIdTo> = <<T as CurrencySwap<
-    AccountIdFrom,
-    AccountIdTo,
->>::From as Currency<AccountIdFrom>>::NegativeImbalance;
+/// An easy way to access the [`fungible::CreditOf`] of [`CurrencySwap::From`] of `T`.
+pub type FromCreditOf<T, AccountIdFrom, AccountIdTo> =
+    fungible::CreditOf<AccountIdFrom, <T as CurrencySwap<AccountIdFrom, AccountIdTo>>::From>;
 
-/// An easy way to access the [`Currency::NegativeImbalance`] of [`CurrencySwap::To`] of `T`.
-pub type ToNegativeImbalanceFor<T, AccountIdFrom, AccountIdTo> = <<T as CurrencySwap<
-    AccountIdFrom,
-    AccountIdTo,
->>::To as Currency<AccountIdTo>>::NegativeImbalance;
+/// An easy way to access the [`fungible::CreditOf`] of [`CurrencySwap::To`] of `T`.
+pub type ToCreditOf<T, AccountIdFrom, AccountIdTo> =
+    fungible::CreditOf<AccountIdTo, <T as CurrencySwap<AccountIdFrom, AccountIdTo>>::To>;
 
 /// A type alias for compact declaration of the error type for the [`CurrencySwap::swap`] call.
 pub type ErrorFor<T, AccountIdFrom, AccountIdTo> = Error<
-    FromNegativeImbalanceFor<T, AccountIdFrom, AccountIdTo>,
+    FromCreditOf<T, AccountIdFrom, AccountIdTo>,
     <T as CurrencySwap<AccountIdFrom, AccountIdTo>>::Error,
 >;
 
