39be55f8006136c38800153823746196650ee224
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-07-25 15:17:16 +0300

    Introduce calculate_expected_to_bridge_balance helper function for balances genesis verifier

--- crates/pallet-bridge-pot-currency-swap/src/genesis_verifier.rs
+++ crates/pallet-bridge-pot-currency-swap/src/genesis_verifier.rs
@@ -1,8 +1,11 @@
 //! An implementation that verifies bridge pot balance values at genesis.
 
 use frame_support::{
-    sp_runtime::traits::Convert,
-    sp_std::marker::PhantomData,
+    sp_runtime::{
+        traits::{CheckedAdd, Convert, Zero},
+        ArithmeticError, DispatchError,
+    },
+    sp_std::{marker::PhantomData, vec::Vec},
     traits::{Currency, Get},
 };
 
@@ -10,6 +13,30 @@ use super::{Config, GenesisVerifier, Pallet};
 
 /// The implementation that requires bridge pot balance values be balanced.
 pub struct Balanced<Pallet>(PhantomData<Pallet>);
+
+impl<T: Config<I>, I: 'static> Balanced<Pallet<T, I>> {
+    /// A function to calculate expected [`Config::PotTo`] balance based on the provided list of
+    /// all [`Config::AccountIdFrom`] balances except [`Config::PotFrom`] balance.
+    pub fn calculate_expected_to_bridge_balance(
+        all_from_balances_without_bridge_balance: Vec<
+            <T::CurrencyFrom as Currency<T::AccountIdFrom>>::Balance,
+        >,
+    ) -> Result<<T::CurrencyTo as Currency<T::AccountIdTo>>::Balance, DispatchError> {
+        let to_bridge_balance = all_from_balances_without_bridge_balance.iter().try_fold(
+            Zero::zero(),
+            |sum: <T::CurrencyFrom as Currency<T::AccountIdFrom>>::Balance, &from_balance| {
+                sum.checked_add(&from_balance)
+                    .ok_or(DispatchError::Arithmetic(ArithmeticError::Overflow))
+            },
+        )?;
+
+        let to_bridge_balance = T::BalanceConverter::convert(to_bridge_balance)
+            .checked_add(&T::CurrencyTo::minimum_balance())
+            .ok_or(DispatchError::Arithmetic(ArithmeticError::Overflow))?;
+
+        Ok(to_bridge_balance)
+    }
+}
 
 impl<T: Config<I>, I: 'static> GenesisVerifier for Balanced<Pallet<T, I>> {
     fn verify() -> bool {
