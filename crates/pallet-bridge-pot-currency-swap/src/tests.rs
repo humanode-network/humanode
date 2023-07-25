ccc7d2864d8a8185cc6a980a3b8cdfcd58ec8d9c
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-07-25 17:26:15 +0300

    Add tests for calculate_expected_to_bridge_balance_fails_overflow function

--- crates/pallet-bridge-pot-currency-swap/src/tests.rs
+++ crates/pallet-bridge-pot-currency-swap/src/tests.rs
@@ -1,4 +1,6 @@
-use crate::mock::*;
+use frame_support::sp_runtime::{ArithmeticError, DispatchError};
+
+use crate::{genesis_verifier::Balanced, mock::*};
 
 /// This test verifies that the genesis builder correctly ensures genesis verifier result in case
 /// it returns false.
@@ -40,5 +42,38 @@ fn genesis_verifier_true() {
         new_test_ext_with(config).execute_with(move || {});
 
         verify_ctx.checkpoint();
+    })
+}
+
+/// This test verifies that `calculate_expected_to_bridge_balance` function works in the happy path.
+#[test]
+fn calculate_expected_to_bridge_balance_works() {
+    with_runtime_lock(|| {
+        let all_from_balances_without_bridge_balance = vec![10, 20, 30, 40];
+        let expected_to_bridge_balance =
+            all_from_balances_without_bridge_balance.iter().sum::<u64>() + EXISTENTIAL_DEPOSIT;
+        assert_eq!(
+            expected_to_bridge_balance,
+            Balanced::<SwapBridge>::calculate_expected_to_bridge_balance(
+                all_from_balances_without_bridge_balance
+            )
+            .unwrap()
+        );
+    })
+}
+
+/// This test verifies that `calculate_expected_to_bridge_balance` function fails in case
+/// overflow error happens.
+#[test]
+fn calculate_expected_to_bridge_balance_fails_overflow() {
+    with_runtime_lock(|| {
+        let all_from_balances_without_bridge_balance = vec![10, 20, 30, u64::MAX];
+        assert_eq!(
+            DispatchError::Arithmetic(ArithmeticError::Overflow),
+            Balanced::<SwapBridge>::calculate_expected_to_bridge_balance(
+                all_from_balances_without_bridge_balance
+            )
+            .unwrap_err()
+        );
     })
 }
