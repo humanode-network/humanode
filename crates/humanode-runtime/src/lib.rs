be0ff839e2023fa91521dc17385d4a75204a4501
MOZGIII <mike-n@narod.ru>
2023-08-04 03:43:13 -0300

    Zero out the DefaultElasticity

--- crates/humanode-runtime/src/lib.rs
+++ crates/humanode-runtime/src/lib.rs
@@ -710,7 +710,7 @@ impl pallet_dynamic_fee::Config for Runtime {
 
 parameter_types! {
     pub DefaultBaseFeePerGas: U256 = U256::from(1_000_000_000);
-    pub DefaultElasticity: Permill = Permill::from_parts(125_000);
+    pub DefaultElasticity: Permill = Permill::from_parts(0);
 }
 
 pub struct BaseFeeThreshold;
