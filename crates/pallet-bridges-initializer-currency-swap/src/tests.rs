33f69568f2ba877fe56a42e2a3cf9dbc46d081ae
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-08-08 16:59:08 +0300

    Improve tests

--- crates/pallet-bridges-initializer-currency-swap/src/tests.rs
+++ crates/pallet-bridges-initializer-currency-swap/src/tests.rs
@@ -3,31 +3,31 @@ use frame_support::traits::Currency;
 use crate::mock::*;
 
 #[test]
-fn initialization_works() {
+fn initialization_bridges_ed_works() {
     with_runtime_lock(|| {
+        let treasury = (NativeTreasury::get(), 1450);
+        let alice = (4201, 20);
+        let bob = (4203, 30);
+        let swap_bridge_native_evm = (
+            SwapBridgeNativeToEvmPot::account_id(),
+            EXISTENTIAL_DEPOSIT_NATIVE,
+        );
+
+        let lion = (4211, 200);
+        let dog = (4212, 300);
+        let cat = (4213, 400);
+        let fish = (4214, 500);
+        let swap_bridge_evm_native = (
+            SwapBridgeEvmToNativePot::account_id(),
+            EXISTENTIAL_DEPOSIT_EVM,
+        );
+
         let config = GenesisConfig {
             balances: pallet_balances::GenesisConfig {
-                balances: vec![
-                    (NativeTreasury::get(), 1450),
-                    (4201, 20),
-                    (4203, 30),
-                    (
-                        SwapBridgeNativeToEvmPot::account_id(),
-                        EXISTENTIAL_DEPOSIT_NATIVE,
-                    ),
-                ],
+                balances: vec![treasury, alice, bob, swap_bridge_native_evm],
             },
             evm_balances: pallet_balances::GenesisConfig {
-                balances: vec![
-                    (4211, 200),
-                    (4212, 300),
-                    (4213, 400),
-                    (4214, 500),
-                    (
-                        SwapBridgeEvmToNativePot::account_id(),
-                        EXISTENTIAL_DEPOSIT_EVM,
-                    ),
-                ],
+                balances: vec![lion, dog, cat, fish, swap_bridge_evm_native],
             },
             swap_bridge_native_to_evm_pot: pallet_pot::GenesisConfig {
                 initial_state: pallet_pot::InitialState::Initialized,
@@ -40,13 +40,110 @@ fn initialization_works() {
         new_test_ext_with(config).execute_with(move || {
             assert_eq!(
                 Balances::total_balance(&SwapBridgeNativeToEvmPot::account_id()),
-                200 + 300 + 400 + 500 + EXISTENTIAL_DEPOSIT_NATIVE
+                lion.1 + dog.1 + cat.1 + fish.1 + EXISTENTIAL_DEPOSIT_NATIVE
             );
-            assert_eq!(Balances::total_balance(&NativeTreasury::get()), 50);
+            assert_eq!(
+                Balances::total_balance(&NativeTreasury::get()),
+                treasury.1 - (lion.1 + dog.1 + cat.1 + fish.1)
+            );
             assert_eq!(
                 EvmBalances::total_balance(&SwapBridgeEvmToNativePot::account_id(),),
-                50 + 20 + 30 + EXISTENTIAL_DEPOSIT_EVM
+                Balances::total_balance(&NativeTreasury::get())
+                    + alice.1
+                    + bob.1
+                    + EXISTENTIAL_DEPOSIT_EVM
             );
         });
+    })
+}
+
+#[test]
+fn initialization_bridges_ed_delta_works() {
+    with_runtime_lock(|| {
+        let treasury = (NativeTreasury::get(), 1450);
+        let alice = (4201, 20);
+        let bob = (4203, 30);
+        let native_bridge_delta = 100;
+        let swap_bridge_native_evm = (
+            SwapBridgeNativeToEvmPot::account_id(),
+            EXISTENTIAL_DEPOSIT_NATIVE + native_bridge_delta,
+        );
+
+        let lion = (4211, 200);
+        let dog = (4212, 300);
+        let cat = (4213, 400);
+        let fish = (4214, 500);
+        let evm_bridge_delta = 50;
+        let swap_bridge_evm_native = (
+            SwapBridgeEvmToNativePot::account_id(),
+            EXISTENTIAL_DEPOSIT_EVM + evm_bridge_delta,
+        );
+
+        let config = GenesisConfig {
+            balances: pallet_balances::GenesisConfig {
+                balances: vec![treasury, alice, bob, swap_bridge_native_evm],
+            },
+            evm_balances: pallet_balances::GenesisConfig {
+                balances: vec![lion, dog, cat, fish, swap_bridge_evm_native],
+            },
+            swap_bridge_native_to_evm_pot: pallet_pot::GenesisConfig {
+                initial_state: pallet_pot::InitialState::Initialized,
+            },
+            swap_bridge_evm_to_native_pot: pallet_pot::GenesisConfig {
+                initial_state: pallet_pot::InitialState::Initialized,
+            },
+            ..Default::default()
+        };
+        new_test_ext_with(config).execute_with(move || {
+            assert_eq!(
+                Balances::total_balance(&SwapBridgeNativeToEvmPot::account_id()),
+                lion.1 + dog.1 + cat.1 + fish.1 + EXISTENTIAL_DEPOSIT_NATIVE
+            );
+            assert_eq!(
+                Balances::total_balance(&NativeTreasury::get()),
+                treasury.1 - (lion.1 + dog.1 + cat.1 + fish.1) + native_bridge_delta
+            );
+            assert_eq!(
+                EvmBalances::total_balance(&SwapBridgeEvmToNativePot::account_id(),),
+                Balances::total_balance(&NativeTreasury::get())
+                    + alice.1
+                    + bob.1
+                    + EXISTENTIAL_DEPOSIT_EVM
+            );
+        });
+    })
+}
+
+#[test]
+#[should_panic = "error during bridges initialization: Arithmetic(Overflow)"]
+fn initialization_fails_overflow() {
+    with_runtime_lock(|| {
+        let treasury = (NativeTreasury::get(), EXISTENTIAL_DEPOSIT_NATIVE);
+        let swap_bridge_native_evm = (
+            SwapBridgeNativeToEvmPot::account_id(),
+            u64::MAX - EXISTENTIAL_DEPOSIT_NATIVE,
+        );
+
+        let swap_bridge_evm_native = (
+            SwapBridgeEvmToNativePot::account_id(),
+            EXISTENTIAL_DEPOSIT_EVM,
+        );
+
+        let config = GenesisConfig {
+            balances: pallet_balances::GenesisConfig {
+                balances: vec![treasury, swap_bridge_native_evm],
+            },
+            evm_balances: pallet_balances::GenesisConfig {
+                balances: vec![swap_bridge_evm_native],
+            },
+            swap_bridge_native_to_evm_pot: pallet_pot::GenesisConfig {
+                initial_state: pallet_pot::InitialState::Initialized,
+            },
+            swap_bridge_evm_to_native_pot: pallet_pot::GenesisConfig {
+                initial_state: pallet_pot::InitialState::Initialized,
+            },
+            ..Default::default()
+        };
+        new_test_ext_with(config).execute_with(move || {});
     })
 }
