d374d66aa06f2a533c5fc8dec059b4fc823c4450
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-08-09 23:20:12 +0300

    Add initialization idempotence test

--- crates/pallet-bridges-initializer-currency-swap/src/tests.rs
+++ crates/pallet-bridges-initializer-currency-swap/src/tests.rs
@@ -1,4 +1,7 @@
-use frame_support::traits::{Currency, OnRuntimeUpgrade};
+use frame_support::{
+    assert_storage_noop,
+    traits::{Currency, OnRuntimeUpgrade},
+};
 
 use crate::mock::{new_test_ext_with, v0, v1, with_runtime_lock, *};
 
@@ -135,6 +138,82 @@ fn initialization_bridges_ed_delta_works() {
                     + BOB.balance
                     + EXISTENTIAL_DEPOSIT_EVM
             );
+        });
+    })
+}
+
+#[test]
+fn initialization_idempotence() {
+    with_runtime_lock(|| {
+        let treasury = AccountInfo {
+            account: NativeTreasury::get(),
+            balance: 1450,
+        };
+        let swap_bridge_native_evm = AccountInfo {
+            account: v1::SwapBridgeNativeToEvmPot::account_id(),
+            balance: EXISTENTIAL_DEPOSIT_NATIVE,
+        };
+
+        let swap_bridge_evm_native = AccountInfo {
+            account: v1::SwapBridgeEvmToNativePot::account_id(),
+            balance: EXISTENTIAL_DEPOSIT_EVM,
+        };
+
+        let config = v1::GenesisConfig {
+            balances: pallet_balances::GenesisConfig {
+                balances: vec![
+                    treasury.into(),
+                    ALICE.into(),
+                    BOB.into(),
+                    swap_bridge_native_evm.into(),
+                ],
+            },
+            evm_balances: pallet_balances::GenesisConfig {
+                balances: vec![
+                    LION.into(),
+                    DOG.into(),
+                    CAT.into(),
+                    FISH.into(),
+                    swap_bridge_evm_native.into(),
+                ],
+            },
+            swap_bridge_native_to_evm_pot: pallet_pot::GenesisConfig {
+                initial_state: pallet_pot::InitialState::Initialized,
+            },
+            swap_bridge_evm_to_native_pot: pallet_pot::GenesisConfig {
+                initial_state: pallet_pot::InitialState::Initialized,
+            },
+            ..Default::default()
+        };
+        new_test_ext_with(config).execute_with(move || {
+            // Verify that bridges initialization has been applied at genesis.
+            assert!(v1::EvmNativeBridgesInitializer::is_balanced().unwrap());
+            assert_eq!(
+                v1::Balances::total_balance(&v1::SwapBridgeNativeToEvmPot::account_id()),
+                LION.balance
+                    + DOG.balance
+                    + CAT.balance
+                    + FISH.balance
+                    + EXISTENTIAL_DEPOSIT_NATIVE
+            );
+            assert_eq!(
+                v1::Balances::total_balance(&NativeTreasury::get()),
+                treasury.balance - (LION.balance + DOG.balance + CAT.balance + FISH.balance)
+            );
+            assert_eq!(
+                v1::EvmBalances::total_balance(&v1::SwapBridgeEvmToNativePot::account_id(),),
+                v1::Balances::total_balance(&NativeTreasury::get())
+                    + ALICE.balance
+                    + BOB.balance
+                    + EXISTENTIAL_DEPOSIT_EVM
+            );
+
+            for _ in 0..5 {
+                v1::Balances::transfer(Some(ALICE.account).into(), BOB.account, 1).unwrap();
+
+                // Initialize bridges one more time.
+                assert_storage_noop!(v1::EvmNativeBridgesInitializer::initialize().unwrap());
+            }
         });
     })
 }
