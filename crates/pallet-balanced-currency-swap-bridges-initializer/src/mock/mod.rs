2721b2b5d55a702997c727956df749373f173243
Dmitry Lavrenov <39522748+dmitrylavrenov@users.noreply.github.com>
2023-08-15 13:23:44 +0300

    Implement bridges pot currency swap accounts inititialization (#716)
    * Initial basic implementation
    
    * Inline implementations
    
    * Docs and refactoring
    
    * Handle all bridges initial balances cases
    
    * Fix basic test
    
    * Properly handle edge cases
    
    * Improve tests
    
    * Add initialization_fails_treasury_insufficient_balance test
    
    * Update features snapshot
    
    * Add weight value calculation
    
    * Add runtime-upgrade logic
    
    * Add runtime upgrade test
    
    * Check is balanced before applying initialization
    
    * Fix try-runtime feature related code
    
    * Rename pallet in mock
    
    * Use runtime_lock and externalities logic as shared components
    
    * More explicit mocks usage in tests
    
    * Use a struct type for address/balance pair in tests
    
    * Rename version mocks
    
    * Add initialization idempotence test
    
    * Add initializer version storage
    
    * Add verify_balanced call
    
    * Edit docs
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>
    
    * Twice immediate reinvocation  idempotency
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>
    
    * Add transfer with creating account to test
    
    * Make is_balanced fn public
    
    * Fix typos
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>
    
    * Improve docs and debug_assert for make_native_bridge_balance_be
    
    * Improve docs and debug_assert for make_evm_bridge_balance_be
    
    * Improve assert messages
    
    * Proportionally equal natve and evm total issuances
    
    * Fix is_balanced logic to calculate different swappable balances
    
    * Rename pallet
    
    * Use a helper function to calculate required bridges minimum balances
    
    * Fix pallet renaming in tests
    
    * Fix pallet renaming in features snapshot
    
    * Return test
    
    * Add docs for tests
    
    ---------
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>

---
+++ crates/pallet-balanced-currency-swap-bridges-initializer/src/mock/mod.rs
@@ -0,0 +1,90 @@
+//! The mocks for the pallet testing.
+
+use frame_support::{parameter_types, sp_io, sp_runtime::BuildStorage};
+
+use crate::{self as pallet_balanced_currency_swap_bridges_initializer};
+
+pub mod v0;
+pub mod v1;
+
+pub(crate) const EXISTENTIAL_DEPOSIT_NATIVE: u64 = 10;
+pub(crate) const EXISTENTIAL_DEPOSIT_EVM: u64 = 20;
+
+pub(crate) type AccountId = u64;
+pub(crate) type EvmAccountId = u64;
+
+type Balance = u64;
+
+type BalancesInstanceNative = pallet_balances::Instance1;
+type BalancesInstanceEvm = pallet_balances::Instance2;
+
+parameter_types! {
+    pub NativeTreasury: AccountId = 4200;
+}
+
+// This function basically just builds a genesis storage key/value store according to
+// our desired mockup.
+pub fn new_test_ext_with(genesis_config: impl BuildStorage) -> sp_io::TestExternalities {
+    let storage = genesis_config.build_storage().unwrap();
+    storage.into()
+}
+
+pub fn runtime_lock() -> std::sync::MutexGuard<'static, ()> {
+    static MOCK_RUNTIME_MUTEX: std::sync::Mutex<()> = std::sync::Mutex::new(());
+
+    // Ignore the poisoning for the tests that panic.
+    // We only care about concurrency here, not about the poisoning.
+    match MOCK_RUNTIME_MUTEX.lock() {
+        Ok(guard) => guard,
+        Err(poisoned) => poisoned.into_inner(),
+    }
+}
+
+pub fn with_runtime_lock<R>(f: impl FnOnce() -> R) -> R {
+    let lock = runtime_lock();
+    let res = f();
+    drop(lock);
+    res
+}
+
+#[derive(Clone, Copy)]
+pub(crate) struct AccountInfo {
+    pub account: u64,
+    pub balance: u64,
+}
+
+impl From<AccountInfo> for (u64, u64) {
+    fn from(account_info: AccountInfo) -> Self {
+        (account_info.account, account_info.balance)
+    }
+}
+
+pub(crate) const ALICE: AccountInfo = AccountInfo {
+    account: 4201,
+    balance: 20,
+};
+
+pub(crate) const BOB: AccountInfo = AccountInfo {
+    account: 4202,
+    balance: 30,
+};
+
+pub(crate) const LION: AccountInfo = AccountInfo {
+    account: 4211,
+    balance: 200,
+};
+
+pub(crate) const DOG: AccountInfo = AccountInfo {
+    account: 4212,
+    balance: 300,
+};
+
+pub(crate) const CAT: AccountInfo = AccountInfo {
+    account: 4213,
+    balance: 400,
+};
+
+pub(crate) const FISH: AccountInfo = AccountInfo {
+    account: 4214,
+    balance: 500,
+};
---
+++ crates/pallet-balanced-currency-swap-bridges-initializer/src/upgrade_init.rs
@@ -0,0 +1,48 @@
+//! Initialization of the bridge pot accounts on runtime upgrade.
+
+use frame_support::pallet_prelude::*;
+
+use crate::{Config, InitializerVersion, Pallet, CURRENT_BRIDGES_INITIALIZER_VERSION};
+
+/// Initialize the bridges pot accounts.
+pub fn on_runtime_upgrade<T: Config>() -> Weight {
+    let initializer_version = <InitializerVersion<T>>::get();
+    let mut weight = T::DbWeight::get().reads(1);
+
+    if initializer_version != CURRENT_BRIDGES_INITIALIZER_VERSION {
+        let is_balanced = Pallet::<T>::is_balanced().unwrap_or_default();
+        weight += T::DbWeight::get().reads(8);
+
+        if !is_balanced {
+            match Pallet::<T>::initialize() {
+                Ok(w) => weight += w,
+                Err(err) => sp_tracing::error!("error during bridges initialization: {err:?}"),
+            }
+        }
+
+        <InitializerVersion<T>>::put(CURRENT_BRIDGES_INITIALIZER_VERSION);
+    }
+
+    weight
+}
+
+/// Check the state before the bridges initialization.
+///
+/// Panics if anything goes wrong.
+#[cfg(feature = "try-runtime")]
+pub fn pre_upgrade<T: Config>() -> Result<Vec<u8>, &'static str> {
+    // Do nothing.
+    Ok(Vec::new())
+}
+
+/// Check the state after the bridges initialization.
+///
+/// Panics if anything goes wrong.
+#[cfg(feature = "try-runtime")]
+pub fn post_upgrade<T: Config>(_state: Vec<u8>) -> Result<(), &'static str> {
+    if !Pallet::<T>::is_balanced()? {
+        return Err("currencies are not balanced");
+    }
+
+    Ok(())
+}
