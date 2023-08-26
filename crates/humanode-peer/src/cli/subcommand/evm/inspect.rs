65deb3c72211ce71257dd2ddd5e7379f09cb805f
MOZGIII <mike-n@narod.ru>
2023-08-26 00:20:56 -0300

    Require a special --dev flag for a dev mnemonic

--- crates/humanode-peer/src/cli/subcommand/evm/inspect.rs
+++ crates/humanode-peer/src/cli/subcommand/evm/inspect.rs
@@ -4,12 +4,25 @@ use bip39::{Language, Mnemonic};
 
 use super::utils::extract_and_print_keys;
 
-/// The `ethereum inspect` command.
-#[derive(Debug, clap::Parser)]
-pub struct InspectAccountCmd {
+/// The mnemonic input.
+#[derive(Debug, clap::Args)]
+#[group(required = true, multiple = false)]
+pub struct MnemonicInput {
     /// Specify the mnemonic.
     #[arg(long, short = 'm')]
     mnemonic: Option<String>,
+
+    /// Use the built-in dev mnemonic.
+    #[arg(long, short = 'd')]
+    dev: bool,
+}
+
+/// The `ethereum inspect` command.
+#[derive(Debug, clap::Parser)]
+pub struct InspectAccountCmd {
+    /// The mnemonic input.
+    #[command(flatten)]
+    pub mnemonic_input: MnemonicInput,
 
     /// The account index to use in the derivation path.
     #[arg(long = "account-index", short = 'a')]
@@ -19,9 +32,9 @@ pub struct InspectAccountCmd {
 impl InspectAccountCmd {
     /// Run the inspect command.
     pub async fn run(&self) -> sc_cli::Result<()> {
-        let mnemonic = self
-            .mnemonic
-            .as_ref()
+        let mnemonic = self.mnemonic_input.mnemonic.as_ref();
+
+        let mnemonic = mnemonic
             .map(|mnemonic| Mnemonic::from_phrase(mnemonic, Language::English))
             .transpose()
             .map_err(|err| sc_cli::Error::Input(err.to_string()))?;
