2a547f0687e546447402624815ecda5e84b80c0e
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-09-04 17:20:58 +0300

    Add transfer_from_works test

--- crates/pallet-wrapped-token/src/tests.rs
+++ crates/pallet-wrapped-token/src/tests.rs
@@ -33,7 +33,6 @@ fn balance_of_works() {
     });
 }
 
-/// This test verifies basic approve related flow.
 #[test]
 fn approve_works() {
     new_test_ext().execute_with_ext(|_| {
@@ -75,5 +74,43 @@ fn transfer_works() {
             alice_balance - transferred_balance
         );
         assert_eq!(Balances::total_balance(&bob), transferred_balance);
+    });
+}
+
+#[test]
+fn transfer_from_works() {
+    new_test_ext().execute_with_ext(|_| {
+        let alice = 42;
+        let alice_balance = 10000;
+        let bob = 43;
+        let approved_balance = 5000;
+        let charlie = 44;
+        let transferred_from_balance = 1000;
+
+        // Prepare the test state.
+        Balances::make_free_balance_be(&alice, alice_balance);
+        WrappedBalances::approve(alice, bob, approved_balance);
+
+        // Execute transfer_from.
+        assert_ok!(WrappedBalances::transfer_from(
+            bob,
+            alice,
+            charlie,
+            transferred_from_balance
+        ));
+
+        // Check resulted balances.
+        assert_eq!(
+            Balances::total_balance(&alice),
+            alice_balance - transferred_from_balance
+        );
+        assert_eq!(Balances::total_balance(&bob), 0);
+        assert_eq!(Balances::total_balance(&charlie), transferred_from_balance);
+
+        // Check updated approvals changes.
+        assert_eq!(
+            WrappedBalances::approvals(alice, bob),
+            Some(approved_balance - transferred_from_balance)
+        );
     });
 }
