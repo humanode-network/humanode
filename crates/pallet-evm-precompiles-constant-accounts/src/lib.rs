bedf1f8b5f9f26a027236dd123026185a63412d9
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-09-18 16:17:44 +0300

    Some refactoring

--- crates/pallet-evm-precompiles-constant-accounts/src/lib.rs
+++ crates/pallet-evm-precompiles-constant-accounts/src/lib.rs
@@ -17,18 +17,13 @@ const STORAGE_VERSION: StorageVersion = StorageVersion::new(0);
 
 // We have to temporarily allow some clippy lints. Later on we'll send patches to substrate to
 // fix them at their end.
-#[allow(
-    missing_docs,
-    clippy::missing_docs_in_private_items,
-    clippy::unused_unit
-)]
+#[allow(clippy::missing_docs_in_private_items)]
 #[frame_support::pallet]
 pub mod pallet {
     use frame_support::pallet_prelude::*;
 
     use super::*;
 
-    /// The Ethereum Chain Id Pallet
     #[pallet::pallet]
     #[pallet::storage_version(STORAGE_VERSION)]
     #[pallet::generate_store(pub(super) trait Store)]
@@ -39,25 +34,17 @@ pub mod pallet {
     pub trait Config: frame_system::Config + pallet_evm::Config {}
 
     #[pallet::genesis_config]
+    #[derive(Default)]
     pub struct GenesisConfig {
-        pub precompiles: Vec<H160>,
-    }
-
-    // The default value for the genesis config type.
-    #[cfg(feature = "std")]
-    impl Default for GenesisConfig {
-        fn default() -> Self {
-            Self {
-                precompiles: Default::default(),
-            }
-        }
+        /// The list of precompiles to be stored as account codes at evm.
+        pub precompiles: Vec<(H160, Vec<u8>)>,
     }
 
     #[pallet::genesis_build]
     impl<T: Config> GenesisBuild<T> for GenesisConfig {
         fn build(&self) {
-            for precompile in &self.precompiles {
-                pallet_evm::Pallet::<T>::create_account(*precompile, vec![1, 2, 3]);
+            for (address, code) in &self.precompiles {
+                pallet_evm::Pallet::<T>::create_account(*address, code.clone());
             }
         }
     }
--- crates/pallet-evm-precompiles-constant-accounts/src/tests.rs
+++ crates/pallet-evm-precompiles-constant-accounts/src/tests.rs
@@ -5,13 +5,13 @@ use crate::{self as pallet_evm_precompiles_constant_accounts, mock::*};
 #[test]
 fn genesis_build() {
     // Prepare some sample data and a config.
-    let precompile_a = H160::from_low_u64_be(2020);
-    let precompile_b = H160::from_low_u64_be(2040);
+    let precompile_a = (H160::from_low_u64_be(2020), vec![1, 2, 3]);
+    let precompile_b = (H160::from_low_u64_be(2040), vec![1, 2, 3]);
 
     let config = GenesisConfig {
         evm_precompiles_constant_accounts: {
             pallet_evm_precompiles_constant_accounts::GenesisConfig {
-                precompiles: vec![precompile_a, precompile_b],
+                precompiles: vec![precompile_a.clone(), precompile_b.clone()],
             }
         },
         ..Default::default()
@@ -20,6 +20,7 @@ fn genesis_build() {
     // Build the state from the config.
     new_test_ext_with(config).execute_with(move || {
         // Assert the state.
-        assert_eq!(Evm::account_codes(precompile_a), vec![1, 2, 3]);
+        assert_eq!(Evm::account_codes(precompile_a.0), precompile_a.1);
+        assert_eq!(Evm::account_codes(precompile_b.0), precompile_b.1);
     })
 }
