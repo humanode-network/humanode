b9411dd6d9d3117747b00bfd147779fac7edf549
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-09-18 13:02:51 +0300

    Initial draft implementation

---
+++ crates/pallet-evm-precompiles-constant-accounts/src/lib.rs
@@ -0,0 +1,58 @@
+//! A substrate pallet.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::traits::StorageVersion;
+pub use pallet::*;
+use sp_core::H160;
+
+/// The current storage version.
+const STORAGE_VERSION: StorageVersion = StorageVersion::new(0);
+
+// We have to temporarily allow some clippy lints. Later on we'll send patches to substrate to
+// fix them at their end.
+#[allow(
+    missing_docs,
+    clippy::missing_docs_in_private_items,
+    clippy::unused_unit
+)]
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::pallet_prelude::*;
+
+    use super::*;
+
+    /// The Ethereum Chain Id Pallet
+    #[pallet::pallet]
+    #[pallet::storage_version(STORAGE_VERSION)]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
+
+    /// Configuration trait of this pallet.
+    #[pallet::config]
+    pub trait Config: frame_system::Config + pallet_evm::Config {}
+
+    #[pallet::genesis_config]
+    pub struct GenesisConfig {
+        pub precompiles: Vec<H160>,
+    }
+
+    // The default value for the genesis config type.
+    #[cfg(feature = "std")]
+    impl Default for GenesisConfig {
+        fn default() -> Self {
+            Self {
+                precompiles: Default::default(),
+            }
+        }
+    }
+
+    #[pallet::genesis_build]
+    impl<T: Config> GenesisBuild<T> for GenesisConfig {
+        fn build(&self) {
+            for precompile in &self.precompiles {
+                pallet_evm::Pallet::<T>::create_account(*precompile, vec![1, 2, 3]);
+            }
+        }
+    }
+}
