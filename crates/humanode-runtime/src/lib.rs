c58d5a399207a29bae55195c471a98ddf60530c2
Dmitry Lavrenov <39522748+dmitrylavrenov@users.noreply.github.com>
2023-09-18 23:19:19 +0300

    Add eHMND ERC20 precompile (#746)
    * Add basic implementation for total_supply and balance_of calls
    
    * Integrate initial precompile-evm-balances-erc20 implementation into humanode-runtime
    
    * Add metadata erc20 methods
    
    * Add approvals related logic
    
    * Add transfer related method
    
    * Add transferFrom related method
    
    * Add event logs
    
    * Add docs to interface actions
    
    * Fix some clippy
    
    * Add docs for approves logic
    
    * Fix typos
    
    * Remove unnecesary docs
    
    * Introduce helper functions to simplify code
    
    * Some refactoring
    
    * Update features snapshot
    
    * Fix docs
    
    * Add ERC20 solidity interface
    
    * Rename erc20 metadata at frontier precompiles
    
    * Add mocked environment
    
    * Add metadata related tests
    
    * Add balance_of_works and total_supply_works tests
    
    * Add approve_works test
    
    * Rename some tests
    
    * Add transfer_works test
    
    * Add transfer_from_works test
    
    * Rename symbol
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>
    
    * Edit name erc20 metadata
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>
    
    * Introduce pallet-erc20 to store approvals data
    
    * Use approvals logic from pallet-erc20 at precompile
    
    * Use currency config instead of pallet-evm-balances config
    
    * Add erc20 related logic into pallet-erc20
    
    * Add utility aliases
    
    * Integrate pallet-erc20 logic into precompile
    
    * Fix mock at pallet-erc20
    
    * Fix mock at precompile
    
    * Remove redundant dependency
    
    * Rename pallet-erc20 to pallet-token-wrapper
    
    * Rename precompile-evm-balances-erc20 into precompile-token-wrapper
    
    * Integrate introduced changes into humanode-runtime
    
    * Use wrapped-token instead for token-wrapper
    
    * Rename EvmBalancesErc20 into WrappedEvmBalances
    
    * Add total_supply_works test to pallet-wrapped-token
    
    * Improve approvals related test at pallet-wrapped-token
    
    * Add transfer_works test
    
    * Add transfer_from_works test
    
    * Add fails tests for transfer from logic
    
    * Add with_storage_layer usage at pallet-wrapped-token
    
    * Fix features
    
    * Use approvals aliases
    
    * Rename pallet-wrapped-token into pallet-erc20-support
    
    * Some renaming and docs improvements
    
    * Update features snapshot
    
    * Add comments for tests at pallet-erc20-support
    
    * Improve dispatch error handling
    
    * Add more tests at precompile-erc20-support
    
    * Rename precompile-erc20-support into precompile-native-currency
    
    * Rename Erc20EvmBalancesMetadata into EvmBalancesErc20Metadata
    
    * Rename Erc20EvmBalances into EvmBalancesErc20Support
    
    * Revert "Use approvals aliases"
    
    This reverts commit 654dc6523c41bb6598fa23e34b3ca80dcec31b42.
    
    * Properly handle transferFrom logic
    
    * Improve dispatch error handling
    
    * Add approve_overwrite_works test
    
    * Fix transfer
    
    * Add tests to check approve logic with sending full approved balance
    
    * Add approve_approval_value_more_than_balance_works test
    
    ---------
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>

--- crates/humanode-runtime/src/lib.rs
+++ crates/humanode-runtime/src/lib.rs
@@ -788,6 +788,28 @@ impl pallet_balanced_currency_swap_bridges_initializer::Config for Runtime {
     type WeightInfo = ();
 }
 
+pub struct EvmBalancesErc20Metadata;
+
+impl pallet_erc20_support::Metadata for EvmBalancesErc20Metadata {
+    fn name() -> &'static str {
+        "Wrapped eHMND"
+    }
+
+    fn symbol() -> &'static str {
+        "WeHMND"
+    }
+
+    fn decimals() -> u8 {
+        18
+    }
+}
+
+impl pallet_erc20_support::Config for Runtime {
+    type AccountId = EvmAccountId;
+    type Currency = EvmBalances;
+    type Metadata = EvmBalancesErc20Metadata;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously
 // configured.
 construct_runtime!(
@@ -832,6 +854,7 @@ construct_runtime!(
         EvmToNativeSwapBridgePot: pallet_pot::<Instance5> = 34,
         CurrencySwap: pallet_currency_swap = 35,
         BalancedCurrencySwapBridgesInitializer: pallet_balanced_currency_swap_bridges_initializer = 36,
+        EvmBalancesErc20Support: pallet_erc20_support = 37,
     }
 );
 
---
+++ crates/precompile-native-currency/src/lib.rs
@@ -0,0 +1,345 @@
+//! A precompile to interact with currency instance using the ERC20 interface standard.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::{
+    sp_runtime::{self, DispatchError},
+    sp_std::{marker::PhantomData, prelude::*},
+    traits::Currency,
+};
+use pallet_erc20_support::Metadata;
+use pallet_evm::{
+    ExitError, Precompile, PrecompileFailure, PrecompileHandle, PrecompileOutput, PrecompileResult,
+};
+use precompile_utils::{
+    keccak256, succeed, Address, Bytes, EvmDataReader, EvmDataWriter, EvmResult, LogExt,
+    LogsBuilder, PrecompileHandleExt,
+};
+use sp_core::{Get, H160, U256};
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+/// Solidity selector of the Transfer log, which is the Keccak of the Log signature.
+pub const SELECTOR_LOG_TRANSFER: [u8; 32] = keccak256!("Transfer(address,address,uint256)");
+
+/// Solidity selector of the Approval log, which is the Keccak of the Log signature.
+pub const SELECTOR_LOG_APPROVAL: [u8; 32] = keccak256!("Approval(address,address,uint256)");
+
+/// Utility alias for easy access to the [`pallet_erc20_support::Config::AccountId`].
+type AccountIdOf<T> = <T as pallet_erc20_support::Config>::AccountId;
+
+/// Utility alias for easy access to the [`Currency::Balance`] of the [`pallet_erc20_support::Config::Currency`] type.
+type BalanceOf<T> =
+    <<T as pallet_erc20_support::Config>::Currency as Currency<AccountIdOf<T>>>::Balance;
+
+/// Possible actions for this interface.
+#[precompile_utils::generate_function_selector]
+#[derive(Debug, PartialEq)]
+pub enum Action {
+    /// Returns the name of the token.
+    Name = "name()",
+    /// Returns the symbol of the token.
+    Symbol = "symbol()",
+    /// Returns the decimals places of the token.
+    Decimals = "decimals()",
+    /// Returns the amount of tokens in existence.
+    TotalSupply = "totalSupply()",
+    /// Returns the amount of tokens owned by provided account.
+    BalanceOf = "balanceOf(address)",
+    /// Returns the remaining number of tokens that spender will be allowed to spend on behalf of
+    /// owner through transferFrom. This is zero by default.
+    Allowance = "allowance(address,address)",
+    /// Sets amount as the allowance of spender over the caller’s tokens.
+    Approve = "approve(address,uint256)",
+    /// Moves amount tokens from the caller’s account to recipient.
+    Transfer = "transfer(address,uint256)",
+    /// Moves amount tokens from sender to recipient using the allowance mechanism,
+    /// amount is then deducted from the caller’s allowance.
+    TransferFrom = "transferFrom(address,address,uint256)",
+}
+
+/// Precompile exposing currency instance as ERC20.
+pub struct NativeCurrency<Erc20SupportT, GasCost>(PhantomData<(Erc20SupportT, GasCost)>)
+where
+    GasCost: Get<u64>;
+
+impl<Erc20SupportT, GasCost> Precompile for NativeCurrency<Erc20SupportT, GasCost>
+where
+    Erc20SupportT: pallet_erc20_support::Config,
+    AccountIdOf<Erc20SupportT>: From<H160>,
+    BalanceOf<Erc20SupportT>: Into<U256> + TryFrom<U256>,
+    GasCost: Get<u64>,
+{
+    fn execute(handle: &mut impl PrecompileHandle) -> PrecompileResult {
+        let selector = handle
+            .read_selector()
+            .map_err(|_| PrecompileFailure::Error {
+                exit_status: ExitError::Other("invalid function selector".into()),
+            })?;
+
+        match selector {
+            Action::Name => Self::name(handle),
+            Action::Symbol => Self::symbol(handle),
+            Action::Decimals => Self::decimals(handle),
+            Action::TotalSupply => Self::total_supply(handle),
+            Action::BalanceOf => Self::balance_of(handle),
+            Action::Allowance => Self::allowance(handle),
+            Action::Approve => Self::approve(handle),
+            Action::Transfer => Self::transfer(handle),
+            Action::TransferFrom => Self::transfer_from(handle),
+        }
+    }
+}
+
+impl<Erc20SupportT, GasCost> NativeCurrency<Erc20SupportT, GasCost>
+where
+    Erc20SupportT: pallet_erc20_support::Config,
+    AccountIdOf<Erc20SupportT>: From<H160>,
+    BalanceOf<Erc20SupportT>: Into<U256> + TryFrom<U256>,
+    GasCost: Get<u64>,
+{
+    /// Returns the name of the token.
+    fn name(_handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+        let name: Bytes = Erc20SupportT::Metadata::name().into();
+
+        Ok(succeed(EvmDataWriter::new().write(name).build()))
+    }
+
+    /// Returns the symbol of the token.
+    fn symbol(_handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+        let symbol: Bytes = Erc20SupportT::Metadata::symbol().into();
+
+        Ok(succeed(EvmDataWriter::new().write(symbol).build()))
+    }
+
+    /// Returns the decimals places of the token.
+    fn decimals(_handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+        let decimals: u8 = Erc20SupportT::Metadata::decimals();
+
+        Ok(succeed(EvmDataWriter::new().write(decimals).build()))
+    }
+
+    /// Returns the amount of tokens in existence.
+    fn total_supply(_handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+        let total_supply: U256 =
+            pallet_erc20_support::Pallet::<Erc20SupportT>::total_supply().into();
+
+        Ok(succeed(EvmDataWriter::new().write(total_supply).build()))
+    }
+
+    /// Returns the amount of tokens owned by provided account.
+    fn balance_of(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+        let mut input = handle.read_input()?;
+        check_input(&mut input, 1)?;
+
+        let owner: Address = input.read()?;
+        let owner: H160 = owner.into();
+
+        check_input_end(&mut input)?;
+
+        let total_balance: U256 =
+            pallet_erc20_support::Pallet::<Erc20SupportT>::balance_of(&owner.into()).into();
+
+        Ok(succeed(EvmDataWriter::new().write(total_balance).build()))
+    }
+
+    /// Returns the remaining number of tokens that spender will be allowed to spend on behalf of
+    /// owner through transferFrom. This is zero by default.
+    fn allowance(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+        let mut input = handle.read_input()?;
+
+        check_input(&mut input, 2)?;
+
+        let owner: Address = input.read()?;
+        let owner: H160 = owner.into();
+
+        let spender: Address = input.read()?;
+        let spender: H160 = spender.into();
+
+        check_input_end(&mut input)?;
+
+        Ok(succeed(
+            EvmDataWriter::new()
+                .write(
+                    pallet_erc20_support::Pallet::<Erc20SupportT>::allowance(
+                        &owner.into(),
+                        &spender.into(),
+                    )
+                    .into(),
+                )
+                .build(),
+        ))
+    }
+
+    /// Sets amount as the allowance of spender over the caller’s tokens.
+    fn approve(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+        handle.record_cost(GasCost::get())?;
+
+        let mut input = handle.read_input()?;
+
+        let owner = handle.context().caller;
+
+        check_input(&mut input, 2)?;
+
+        let spender: Address = input.read()?;
+        let spender: H160 = spender.into();
+
+        let amount: U256 = input.read()?;
+
+        check_input_end(&mut input)?;
+
+        pallet_erc20_support::Pallet::<Erc20SupportT>::approve(
+            owner.into(),
+            spender.into(),
+            amount.try_into().map_err(|_| PrecompileFailure::Error {
+                exit_status: ExitError::Other("value is out of bounds".into()),
+            })?,
+        );
+
+        let logs_builder = LogsBuilder::new(handle.context().address);
+
+        logs_builder
+            .log3(
+                SELECTOR_LOG_APPROVAL,
+                handle.context().caller,
+                spender,
+                EvmDataWriter::new().write(amount).build(),
+            )
+            .record(handle)?;
+
+        Ok(succeed(EvmDataWriter::new().write(true).build()))
+    }
+
+    /// Moves amount tokens from the caller’s account to recipient.
+    fn transfer(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+        handle.record_cost(GasCost::get())?;
+
+        let mut input = handle.read_input()?;
+
+        let caller = handle.context().caller;
+
+        check_input(&mut input, 2)?;
+
+        let recipient: Address = input.read()?;
+        let recipient: H160 = recipient.into();
+
+        let amount: U256 = input.read()?;
+
+        check_input_end(&mut input)?;
+
+        pallet_erc20_support::Pallet::<Erc20SupportT>::transfer(
+            caller.into(),
+            recipient.into(),
+            amount.try_into().map_err(|_| PrecompileFailure::Error {
+                exit_status: ExitError::Other("value is out of bounds".into()),
+            })?,
+        )
+        .map_err(process_dispatch_error::<Erc20SupportT>)?;
+
+        let logs_builder = LogsBuilder::new(handle.context().address);
+
+        logs_builder
+            .log3(
+                SELECTOR_LOG_TRANSFER,
+                caller,
+                recipient,
+                EvmDataWriter::new().write(amount).build(),
+            )
+            .record(handle)?;
+
+        Ok(succeed(EvmDataWriter::new().write(true).build()))
+    }
+
+    /// Moves amount tokens from sender to recipient using the allowance mechanism,
+    /// amount is then deducted from the caller’s allowance.
+    fn transfer_from(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+        handle.record_cost(GasCost::get())?;
+
+        let mut input = handle.read_input()?;
+
+        let caller = handle.context().caller;
+
+        check_input(&mut input, 3)?;
+
+        let sender: Address = input.read()?;
+        let sender: H160 = sender.into();
+
+        let recipient: Address = input.read()?;
+        let recipient: H160 = recipient.into();
+
+        let amount: U256 = input.read()?;
+
+        check_input_end(&mut input)?;
+
+        pallet_erc20_support::Pallet::<Erc20SupportT>::transfer_from(
+            caller.into(),
+            sender.into(),
+            recipient.into(),
+            amount.try_into().map_err(|_| PrecompileFailure::Error {
+                exit_status: ExitError::Other("value is out of bounds".into()),
+            })?,
+        )
+        .map_err(process_dispatch_error::<Erc20SupportT>)?;
+
+        let logs_builder = LogsBuilder::new(handle.context().address);
+
+        logs_builder
+            .log4(
+                SELECTOR_LOG_TRANSFER,
+                caller,
+                sender,
+                recipient,
+                EvmDataWriter::new().write(amount).build(),
+            )
+            .record(handle)?;
+
+        Ok(succeed(EvmDataWriter::new().write(true).build()))
+    }
+}
+
+/// A helper function to process dispatch related errors.
+fn process_dispatch_error<Erc20SupportT: pallet_erc20_support::Config>(
+    error: DispatchError,
+) -> PrecompileFailure {
+    if error == pallet_erc20_support::Error::SpendMoreThanAllowed::<Erc20SupportT>.into() {
+        PrecompileFailure::Error {
+            exit_status: ExitError::Other("spend more than allowed".into()),
+        }
+    } else {
+        match error {
+            DispatchError::Token(sp_runtime::TokenError::NoFunds) => PrecompileFailure::Error {
+                exit_status: ExitError::OutOfFund,
+            },
+            _ => PrecompileFailure::Error {
+                exit_status: ExitError::Other("unable to transfer funds".into()),
+            },
+        }
+    }
+}
+
+/// A helper function to check expected arguments number.
+fn check_input(input: &mut EvmDataReader, args_number: u8) -> Result<(), PrecompileFailure> {
+    input
+        .expect_arguments(args_number.into())
+        .map_err(|_| PrecompileFailure::Error {
+            exit_status: ExitError::Other("not expected arguments number".into()),
+        })?;
+
+    Ok(())
+}
+
+/// A helper function that verifies possible junk at the end of input.
+fn check_input_end(input: &mut EvmDataReader) -> Result<(), PrecompileFailure> {
+    let junk_data = input.read_till_end()?;
+    if !junk_data.is_empty() {
+        return Err(PrecompileFailure::Error {
+            exit_status: ExitError::Other("junk at the end of input".into()),
+        });
+    }
+
+    Ok(())
+}
