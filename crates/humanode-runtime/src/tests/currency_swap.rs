67f88e4b43ce9aebcf2c65780ca148a988d54078
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-09-21 15:39:00 +0300

    Fix tests again

--- crates/humanode-runtime/src/tests/currency_swap.rs
+++ crates/humanode-runtime/src/tests/currency_swap.rs
@@ -122,7 +122,7 @@ fn currencies_are_balanced() {
     new_test_ext_with().execute_with(move || {
         assert_eq!(
             BalancedCurrencySwapBridgesInitializer::last_initializer_version(),
-            BridgesInitializerVersion::get()
+            pallet_balanced_currency_swap_bridges_initializer::CURRENT_BRIDGES_INITIALIZER_VERSION
         );
         assert!(BalancedCurrencySwapBridgesInitializer::is_balanced().unwrap());
     })
