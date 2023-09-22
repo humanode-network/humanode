87d31d0f7c3fbe50b756c4b9fbeb6756b61f66c7
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-09-22 15:27:03 +0300

    Add transfer logic into solidity interface

--- crates/precompile-native-currency/WETH.sol
+++ crates/precompile-native-currency/WETH.sol
@@ -27,6 +27,16 @@ interface IWETH {
     function withdraw(uint256 value) external;
 
     /**
+     * Transfer token for a specified address.
+     * Selector: a9059cbb
+     *
+     * @param to The address to transfer tokens to.
+     * @param value The amount to be transferred.
+     * @return true if the transfer was succesful, revert otherwise.
+     */
+    function transfer(address to, uint256 value) external returns (bool);
+
+    /**
      * Event emited when deposit has been called.
      * Selector: e1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c
      *
@@ -43,4 +53,14 @@ interface IWETH {
      * @param value The amount of tokens "unwrapped".
      */
     event Withdrawal(address indexed owner, uint256 value);
+
+    /**
+     * Event emited when a transfer has been performed.
+     * Selector: ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef
+     *
+     * @param from The address sending the tokens
+     * @param to The address receiving the tokens.
+     * @param value The amount of tokens transfered.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
 }
