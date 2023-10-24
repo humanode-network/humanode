c55cffb168269954ec996cdfb28498abd31927ac
MOZGIII <mike-n@narod.ru>
2023-10-24 01:36:15 -0300

    Drop zld from intel mac builds (#813)
    Drop zld

--- .github/scripts/build_env/macos.sh
+++ .github/scripts/build_env/macos.sh
@@ -4,23 +4,4 @@ set -euo pipefail
 brew install \
   coreutils
 
-if [[ "$(uname -m)" == "x86_64" ]]; then
-  echo "Intel-specific setup..."
-
-  brew install \
-    michaeleisel/zld/zld
-
-  ZLD_PATH="$(which zld)"
-
-  "$ZLD_PATH" -v
-
-  cat >>./.cargo/config.toml <<EOF
-[target.x86_64-apple-darwin]
-linker = "clang"
-rustflags = ["-C", "link-arg=-fuse-ld=${ZLD_PATH}"]
-EOF
-
-  cat ./.cargo/config.toml
-fi
-
 .github/scripts/protoc.sh
