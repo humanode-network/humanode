d562c70924689a565a5f60cf06171cc68e6f92bf
MOZGIII <mike-n@narod.ru>
2023-10-25 16:36:35 -0300

    WeHMND E2E tests (#815)
    * Add viem to the e2e tests
    
    * Rename eth.ts to ethEthers.ts
    
    * Add ethViem
    
    * Add WeHMND test

--- utils/e2e-tests/ts/lib/eth.ts
+++ utils/e2e-tests/ts/lib/eth.ts
@@ -1,35 +1,6 @@
-import { HDNodeWallet, ethers, Mnemonic, Wallet } from "ethers";
-import { RunNodeState } from "./node";
-import { arrayMap } from "./jsbase";
-
-export type Provider = ethers.JsonRpcProvider;
-
-export const provider = (url: string): Provider =>
-  new ethers.JsonRpcProvider(url);
-
-export const providerFromNode = (node: RunNodeState): Provider =>
-  provider(node.meta.rpcUrlHttp);
-
 export const SUBSTRATE_DEV_SEED_PHRASE =
   "bottom drive obey lake curtain smoke basket hold race lonely fit walk";
 
 export const ROOT_DEV_ACCOUNT_DERIVATION_PATH = "m/44'/60'/0'/0" as const;
 
 export const DEV_ACCOUNT_INDICIES = [0, 1] as const;
-
-export const devHDNodeWalletRoot = HDNodeWallet.fromMnemonic(
-  Mnemonic.fromPhrase(SUBSTRATE_DEV_SEED_PHRASE),
-  ROOT_DEV_ACCOUNT_DERIVATION_PATH
-);
-
-export const devHDNodeWallets = arrayMap(DEV_ACCOUNT_INDICIES, (accountIndex) =>
-  devHDNodeWalletRoot.deriveChild(accountIndex)
-);
-
-export const devSigners = (provider: Provider) =>
-  arrayMap(
-    devHDNodeWallets,
-    (hdnodeWallet) => new Wallet(hdnodeWallet.privateKey, provider)
-  );
-
-export type DevSigners = ReturnType<typeof devSigners>;
---
+++ utils/e2e-tests/ts/lib/ethEthers.ts
@@ -0,0 +1,33 @@
+import { HDNodeWallet, ethers, Mnemonic, Wallet } from "ethers";
+import { RunNodeState } from "./node";
+import { arrayMap } from "./jsbase";
+import {
+  DEV_ACCOUNT_INDICIES,
+  ROOT_DEV_ACCOUNT_DERIVATION_PATH,
+  SUBSTRATE_DEV_SEED_PHRASE,
+} from "./eth";
+
+export type Provider = ethers.JsonRpcProvider;
+
+export const provider = (url: string): Provider =>
+  new ethers.JsonRpcProvider(url);
+
+export const providerFromNode = (node: RunNodeState): Provider =>
+  provider(node.meta.rpcUrlHttp);
+
+export const devHDNodeWalletRoot = HDNodeWallet.fromMnemonic(
+  Mnemonic.fromPhrase(SUBSTRATE_DEV_SEED_PHRASE),
+  ROOT_DEV_ACCOUNT_DERIVATION_PATH
+);
+
+export const devHDNodeWallets = arrayMap(DEV_ACCOUNT_INDICIES, (accountIndex) =>
+  devHDNodeWalletRoot.deriveChild(accountIndex)
+);
+
+export const devSigners = (provider: Provider) =>
+  arrayMap(
+    devHDNodeWallets,
+    (hdnodeWallet) => new Wallet(hdnodeWallet.privateKey, provider)
+  );
+
+export type DevSigners = ReturnType<typeof devSigners>;
