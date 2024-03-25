16c848a901c420f79f3ca5a094d042c379fb8957
Dmitry Lavrenov <lawrenowdima@gmail.com>
2024-03-25 13:20:50 +0300

    Add draft working live test

---
+++ utils/e2e-tests/bash/tests/try-runtime/live.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+# Set up command.
+COMMAND="$1"
+
+# Run try-runtime live command.
+"$COMMAND" try-runtime \
+  --detailed-log-output \
+  --runtime ./target/release/wbuild/humanode-runtime/humanode_runtime.wasm  \
+  on-runtime-upgrade \
+  live \
+  --uri "wss://explorer-rpc-ws.it6.stages.humanode.io:443"
+
+printf "Test succeded\n" >&2
