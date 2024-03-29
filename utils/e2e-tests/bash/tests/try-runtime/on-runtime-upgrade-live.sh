cefed2ad56f8be42b64a16bb3317bd7e71121f9d
Dmitry Lavrenov <lawrenowdima@gmail.com>
2024-03-29 19:05:01 +0300

    Rename RUNTIME_WASM to RUNTIME_WASM_PATH

--- utils/e2e-tests/bash/tests/try-runtime/on-runtime-upgrade-live.sh
+++ utils/e2e-tests/bash/tests/try-runtime/on-runtime-upgrade-live.sh
@@ -6,7 +6,7 @@ LIVE_NETWORK_URL="wss://explorer-rpc-ws.mainnet.stages.humanode.io:443"
 # Run try-runtime on-runtime-upgrade command.
 "$HUMANODE_PEER_PATH" try-runtime \
   --detailed-log-output \
-  --runtime "$RUNTIME_WASM"  \
+  --runtime "$RUNTIME_WASM_PATH"  \
   on-runtime-upgrade \
   live \
   --uri "$LIVE_NETWORK_URL"
