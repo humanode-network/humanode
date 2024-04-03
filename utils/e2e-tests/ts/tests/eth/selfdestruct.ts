a0d9ff29bc85fb35c369f128a1e2c5ddd70e8936
Dmitry Lavrenov <lawrenowdima@gmail.com>
2024-04-03 15:12:27 +0300

    Working selfdestruct test

--- utils/e2e-tests/ts/tests/eth/selfdestruct.ts
+++ utils/e2e-tests/ts/tests/eth/selfdestruct.ts
@@ -5,7 +5,6 @@ import selfdestruct from "../../lib/abis/selfdestruct";
 import "../../lib/expect";
 import { beforeEachWithCleanup } from "../../lib/lifecycle";
 import * as ethers from "ethers";
-import { getContractAddress } from 'viem';
 
 describe("selfdestruct", () => {
   let node: RunNodeState;
@@ -20,63 +19,53 @@ describe("selfdestruct", () => {
     devClients = eth.devClientsFromNodeWebSocket(node, cleanup.push);
   }, 60 * 1000);
 
-  describe("deploy contract", async () => {
-    let contract: `0x${string}`;
+  it("deploy contract, transfer 1 token unit, call sefldestruct", async () => {
     const transferValue = ethers.parseEther("1");
-    beforeEach(async () => {
-      const [alice, _] = devClients;
+    const [alice, _] = devClients;
 
-      const deploy_contract_tx_hash = await alice.deployContract({
-        abi: selfdestruct.abi,
-        bytecode: selfdestruct.bytecode as `0x${string}`,
-      });
-
-      await publicClient.waitForTransactionReceipt({
-        hash: deploy_contract_tx_hash,
-        timeout: 18_000,
-      });
-
-      contract = getContractAddress({
-        from: alice.account.address,
-        nonce: 1n,
-      });
-
-      const hash = await alice.sendTransaction({
-        to: contract,
-        value: transferValue,
-      });
-
-      await publicClient.waitForTransactionReceipt({
-        hash: hash,
-        timeout: 18_000,
-      });
+    // Deploy contract and get address.
+    const deploy_contract_tx_hash = await alice.deployContract({
+      abi: selfdestruct.abi,
+      bytecode: selfdestruct.bytecode as `0x${string}`,
+    });
+    const deploy_contract_tx_receipt = await publicClient.waitForTransactionReceipt({
+      hash: deploy_contract_tx_hash,
+      timeout: 18_000,
     });
 
-    it("call selfdestruct", async () => {
-      const [alice, bob] = devClients;
+    const contract = deploy_contract_tx_receipt.contractAddress!;
 
-      const contract_balance_before = await publicClient.getBalance({
-        address: contract,
-      });
-
-      expect(contract_balance_before).toBe(transferValue);
-
-      const selfdestructHash = await alice.writeContract({
-        abi: selfdestruct.abi,
-        address: contract,
-        functionName: "close",
-      });
-
-      await publicClient.waitForTransactionReceipt({
-        hash: selfdestructHash,
-        timeout: 18_000,
-      });
-
-      const contract_balance_after = await publicClient.getBalance({
-        address: contract,
-      });
-
-      expect(contract_balance_after).toBe(transferValue);
+    // Send 1 token unit to the contract.
+    const hash = await alice.sendTransaction({
+      to: contract,
+      value: transferValue,
     });
+    await publicClient.waitForTransactionReceipt({
+      hash: hash,
+      timeout: 18_000,
+    });
+
+    // Check balance before executing selfdestruct.
+    const contract_balance_before = await publicClient.getBalance({
+      address: contract,
+    });
+    expect(contract_balance_before).toBe(transferValue);
+
+    // Execute selfdestruct.
+    const selfdestructHash = await alice.writeContract({
+      abi: selfdestruct.abi,
+      address: contract,
+      functionName: "close",
+    });
+    await publicClient.waitForTransactionReceipt({
+      hash: selfdestructHash,
+      timeout: 18_000,
+    });
+
+    // Verify balance after executing selfdestruct.
+    const contract_balance_after = await publicClient.getBalance({
+      address: contract,
+    });
+    expect(contract_balance_after).toBe(transferValue);
   });
 });
