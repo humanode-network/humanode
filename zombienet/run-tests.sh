79e7c94fa417bcafe85258a632d8a6bbf5ad1d56
Dmitry Lavrenov <lawrenowdima@gmail.com>
2024-04-04 18:41:23 +0300

    Remove useless arg

--- zombienet/run-tests.sh
+++ zombienet/run-tests.sh
@@ -3,17 +3,12 @@ set -euo pipefail
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 cd "$SCRIPT_DIR/.."
 
-ZOMBIENET_EXEC_PATH="$(realpath -s "$ZOMBIENET_EXEC_PATH")"
-
 HUMANODE_PEER_PATH="$(realpath -s "$HUMANODE_PEER_PATH")"
 export HUMANODE_PEER_PATH
-
-ZOMBIENET_PLAYGROUND_PATH="./zombienet-plaground"
-export ZOMBIENET_PLAYGROUND_PATH
 
 # Run all zombienet tests.
 for TESTFILE in "$SCRIPT_DIR"/*/*.zndsl; do
   printf "=> Running test %s\n" "$TESTFILE" >&2
-  "$ZOMBIENET_EXEC_PATH" -p native test "$TESTFILE"
+  zombienet -p native test "$TESTFILE"
   printf "=> Test %s passed\n" "$TESTFILE" >&2
 done
