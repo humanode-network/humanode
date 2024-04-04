38be47eb9040585bbb32e701b67cd7f82d11b0c1
Dmitry Lavrenov <lawrenowdima@gmail.com>
2024-04-04 17:55:32 +0300

    Use global run-tests for all zombienet configurations

--- zombienet/run-tests.sh
+++ zombienet/run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-cd "$SCRIPT_DIR/../.."
+cd "$SCRIPT_DIR/.."
 
 ZOMBIENET_EXEC_PATH="$(realpath -s "$ZOMBIENET_EXEC_PATH")"
 
@@ -11,4 +11,9 @@ export HUMANODE_PEER_PATH
 ZOMBIENET_PLAYGROUND_PATH="./zombienet-plaground"
 export ZOMBIENET_PLAYGROUND_PATH
 
-"$ZOMBIENET_EXEC_PATH" -p native test ./zombienet/block-building/block-building.zndsl
+# Run all zombienet tests.
+for TESTFILE in "$SCRIPT_DIR"/*/*.zndsl; do
+  printf "=> Running test %s\n" "$TESTFILE" >&2
+  "$ZOMBIENET_EXEC_PATH" -p native test "$TESTFILE"
+  printf "=> Test %s passed\n" "$TESTFILE" >&2
+done
