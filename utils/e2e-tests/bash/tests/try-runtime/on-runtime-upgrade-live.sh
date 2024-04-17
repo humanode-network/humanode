6659947e5e716bb879c824a160733b51667190d6
Dmitry Lavrenov <lawrenowdima@gmail.com>
2024-04-17 17:13:00 +0300

    Use all try-runtime checks

--- utils/e2e-tests/bash/tests/try-runtime/on-runtime-upgrade-live.sh
+++ utils/e2e-tests/bash/tests/try-runtime/on-runtime-upgrade-live.sh
@@ -8,6 +8,7 @@ LIVE_NETWORK_URL="wss://explorer-rpc-ws.mainnet.stages.humanode.io:443"
   --detailed-log-output \
   --runtime "$RUNTIME_WASM_PATH"  \
   on-runtime-upgrade \
+  --checks=all \
   live \
   --uri "$LIVE_NETWORK_URL"
 
