b50a71cba757307b0a96b29a58b16c69fd9c1590
Dmitry Lavrenov <lawrenowdima@gmail.com>
2024-05-09 15:25:47 +0300

    Add keys related fixtures and modify help check script

--- utils/e2e-tests/bash/tests/base/help.sh
+++ utils/e2e-tests/bash/tests/base/help.sh
@@ -3,23 +3,43 @@ set -euo pipefail
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-# This test ensures we have an adequate and working help command.
-# It also serves as a snapshot test for the help text, so we can track when
-# the help text changes.
+# This test ensures we have an adequate and working help command and subcommands.
+# It also serves as a snapshot test for the help texts, so we can track when
+# the help texts change.
 
-OUTPUT="$("$HUMANODE_PEER_PATH" --help)"
-TEMPLATE="$(cat "$SCRIPT_DIR/../../fixtures/help.stdout.txt")"
+compare() {
+  COMMAND="${@:-""}"
+  FIXTURE="help."${COMMAND// /.}".stdout.txt"
 
-DIFF_CMD_ARGS=(
-  -u
-  -b
-  -I '^humanode-peer {{sha}}$'        # template
-  -I '^humanode-peer [0-9a-f]\{40\}$' # expected
+  echo $FIXTURE
+
+  OUTPUT="$("$HUMANODE_PEER_PATH" $COMMAND --help)"
+  TEMPLATE="$(cat "$SCRIPT_DIR/../../fixtures/$FIXTURE")"
+
+  DIFF_CMD_ARGS=(
+    -u
+    -b
+    -I '^humanode-peer {{sha}}$'        # template
+    -I '^humanode-peer [0-9a-f]\{40\}$' # expected
+  )
+
+  if ! DIFF="$(diff "${DIFF_CMD_ARGS[@]}" <(printf '%s' "$TEMPLATE") <(printf '%s' "$OUTPUT"))"; then
+    printf "Output did not match:\n%s\n" "$DIFF"
+    exit 1
+  fi
+}
+
+COMMANDS=(
+  "key"
+  "key generate-node-key"
+  "key generate"
+  "key inspect"
+  "key inspect-node-key"
+  "key insert"
 )
 
-if ! DIFF="$(diff "${DIFF_CMD_ARGS[@]}" <(printf '%s' "$TEMPLATE") <(printf '%s' "$OUTPUT"))"; then
-  printf "Output did not match:\n%s\n" "$DIFF"
-  exit 1
-fi
+for COMMAND in "${COMMANDS[@]}"; do
+  compare $COMMAND
+done
 
 printf "Test succeded\n" >&2
