c9ef8b0f07d9998c776471edfb155b6370ab7ac5
MOZGIII <mike-n@narod.ru>
2024-09-17 11:13:11 -0300

    `bioauth-flow-rpc` refactor after the `scanResultBlob` integration (#1254)
    * Refactor bioauth flow rpc error structure
    
    * Move signer-related stuff into a mod
    
    * Move the request/response data into a mod

--- crates/bioauth-flow-rpc/src/method/enroll_v2.rs
+++ crates/bioauth-flow-rpc/src/method/enroll_v2.rs
@@ -1,28 +1,30 @@
-//! The `enroll` method error.
+//! The `enroll_v2` method error.
 
-use super::shared;
-use crate::error_data;
+use crate::error;
 
-/// The `enroll` method error kinds.
+/// The `enroll_v2` method error kinds.
 #[derive(Debug)]
-pub struct Error(pub shared::FlowBaseError<robonode_client::EnrollError>);
+pub struct Error(pub error::shared::FlowBaseError<robonode_client::EnrollError>);
 
 impl From<Error> for jsonrpsee::core::Error {
     fn from(err: Error) -> Self {
         err.0
-            .to_jsonrpsee_error::<_, error_data::ShouldRetry>(|call_error| match call_error {
-                robonode_client::EnrollError::FaceScanRejectedNoBlob
-                | robonode_client::EnrollError::FaceScanRejected(_) => {
-                    Some(error_data::ShouldRetry)
+            .to_jsonrpsee_error::<_, error::data::BlobOrRetry>(|call_error| match call_error {
+                robonode_client::EnrollError::FaceScanRejected(ref scan_result_blob)
+                | robonode_client::EnrollError::PersonAlreadyEnrolled(ref scan_result_blob)
+                | robonode_client::EnrollError::LogicInternal(ref scan_result_blob) => {
+                    Some(error::data::ScanResultBlob(scan_result_blob.clone()).into())
+                }
+
+                robonode_client::EnrollError::FaceScanRejectedNoBlob => {
+                    Some(error::data::ShouldRetry.into())
                 }
 
                 robonode_client::EnrollError::InvalidPublicKey
                 | robonode_client::EnrollError::InvalidLivenessData
                 | robonode_client::EnrollError::PublicKeyAlreadyUsed
                 | robonode_client::EnrollError::PersonAlreadyEnrolledNoBlob
-                | robonode_client::EnrollError::PersonAlreadyEnrolled(_)
                 | robonode_client::EnrollError::LogicInternalNoBlob
-                | robonode_client::EnrollError::LogicInternal(_)
                 | robonode_client::EnrollError::UnknownCode(_)
                 | robonode_client::EnrollError::Unknown(_) => None,
             })
@@ -36,11 +38,10 @@ mod tests {
     use rpc_validator_key_logic::Error as ValidatorKeyError;
 
     use super::*;
-    use crate::errors::sign::Error as SignError;
 
     #[test]
     fn error_key_extraction_validator_key_extraction() {
-        let error: jsonrpsee::core::Error = Error(shared::FlowBaseError::KeyExtraction(
+        let error: jsonrpsee::core::Error = Error(error::shared::FlowBaseError::KeyExtraction(
             ValidatorKeyError::ValidatorKeyExtraction,
         ))
         .into();
@@ -55,7 +56,7 @@ mod tests {
 
     #[test]
     fn error_key_extraction_missing_validator_key() {
-        let error: jsonrpsee::core::Error = Error(shared::FlowBaseError::KeyExtraction(
+        let error: jsonrpsee::core::Error = Error(error::shared::FlowBaseError::KeyExtraction(
             ValidatorKeyError::MissingValidatorKey,
         ))
         .into();
@@ -70,14 +71,16 @@ mod tests {
 
     #[test]
     fn error_robonode_face_scan_rejected() {
-        let error: jsonrpsee::core::Error = Error(shared::FlowBaseError::RobonodeClient(
-            robonode_client::Error::Call(robonode_client::EnrollError::FaceScanRejectedNoBlob),
+        let error: jsonrpsee::core::Error = Error(error::shared::FlowBaseError::RobonodeClient(
+            robonode_client::Error::Call(robonode_client::EnrollError::FaceScanRejected(
+                "scan result blob".to_owned(),
+            )),
         ))
         .into();
         let error: ErrorObject = error.into();
 
         let expected_error_message =
-            "{\"code\":200,\"message\":\"server error: face scan rejected\",\"data\":{\"shouldRetry\":true}}";
+            "{\"code\":200,\"message\":\"server error: face scan rejected\",\"data\":{\"scanResultBlob\":\"scan result blob\"}}";
         assert_eq!(
             expected_error_message,
             serde_json::to_string(&error).unwrap()
@@ -86,14 +89,16 @@ mod tests {
 
     #[test]
     fn error_robonode_logic_internal() {
-        let error: jsonrpsee::core::Error = Error(shared::FlowBaseError::RobonodeClient(
-            robonode_client::Error::Call(robonode_client::EnrollError::LogicInternalNoBlob),
+        let error: jsonrpsee::core::Error = Error(error::shared::FlowBaseError::RobonodeClient(
+            robonode_client::Error::Call(robonode_client::EnrollError::LogicInternal(
+                "scan result blob".to_owned(),
+            )),
         ))
         .into();
         let error: ErrorObject = error.into();
 
         let expected_error_message =
-            "{\"code\":200,\"message\":\"server error: logic internal error\"}";
+            "{\"code\":200,\"message\":\"server error: logic internal error\",\"data\":{\"scanResultBlob\":\"scan result blob\"}}";
         assert_eq!(
             expected_error_message,
             serde_json::to_string(&error).unwrap()
@@ -102,7 +107,7 @@ mod tests {
 
     #[test]
     fn error_robonode_other() {
-        let error: jsonrpsee::core::Error = Error(shared::FlowBaseError::RobonodeClient(
+        let error: jsonrpsee::core::Error = Error(error::shared::FlowBaseError::RobonodeClient(
             robonode_client::Error::Call(robonode_client::EnrollError::Unknown("test".to_owned())),
         ))
         .into();
@@ -118,8 +123,10 @@ mod tests {
 
     #[test]
     fn error_sign() {
-        let error: jsonrpsee::core::Error =
-            Error(shared::FlowBaseError::Sign(SignError::SigningFailed)).into();
+        let error: jsonrpsee::core::Error = Error(error::shared::FlowBaseError::Sign(
+            error::Sign::SigningFailed,
+        ))
+        .into();
         let error: ErrorObject = error.into();
 
         let expected_error_message = "{\"code\":100,\"message\":\"signing failed\"}";
