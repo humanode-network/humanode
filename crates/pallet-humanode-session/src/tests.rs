e7624f9ef200a316a137113b5039f106e0201198
Dmitry Lavrenov <lawrenowdima@gmail.com>
2024-10-07 17:24:51 +0300

    Implement full_ban_unban_lifecycle test

--- crates/pallet-humanode-session/src/tests.rs
+++ crates/pallet-humanode-session/src/tests.rs
@@ -1,15 +1,44 @@
 //! The tests for the pallet.
 
+// Allow simple integer arithmetic in tests.
+#![allow(clippy::arithmetic_side_effects)]
+
 use std::collections::BTreeSet;
 
-use frame_support::{assert_noop, assert_ok};
-use mock::{HumanodeSession, RuntimeOrigin};
-use sp_runtime::BoundedBTreeSet;
+use frame_support::{assert_noop, assert_ok, pallet_prelude::*};
+use mockall::predicate;
+use sp_runtime::{traits::BlockNumberProvider, BoundedBTreeSet};
 
 use crate::{
-    mock::{new_test_ext, Test, TestExternalitiesExt},
+    mock::{
+        new_test_ext, Bioauth, HumanodeSession, MockShouldEndSession, RuntimeOrigin, Session,
+        System, Test, TestExternalitiesExt,
+    },
     *,
 };
+
+/// A helper function to rotate session.
+fn rotate_session() {
+    let next_block_number = System::current_block_number() + 1;
+    let session_index_before = Session::current_index();
+
+    // Set mock expectations.
+    let should_end_session_ctx = MockShouldEndSession::should_end_session_context();
+    should_end_session_ctx
+        .expect()
+        .once()
+        .with(predicate::eq(next_block_number))
+        .return_const(true);
+
+    // Invoke new block initialization.
+    Session::on_initialize(next_block_number);
+
+    // Assert state changes.
+    assert_eq!(Session::current_index(), session_index_before + 1);
+
+    // Assert mock invocations.
+    should_end_session_ctx.checkpoint();
+}
 
 /// This test verifies that basic setup works in the happy path.
 #[test]
@@ -111,6 +140,167 @@ fn unban_fails_account_is_not_banned() {
         assert_noop!(
             HumanodeSession::unban(RuntimeOrigin::root(), 1),
             Error::<Test>::AccountIsNotBanned
+        );
+    });
+}
+
+/// This test verifies full ban and unban lifecycle logic.
+#[test]
+fn full_ban_unban_lifecycle() {
+    new_test_ext().execute_with_ext(|_| {
+        // Check initial state.
+        assert!(<BannedAccounts<Test>>::get().is_empty());
+        assert_eq!(
+            Bioauth::active_authentications(),
+            vec![pallet_bioauth::Authentication {
+                public_key: 1,
+                expires_at: 1000,
+            }]
+        );
+        assert_eq!(Session::validators(), vec![42, 43, 44, 1]);
+        assert_eq!(
+            Session::queued_keys(),
+            vec![
+                (42, sp_runtime::testing::UintAuthorityId(42)),
+                (43, sp_runtime::testing::UintAuthorityId(43)),
+                (44, sp_runtime::testing::UintAuthorityId(44)),
+                (1, sp_runtime::testing::UintAuthorityId(1)),
+            ]
+        );
+
+        // Invoke the ban under test.
+        assert_ok!(HumanodeSession::ban(RuntimeOrigin::root(), 1));
+
+        // Assert state changes.
+        assert_eq!(
+            <BannedAccounts<Test>>::get().into_inner(),
+            vec![1].into_iter().collect::<_>()
+        );
+        assert_eq!(
+            Bioauth::active_authentications(),
+            vec![pallet_bioauth::Authentication {
+                public_key: 1,
+                expires_at: 1000,
+            }]
+        );
+        assert_eq!(Session::validators(), vec![42, 43, 44, 1]);
+        assert_eq!(
+            Session::queued_keys(),
+            vec![
+                (42, sp_runtime::testing::UintAuthorityId(42)),
+                (43, sp_runtime::testing::UintAuthorityId(43)),
+                (44, sp_runtime::testing::UintAuthorityId(44)),
+                (1, sp_runtime::testing::UintAuthorityId(1)),
+            ]
+        );
+
+        // Rotate session.
+        rotate_session();
+
+        // Assert state changes.
+        //
+        // Expect that the banned account hasn't been added to queued keys.
+        assert_eq!(
+            <BannedAccounts<Test>>::get().into_inner(),
+            vec![1].into_iter().collect::<_>()
+        );
+        assert_eq!(
+            Bioauth::active_authentications(),
+            vec![pallet_bioauth::Authentication {
+                public_key: 1,
+                expires_at: 1000,
+            }]
+        );
+        assert_eq!(Session::validators(), vec![42, 43, 44, 1]);
+        assert_eq!(
+            Session::queued_keys(),
+            vec![
+                (42, sp_runtime::testing::UintAuthorityId(42)),
+                (43, sp_runtime::testing::UintAuthorityId(43)),
+                (44, sp_runtime::testing::UintAuthorityId(44)),
+            ]
+        );
+
+        // Rotate session.
+        rotate_session();
+
+        // Assert state changes.
+        //
+        // Expect that the banned account has been removed from validators list
+        // and hasn't been added to queued keys.
+        assert_eq!(
+            <BannedAccounts<Test>>::get().into_inner(),
+            vec![1].into_iter().collect::<_>()
+        );
+        assert_eq!(
+            Bioauth::active_authentications(),
+            vec![pallet_bioauth::Authentication {
+                public_key: 1,
+                expires_at: 1000,
+            }]
+        );
+        assert_eq!(Session::validators(), vec![42, 43, 44]);
+        assert_eq!(
+            Session::queued_keys(),
+            vec![
+                (42, sp_runtime::testing::UintAuthorityId(42)),
+                (43, sp_runtime::testing::UintAuthorityId(43)),
+                (44, sp_runtime::testing::UintAuthorityId(44)),
+            ]
+        );
+
+        // Invoke the unban under test.
+        assert_ok!(HumanodeSession::unban(RuntimeOrigin::root(), 1));
+
+        // Rotate session.
+        rotate_session();
+
+        // Assert state changes.
+        //
+        // Expect that the unbanned account has been added to queued keys.
+        assert!(<BannedAccounts<Test>>::get().is_empty());
+        assert_eq!(
+            Bioauth::active_authentications(),
+            vec![pallet_bioauth::Authentication {
+                public_key: 1,
+                expires_at: 1000,
+            }]
+        );
+        assert_eq!(Session::validators(), vec![42, 43, 44]);
+        assert_eq!(
+            Session::queued_keys(),
+            vec![
+                (42, sp_runtime::testing::UintAuthorityId(42)),
+                (43, sp_runtime::testing::UintAuthorityId(43)),
+                (44, sp_runtime::testing::UintAuthorityId(44)),
+                (1, sp_runtime::testing::UintAuthorityId(1)),
+            ]
+        );
+
+        // Rotate session.
+        rotate_session();
+
+        // Assert state changes.
+        //
+        // Expect that the unbanned account has been included to validators list
+        // and has been added to queued keys.
+        assert!(<BannedAccounts<Test>>::get().is_empty());
+        assert_eq!(
+            Bioauth::active_authentications(),
+            vec![pallet_bioauth::Authentication {
+                public_key: 1,
+                expires_at: 1000,
+            }]
+        );
+        assert_eq!(Session::validators(), vec![42, 43, 44, 1]);
+        assert_eq!(
+            Session::queued_keys(),
+            vec![
+                (42, sp_runtime::testing::UintAuthorityId(42)),
+                (43, sp_runtime::testing::UintAuthorityId(43)),
+                (44, sp_runtime::testing::UintAuthorityId(44)),
+                (1, sp_runtime::testing::UintAuthorityId(1)),
+            ]
         );
     });
 }
