32de381ddd93482f9f197b805a5a12d3b7888d48
Dmitry Lavrenov <lawrenowdima@gmail.com>
2024-10-07 13:31:38 +0300

    Add pallet-humanode-session to be benchmarked at humanode-runtime

--- crates/humanode-runtime/src/lib.rs
+++ crates/humanode-runtime/src/lib.rs
@@ -1060,6 +1060,7 @@ mod benches {
         [pallet_token_claims, TokenClaims]
         [pallet_utility, Utility]
         [pallet_vesting, Vesting]
+        [pallet_humanode_session, HumanodeSession]
     );
 }
 
