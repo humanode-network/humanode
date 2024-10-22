a46fde0966693ab8cc94656009f5a1b21685ac36
Dmitry Lavrenov <lawrenowdima@gmail.com>
2024-10-22 14:16:32 +0300

    Rerun benchmarks

--- crates/humanode-runtime/src/weights/pallet_utility.rs
+++ crates/humanode-runtime/src/weights/pallet_utility.rs
@@ -17,7 +17,7 @@ impl<T: frame_system::Config> pallet_utility::WeightInfo for WeightInfo<T> {
     //  Measured:  `0`
     //  Estimated: `0`
     // Minimum execution time: 4_000_000 picoseconds.
-    Weight::from_parts(1_870_000_000, 0)
+    Weight::from_parts(2_018_000_000, 0)
   }
   fn as_derivative() -> Weight {
     // Proof Size summary in bytes:
@@ -31,8 +31,8 @@ impl<T: frame_system::Config> pallet_utility::WeightInfo for WeightInfo<T> {
     // Proof Size summary in bytes:
     //  Measured:  `0`
     //  Estimated: `0`
-    // Minimum execution time: 3_000_000 picoseconds.
-    Weight::from_parts(2_078_000_000, 0)
+    // Minimum execution time: 4_000_000 picoseconds.
+    Weight::from_parts(2_029_000_000, 0)
   }
   fn dispatch_as() -> Weight {
     // Proof Size summary in bytes:
@@ -46,7 +46,7 @@ impl<T: frame_system::Config> pallet_utility::WeightInfo for WeightInfo<T> {
     // Proof Size summary in bytes:
     //  Measured:  `0`
     //  Estimated: `0`
-    // Minimum execution time: 4_000_000 picoseconds.
-    Weight::from_parts(1_870_000_000, 0)
+    // Minimum execution time: 3_000_000 picoseconds.
+    Weight::from_parts(2_009_000_000, 0)
   }
 }
