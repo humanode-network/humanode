93d7b8e5e407f045484bb2c9493833f80c50d10d
Dmitry Lavrenov <39522748+dmitrylavrenov@users.noreply.github.com>
2025-02-07 10:19:59 +0300

    Properly manage contract accounts creation and removal logic (#140)
    * Introduce has_code to AccountInfo at pallet-evm-system
    
    * Improve AccountProvider related naming
    
    * Rework tests logic
    
    * Use had_code in tuple
    
    * Apply fmt
    
    * Set has_code to true in case account exists
    
    * Some renaming
    
    * Explicitly set has_code to true at create_contract_account_fails test
    
    * Introduce evm managed terminologies
    
    * Apply fmt
    
    * Improve comments in tests
    
    * Add create_evm_managed_account_works_already_exists test
    
    * Add more data checks in tests
    
    * Use assert_storage_noop
    
    * Improve docs for create_evm_managed_account call
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>
    
    * Improve docs for remove_evm_managed_account call
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>
    
    * Improve some docs
    
    * Properly unmark that account is not managed by EVM
    
    * Unify tests naming semantic
    
    * Apply fmt
    
    * Add migrations logic (#148)
    
    * Add STORAGE_VERSION usage
    
    * Add migrations structure
    
    * Implement OnRuntimeUpgrade for MigrationV0ToV1
    
    * Some improvements
    
    * Implement logic for pre_upgrade and post_upgrade
    
    * Use reference in EvmProvider
    
    * Fix vec usage at try-runtime
    
    * Properly use translate
    
    * Use u64 for counting at try-runtime
    
    * Properly use old Account storage alias
    
    * Ensure storafe data is updated correctly
    
    * Some renaming
    
    * Renaming after another iteration of investigations
    
    * Return prev implementation
    
    * Dissallow removing account as pub method
    
    * Minor refactoring
    
    * Don't use managed_by_evm field
    
    * Remove storage version usage
    
    * Migrations renaming
    
    * Remove unused logic at migrations and leave todo for future implementations
    
    * Working migrations with todo
    
    * Properly use nonce related operations
    
    * Apply introduced logic in tests
    
    * Apply fmt
    
    * Tests refactoring
    
    * Minor improvements
    
    * Remove unused error
    
    * Use create_account at account provider
    
    * More info to comment
    
    * Remove migrations from current code changes
    
    * Use on_account_self_destruct naming
    
    ---------
    
    Co-authored-by: MOZGIII <mike-n@narod.ru>

--- frame/evm-system/src/tests.rs
+++ frame/evm-system/src/tests.rs
@@ -8,9 +8,10 @@ use sp_core::H160;
 
 use crate::{mock::*, *};
 
-/// This test verifies that creating account works in the happy path.
+/// This test verifies that creating an account works as expected
+/// in case a new account should be created.
 #[test]
-fn create_account_works() {
+fn create_account_created() {
 	new_test_ext().execute_with_ext(|_| {
 		// Prepare test data.
 		let account_id = H160::from_str("1000000000000000000000000000000000000001").unwrap();
@@ -37,6 +38,10 @@ fn create_account_works() {
 
 		// Assert state changes.
 		assert!(EvmSystem::account_exists(&account_id));
+		assert_eq!(
+			<Account<Test>>::get(&account_id),
+			AccountInfo::<_, _>::default()
+		);
 		System::assert_has_event(RuntimeEvent::EvmSystem(Event::NewAccount {
 			account: account_id,
 		}));
@@ -46,9 +51,10 @@ fn create_account_works() {
 	});
 }
 
-/// This test verifies that creating account fails when the account already exists.
+/// This test verifies that creating an account works as expected
+/// when the account already exists.
 #[test]
-fn create_account_fails() {
+fn create_account_already_exists() {
 	new_test_ext().execute_with_ext(|_| {
 		// Prepare test data.
 		let account_id = H160::from_str("1000000000000000000000000000000000000001").unwrap();
@@ -58,57 +64,6 @@ fn create_account_fails() {
 		assert_storage_noop!(assert_eq!(
 			EvmSystem::create_account(&account_id),
 			AccountCreationOutcome::AlreadyExists
-		));
-	});
-}
-
-/// This test verifies that removing account works in the happy path.
-#[test]
-fn remove_account_works() {
-	new_test_ext().execute_with_ext(|_| {
-		// Prepare test data.
-		let account_id = H160::from_str("1000000000000000000000000000000000000001").unwrap();
-		<Account<Test>>::insert(account_id.clone(), AccountInfo::<_, _>::default());
-
-		// Set block number to enable events.
-		System::set_block_number(1);
-
-		// Set mock expectations.
-		let on_killed_account_ctx = MockDummyOnKilledAccount::on_killed_account_context();
-		on_killed_account_ctx
-			.expect()
-			.once()
-			.with(predicate::eq(account_id))
-			.return_const(());
-
-		// Invoke the function under test.
-		assert_eq!(
-			EvmSystem::remove_account(&account_id),
-			AccountRemovalOutcome::Reaped
-		);
-
-		// Assert state changes.
-		assert!(!EvmSystem::account_exists(&account_id));
-		System::assert_has_event(RuntimeEvent::EvmSystem(Event::KilledAccount {
-			account: account_id,
-		}));
-
-		// Assert mock invocations.
-		on_killed_account_ctx.checkpoint();
-	});
-}
-
-/// This test verifies that removing account fails when the account doesn't exist.
-#[test]
-fn remove_account_fails() {
-	new_test_ext().execute_with_ext(|_| {
-		// Prepare test data.
-		let account_id = H160::from_str("1000000000000000000000000000000000000001").unwrap();
-
-		// Invoke the function under test.
-		assert_storage_noop!(assert_eq!(
-			EvmSystem::remove_account(&account_id),
-			AccountRemovalOutcome::DidNotExist
 		));
 	});
 }
@@ -212,7 +167,13 @@ fn try_mutate_exists_account_created() {
 
 		// Assert state changes.
 		assert!(EvmSystem::account_exists(&account_id));
-		assert_eq!(EvmSystem::get(&account_id), 1);
+		assert_eq!(
+			<Account<Test>>::get(&account_id),
+			AccountInfo {
+				data: 1,
+				..Default::default()
+			}
+		);
 		System::assert_has_event(RuntimeEvent::EvmSystem(Event::NewAccount {
 			account: account_id,
 		}));
@@ -250,20 +211,24 @@ fn try_mutate_exists_account_updated() {
 
 		// Assert state changes.
 		assert!(EvmSystem::account_exists(&account_id));
-		assert_eq!(EvmSystem::get(&account_id), data + 1);
+		assert_eq!(
+			<Account<Test>>::get(&account_id),
+			AccountInfo {
+				nonce,
+				data: data + 1,
+			}
+		);
 	});
 }
 
 /// This test verifies that try_mutate_exists works as expected in case data was providing
-/// and returned data is `None`. As a result, the account has been removed.
+/// and returned data is `None`, account has zero nonce. As a result, the account has been removed.
 #[test]
 fn try_mutate_exists_account_removed() {
 	new_test_ext().execute_with_ext(|_| {
 		// Prepare test data.
 		let account_id = H160::from_str("1000000000000000000000000000000000000001").unwrap();
-		let nonce = 1;
-		let data = 1;
-		<Account<Test>>::insert(account_id.clone(), AccountInfo { nonce, data });
+		<Account<Test>>::insert(account_id.clone(), AccountInfo::<_, _>::default());
 
 		// Check test preconditions.
 		assert!(EvmSystem::account_exists(&account_id));
@@ -297,6 +262,41 @@ fn try_mutate_exists_account_removed() {
 	});
 }
 
+/// This test verifies that try_mutate_exists works as expected in case data was providing
+/// and returned data is `None`, account has non zero nonce. As a result, the account has been retained.
+#[test]
+fn try_mutate_exists_account_retained() {
+	new_test_ext().execute_with_ext(|_| {
+		// Prepare test data.
+		let account_id = H160::from_str("1000000000000000000000000000000000000001").unwrap();
+		let nonce = 10;
+		let data = 100;
+
+		let account_info = AccountInfo { nonce, data };
+		<Account<Test>>::insert(account_id.clone(), account_info);
+
+		// Check test preconditions.
+		assert!(EvmSystem::account_exists(&account_id));
+
+		// Invoke the function under test.
+		EvmSystem::try_mutate_exists(&account_id, |maybe_data| -> Result<(), DispatchError> {
+			*maybe_data = None;
+			Ok(())
+		})
+		.unwrap();
+
+		// Assert state changes.
+		assert!(EvmSystem::account_exists(&account_id));
+		assert_eq!(
+			<Account<Test>>::get(&account_id),
+			AccountInfo {
+				nonce,
+				..Default::default()
+			}
+		);
+	});
+}
+
 /// This test verifies that try_mutate_exists works as expected in case data wasn't providing
 /// and returned data is `None`. As a result, the account hasn't been created.
 #[test]
@@ -312,27 +312,25 @@ fn try_mutate_exists_account_not_created() {
 		System::set_block_number(1);
 
 		// Invoke the function under test.
-		<Account<Test>>::try_mutate_exists(account_id, |maybe_data| -> Result<(), ()> {
-			*maybe_data = None;
-			Ok(())
-		})
-		.unwrap();
-
-		// Assert state changes.
-		assert!(!EvmSystem::account_exists(&account_id));
+		assert_storage_noop!(<Account<Test>>::try_mutate_exists(
+			account_id,
+			|maybe_data| -> Result<(), ()> {
+				*maybe_data = None;
+				Ok(())
+			}
+		)
+		.unwrap());
 	});
 }
 
 /// This test verifies that try_mutate_exists works as expected in case getting error
 /// during data mutation.
 #[test]
-fn try_mutate_exists_fails_without_changes() {
+fn try_mutate_exists_without_changes() {
 	new_test_ext().execute_with_ext(|_| {
 		// Prepare test data.
 		let account_id = H160::from_str("1000000000000000000000000000000000000001").unwrap();
-		let nonce = 1;
-		let data = 1;
-		<Account<Test>>::insert(account_id.clone(), AccountInfo { nonce, data });
+		<Account<Test>>::insert(account_id.clone(), AccountInfo::<_, _>::default());
 
 		// Check test preconditions.
 		assert!(EvmSystem::account_exists(&account_id));
@@ -345,9 +343,5 @@ fn try_mutate_exists_fails_without_changes() {
 			}),
 			()
 		);
-
-		// Assert state changes.
-		assert!(EvmSystem::account_exists(&account_id));
-		assert_eq!(EvmSystem::get(&account_id), data);
 	});
 }
