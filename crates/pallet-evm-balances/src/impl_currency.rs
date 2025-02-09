d3767c86bae7b2914aac1a77d86239eeb812528a
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-02-09 21:12:33 +0300

    Prettify try_mutate_account_handling_dust usage

--- crates/pallet-evm-balances/src/impl_currency.rs
+++ crates/pallet-evm-balances/src/impl_currency.rs
@@ -131,23 +131,18 @@ where
             return (NegativeImbalance::zero(), value);
         }
 
-        let mutate_closure = |account: &mut AccountData<<T as Config<I>>::Balance>| -> Result<
-            (Self::NegativeImbalance, Self::Balance),
-            DispatchError,
-        > {
-            // Best value is the most amount we can slash following liveness rules.
-            let actual = value.min(account.free);
-            account.free.saturating_reduce(actual);
-            let remaining = value.saturating_sub(actual);
-            Ok((NegativeImbalance::new(actual), remaining))
-        };
-
-        match Self::try_mutate_account_handling_dust(
+        let result = Self::try_mutate_account_handling_dust(
             who,
             |account, _is_new| -> Result<(Self::NegativeImbalance, Self::Balance), DispatchError> {
-                mutate_closure(account)
+                // Best value is the most amount we can slash following liveness rules.
+                let actual = value.min(account.free);
+                account.free.saturating_reduce(actual);
+                let remaining = value.saturating_sub(actual);
+                Ok((NegativeImbalance::new(actual), remaining))
             },
-        ) {
+        );
+
+        match result {
             Ok((imbalance, remaining)) => {
                 Self::deposit_event(Event::Slashed {
                     who: who.clone(),
