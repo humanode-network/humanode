a972a9f0a1b3c2fcfd3d7e8fbdfd4af536762291
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-02-25 11:46:18 +0300

    Implement mock testing environment with basic setup works test

---
+++ crates/pallet-evm-swap/src/tests/mod.rs
@@ -0,0 +1,24 @@
+use sp_core::Get;
+
+use crate::{mock::*, *};
+
+/// This test verifies that basic genesis setup works in the happy path.
+#[test]
+fn basic_setup_works() {
+    new_test_ext().execute_with_ext(|_| {
+        assert_eq!(
+            Balances::total_balance(&BridgePotNative::get()),
+            INIT_BALANCE
+        );
+        assert_eq!(Balances::total_balance(&alice()), INIT_BALANCE);
+        assert_eq!(Balances::total_balance(&bob()), INIT_BALANCE);
+        assert_eq!(
+            EvmBalances::total_balance(&BridgePotEvm::get()),
+            INIT_BALANCE
+        );
+        assert_eq!(EvmBalances::total_balance(&alice_evm()), INIT_BALANCE);
+        assert_eq!(EvmBalances::total_balance(&bob_evm()), INIT_BALANCE);
+
+        assert_eq!(Balances::total_issuance(), EvmBalances::total_issuance());
+    });
+}
