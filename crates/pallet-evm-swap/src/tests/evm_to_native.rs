fc662b8e4940cac54c716e59c02cc35dcf790815
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-02-25 14:23:44 +0300

    Add more ethereum checks for evm to native swap tests

--- crates/pallet-evm-swap/src/tests/evm_to_native.rs
+++ crates/pallet-evm-swap/src/tests/evm_to_native.rs
@@ -1,14 +1,14 @@
-use pallet_evm::{Config, Runner};
+use fp_ethereum::ValidatedTransaction;
+use fp_evm::{ExitReason, ExitSucceed};
+use frame_support::{
+    dispatch::{Pays, PostDispatchInfo},
+    traits::OnFinalize,
+};
+use pallet_evm::GasWeightMapping;
 use precompile_utils::{EvmDataWriter, LogsBuilder};
 use sp_core::H256;
 
 use crate::{mock::*, *};
-
-/// A test utility that performs gas to fee computation.
-/// Might not be explicitly correct, but does the job.
-fn gas_to_fee(gas: u64) -> Balance {
-    u128::from(gas) * u128::try_from(*GAS_PRICE).unwrap()
-}
 
 /// This test verifies that the swap precompile call works in the happy path.
 #[test]
@@ -20,7 +20,6 @@ fn swap_works() {
         let swap_balance = 100;
 
         let expected_gas_usage: u64 = 21216 + 200;
-        let expected_fee: Balance = gas_to_fee(expected_gas_usage);
 
         // Check test preconditions.
         assert_eq!(EvmBalances::total_balance(&alice_evm()), INIT_BALANCE);
@@ -30,48 +29,47 @@ fn swap_works() {
         System::set_block_number(1);
 
         // Prepare EVM call.
-        let input = EvmDataWriter::new_with_selector(precompile::Action::Swap)
-            .write(H256::from(swap_native_account.as_ref()))
-            .build();
+        let transaction = pallet_ethereum::Transaction::EIP1559(ethereum::EIP1559Transaction {
+            chain_id: <Test as pallet_evm::Config>::ChainId::get(),
+            nonce: pallet_evm::Pallet::<Test>::account_basic(&alice_evm())
+                .0
+                .nonce,
+            max_priority_fee_per_gas: 0.into(),
+            max_fee_per_gas: 0.into(),
+            gas_limit: 50_000.into(), // a reasonable upper bound for tests
+            action: ethereum::TransactionAction::Call(*PRECOMPILE_ADDRESS),
+            value: U256::from(swap_balance),
+            input: EvmDataWriter::new_with_selector(precompile::Action::Swap)
+                .write(H256::from(swap_native_account.as_ref()))
+                .build(),
+            access_list: Default::default(),
+            odd_y_parity: false,
+            r: Default::default(),
+            s: Default::default(),
+        });
+        let transaction_hash = transaction.hash();
 
         // Invoke the function under test.
-        let execinfo = <Test as pallet_evm::Config>::Runner::call(
-            alice_evm(),
-            *PRECOMPILE_ADDRESS,
-            input,
-            swap_balance.into(),
-            50_000, // a reasonable upper bound for tests
-            Some(*GAS_PRICE),
-            Some(*GAS_PRICE),
-            None,
-            Vec::new(),
-            true,
-            true,
-            None,
-            None,
-            Test::config(),
-        )
-        .unwrap();
+        let post_info =
+            pallet_ethereum::ValidatedTransaction::<Test>::apply(alice_evm(), transaction).unwrap();
+
         assert_eq!(
-            execinfo.exit_reason,
-            fp_evm::ExitReason::Succeed(fp_evm::ExitSucceed::Returned)
-        );
-        assert_eq!(execinfo.used_gas.standard, expected_gas_usage.into());
-        assert_eq!(execinfo.value, EvmDataWriter::new().write(true).build());
-        assert_eq!(
-            execinfo.logs,
-            vec![LogsBuilder::new(*PRECOMPILE_ADDRESS).log3(
-                precompile::SELECTOR_LOG_SWAP,
-                alice_evm(),
-                H256::from(swap_native_account.as_ref()),
-                EvmDataWriter::new().write(swap_balance).build(),
-            )]
+            post_info,
+            PostDispatchInfo {
+                actual_weight: Some(
+                    <Test as pallet_evm::Config>::GasWeightMapping::gas_to_weight(
+                        expected_gas_usage,
+                        true
+                    )
+                ),
+                pays_fee: Pays::No
+            },
         );
 
         // Assert state changes.
         assert_eq!(
             EvmBalances::total_balance(&alice_evm()),
-            INIT_BALANCE - swap_balance - expected_fee
+            INIT_BALANCE - swap_balance
         );
         assert_eq!(
             EvmBalances::total_balance(&BridgePotEvm::get()),
@@ -81,6 +79,32 @@ fn swap_works() {
         assert_eq!(
             Balances::total_balance(&BridgePotNative::get()),
             INIT_BALANCE - swap_balance
+        );
+        System::assert_has_event(RuntimeEvent::Ethereum(pallet_ethereum::Event::Executed {
+            from: alice_evm(),
+            to: *PRECOMPILE_ADDRESS,
+            transaction_hash,
+            exit_reason: ExitReason::Succeed(ExitSucceed::Returned),
+            extra_data: vec![],
+        }));
+
+        // Finalize block to check transaction logs.
+        Ethereum::on_finalize(1);
+
+        let transaction_logs = pallet_ethereum::pallet::CurrentTransactionStatuses::<Test>::get()
+            .unwrap()
+            .first()
+            .cloned()
+            .unwrap()
+            .logs;
+        assert_eq!(
+            transaction_logs,
+            vec![LogsBuilder::new(*PRECOMPILE_ADDRESS).log3(
+                precompile::SELECTOR_LOG_SWAP,
+                alice_evm(),
+                H256::from(swap_native_account.as_ref()),
+                EvmDataWriter::new().write(swap_balance).build(),
+            )]
         );
     });
 }
