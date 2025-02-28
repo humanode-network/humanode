2b7396c936027bc3f5cfbde894ea4e8b3aecc74b
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-02-28 18:30:45 +0300

    Remove description of the interface from abi

--- utils/e2e-tests/ts/lib/abis/evmSwap.ts
+++ utils/e2e-tests/ts/lib/abis/evmSwap.ts
@@ -1,22 +1,3 @@
-// /**
-//  * @title Evm Swap Interface
-//  *
-//  * An interface enabling swapping the funds from EVM accounts to
-//  * native Substrate accounts.
-//  *
-//  * Address: 0x0000000000000000000000000000000000000901
-//  */
-// interface EvmSwap {
-//   /**
-//    * Transfer the funds from an EVM account to native substrate account.
-//    * Selector: 76467cbd
-//    *
-//    * @param nativeAddress The native address to send the funds to.
-//    * @return success Whether or not the swap was successful.
-//    */
-//   function swap(bytes32 nativeAddress) external payable returns (bool success);
-// }
-
 export default {
   abi: [
     {
