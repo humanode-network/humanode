222d9d4120ecb4085a8140d74a226d27a8d9e777
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-03-03 10:25:56 +0300

    Move getNativeBalance to swap utils

--- utils/e2e-tests/ts/tests/swap/evmToNative.ts
+++ utils/e2e-tests/ts/tests/swap/evmToNative.ts
@@ -6,27 +6,7 @@ import { beforeEachWithCleanup } from "../../lib/lifecycle";
 import evmSwap from "../../lib/abis/evmSwap";
 import { decodeEventLog } from "viem";
 import * as substrate from "../../lib/substrate";
-
-type SystemAccount = {
-  data: {
-    free: bigint;
-  };
-};
-
-/// A helper function to get balance of native account.
-const getNativeBalance = async (
-  substrateApi: substrate.Api,
-  nativeAccount: string,
-) => {
-  const systemAccount = (await substrateApi.query["system"]?.["account"]?.(
-    nativeAccount,
-  )) as unknown as SystemAccount;
-
-  const free = systemAccount.data.free;
-
-  // We should explicitly convert to native bigint for future math operations.
-  return BigInt(free);
-};
+import { getNativeBalance } from "../swap/utils";
 
 const evmSwapPrecompileAddress = "0x0000000000000000000000000000000000000901";
 const bridgePotEvmAddress = "0x6d6f646c686d63732f656e310000000000000000";
