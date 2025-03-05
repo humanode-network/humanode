b71246bc0ee068efe9d36b9776bf6c29e1e2f629
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-03-05 11:21:50 +0300

    Add event description to swap precompile interface

--- crates/precompile-evm-swap/EvmSwap.sol
+++ crates/precompile-evm-swap/EvmSwap.sol
@@ -19,4 +19,14 @@ interface EvmSwap {
    * @return success Whether or not the swap was successful.
    */
   function swap(bytes32 nativeAddress) external payable returns (bool success);
+
+  /**
+   * Event emitted when a transfer has been performed.
+   * Selector: 69d31d1d87c1206beee49fbab11570a7f001121cf21fbb234d5b0a2473fa5c58
+   *
+   * @param from The EVM account id the tokens withdrawed from.
+   * @param to The Native account id the tokens deposited to.
+   * @param value The amount of tokens swapped.
+   */
+  event Swap(address indexed from, bytes32 indexed to, uint256 value);
 }
