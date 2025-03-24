ec4a4c8b71d9930da5cdb7eeb1bf899b889cf6cc
Petr Ermishkin <petr@humanode.io>
2025-03-24 13:03:48 +0300

    Fix CLI help for `--rpc-url` parameter

--- crates/humanode-peer/src/cli/params.rs
+++ crates/humanode-peer/src/cli/params.rs
@@ -22,7 +22,7 @@ pub struct BioauthFlowParams {
     pub webapp_url: Option<String>,
 
     /// The URL to pass to the web app to connect to the node RPC.
-    /// If not passed, a URL with `localhost` and the HTTP RPC port will be used.
+    /// If not passed, a URL with WebSocket scheme and `localhost` path will be used.
     #[arg(long, value_name = "RPC_URL", conflicts_with_all = &["rpc_url_scheme_preference", "rpc_url_ngrok_detect", "rpc_url_unset"])]
     pub rpc_url: Option<String>,
 
