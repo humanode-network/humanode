6dadff7f5e2dd8d73240d6dddf47190e18028a3b
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-05-08 21:42:16 +0300

    Fix clippy at evm debug api

--- crates/evm-debug-api/src/lib.rs
+++ crates/evm-debug-api/src/lib.rs
@@ -1,13 +1,15 @@
 //! The runtime API for the EVM debug logic.
 
 #![cfg_attr(not(feature = "std"), no_std)]
-// TODO: fix clippy.
-#![allow(missing_docs, clippy::too_many_arguments)]
 
 use sp_core::{sp_std::vec::Vec, H160, H256, U256};
 
 sp_api::decl_runtime_apis! {
+    /// Runtime API for the EVM debug logic.
     pub trait DebugRuntimeApi {
+        // Allow too many arguments to pass them in the way used at EVM runner call.
+        #[allow(clippy::too_many_arguments)]
+        /// Trace EVM call execution.
         fn trace_call(
             header: &Block::Header,
             from: H160,
