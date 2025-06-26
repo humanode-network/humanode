4fc864c783cd9637a142c96da6e6863b082e2870
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-06-26 12:57:44 +0300

    Define response formatter

---
+++ crates/evm-tracing-client/src/formatters/mod.rs
@@ -0,0 +1,15 @@
+//! Formatters implementation.
+
+use primitives_evm_tracing_events::Listener;
+use serde::Serialize;
+
+/// Response formatter.
+pub trait ResponseFormatter {
+    /// Listener type.
+    type Listener: Listener;
+    /// Response type.
+    type Response: Serialize;
+
+    /// Format.
+    fn format(listener: Self::Listener) -> Option<Self::Response>;
+}
