53813288e0812520af0348aafa29124d6b8f4ae1
Petr Ermishkin <petr@humanode.io>
2025-06-26 15:16:32 +0300

    Remove explicit "peer" indication as an artifact selector

--- .github/scripts/artifact-upload-params.sh
+++ .github/scripts/artifact-upload-params.sh
@@ -1,35 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-case "$ARTIFACT_SELECTOR" in
-  peer)
-    ARTIFACT_PATH='target/release/humanode-peer'
-    ARTIFACT_NAME="humanode-peer-$(rustc -vV | sed -n 's|host: ||p')"
-
-    if [[ "$PLATFORM_ARTIFACT_MARKER" != "" ]]; then
-      ARTIFACT_NAME="${ARTIFACT_NAME}-${PLATFORM_ARTIFACT_MARKER}"
-    fi
-
-    if [[ "${PATHEXT:-""}" != "" ]]; then
-      ARTIFACT_PATH="${ARTIFACT_PATH}.exe"
-    fi
-    ;;
-
-  runtime)
-    if [[ -z "${MODE_ARTIFACT_MARKER}" ]]; then
-      printf 'MODE_ARTIFACT_MARKER must not be empty\n' >&2
-      exit 1
-    fi
-
-    ARTIFACT_PATH='target/release/wbuild/humanode-runtime/humanode_runtime.compact.compressed.wasm'
-    ARTIFACT_NAME="humanode-runtime-${MODE_ARTIFACT_MARKER}.wasm"
-    ;;
-
-  *)
-    printf 'Unknown artifact selector `%s`\n' "$ARTIFACT_SELECTOR" >&2
+if [[ "$ARTIFACT_SELECTOR" == 'runtime' ]]; then
+  if [[ -z "$MODE_ARTIFACT_MARKER" ]]; then
+    printf 'MODE_ARTIFACT_MARKER must not be empty\n' >&2
     exit 1
-    ;;
-esac
+  fi
+
+  ARTIFACT_PATH='target/release/wbuild/humanode-runtime/humanode_runtime.compact.compressed.wasm'
+  ARTIFACT_NAME="humanode-runtime-${MODE_ARTIFACT_MARKER}.wasm"
+else
+  ARTIFACT_PATH='target/release/humanode-peer'
+  ARTIFACT_NAME="humanode-peer-$(rustc -vV | sed -n 's|host: ||p')"
+
+  if [[ "$PLATFORM_ARTIFACT_MARKER" != "" ]]; then
+    ARTIFACT_NAME="${ARTIFACT_NAME}-${PLATFORM_ARTIFACT_MARKER}"
+  fi
+
+  if [[ "${PATHEXT:-""}" != "" ]]; then
+    ARTIFACT_PATH="${ARTIFACT_PATH}.exe"
+  fi
+fi
 
 printf 'artifact-path=%s\n' "$ARTIFACT_PATH" >> "$GITHUB_OUTPUT"
 printf 'artifact-name=%s\n' "$ARTIFACT_NAME" >> "$GITHUB_OUTPUT"
