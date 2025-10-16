021dfcd3c38c3b7775920a78946f4731072b4862
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-10-16 14:49:32 +0300

    Extend debugTraceTransaction tests with the one that test big responses

--- utils/e2e-tests/ts/tests/evm-tracing/debugTraceTransaction.ts
+++ utils/e2e-tests/ts/tests/evm-tracing/debugTraceTransaction.ts
@@ -1,9 +1,10 @@
-import { beforeEach, describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { RunNodeState, runNode } from "../../lib/node";
 import * as eth from "../../lib/ethViem";
 import { beforeEachWithCleanup } from "../../lib/lifecycle";
 import callee from "../../lib/abis/evmTracing/callee";
 import caller from "../../lib/abis/evmTracing/caller";
+import heavy from "../../lib/abis/evmTracing/heavy";
 import { encodeFunctionData } from "viem";
 import { customRpcRequest } from "../../lib/rpcUtils";
 
@@ -25,37 +26,34 @@ describe("test debug trace transaction logic", () => {
     devClients = eth.devClientsFromNodeWebSocket(node, cleanup.push);
   }, 60 * 1000);
 
-  let calleeAddress: `0x${string}`;
-  let callerAddress: `0x${string}`;
-
-  beforeEach(async () => {
+  it("should trace nested contract calls", async () => {
     const [alice, _] = devClients;
 
     const deployCalleeContractTxHash = await alice.deployContract({
       abi: callee.abi,
       bytecode: callee.bytecode,
     });
+
     const deployCalleeContractTxReceipt =
       await publicClient.waitForTransactionReceipt({
         hash: deployCalleeContractTxHash,
         timeout: 18_000,
       });
-    calleeAddress = deployCalleeContractTxReceipt.contractAddress!;
+
+    const calleeAddress = deployCalleeContractTxReceipt.contractAddress!;
 
     const deployCallerContractTxHash = await alice.deployContract({
       abi: caller.abi,
       bytecode: caller.bytecode,
     });
+
     const deployCallerContractTxReceipt =
       await publicClient.waitForTransactionReceipt({
         hash: deployCallerContractTxHash,
         timeout: 18_000,
       });
-    callerAddress = deployCallerContractTxReceipt.contractAddress!;
-  });
 
-  it("should trace nested contract calls", async () => {
-    const [alice, _] = devClients;
+    const callerAddress = deployCallerContractTxReceipt.contractAddress!;
 
     const txHash = await alice.sendTransaction({
       to: callerAddress,
@@ -85,5 +83,48 @@ describe("test debug trace transaction logic", () => {
     expect(logs).to.be.lengthOf(2);
     expect(logs[0].depth).to.be.equal(2);
     expect(logs[1].depth).to.be.equal(1);
+  });
+
+  it("should not trace call that would produce too big responses", async () => {
+    const [alice, _] = devClients;
+
+    const deployHeavyContractTxHash = await alice.deployContract({
+      abi: heavy.abi,
+      bytecode: heavy.bytecode,
+      args: [false],
+    });
+
+    const deployHeavyContractTxReceipt =
+      await publicClient.waitForTransactionReceipt({
+        hash: deployHeavyContractTxHash,
+        timeout: 18_000,
+      });
+
+    const heavyAddress = deployHeavyContractTxReceipt.contractAddress!;
+
+    const txHash = await alice.sendTransaction({
+      to: heavyAddress,
+      gasLimit: "0x800000",
+      value: 0n,
+      data: encodeFunctionData({
+        abi: heavy.abi,
+        functionName: "heavy_steps",
+        args: [100n, 1000n],
+      }),
+    });
+    await publicClient.waitForTransactionReceipt({ hash: txHash });
+
+    await customRpcRequest(node.meta.rpcUrlHttp, "debug_traceTransaction", [
+      txHash,
+    ]).then(
+      () => {
+        expect.fail("trace should be reverted but it worked instead");
+      },
+      (error) => {
+        expect(error.message).to.eq(
+          "replayed transaction generated too much data. try disabling memory or storage?",
+        );
+      },
+    );
   });
 });
